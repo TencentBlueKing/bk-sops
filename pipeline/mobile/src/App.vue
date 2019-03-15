afe88861c62f0cfb0e79b94124fb35873110e4a6
fannluo <fannluo@gmail.com>
2019-03-15 15:56:28 +0800

    feature: 初始化移动端工程

---
+++ pipeline/mobile/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="app-container">
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    import { mapGetters } from 'vuex'
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+                routerKey: +new Date(),
+                systemCls: 'mac'
+            }
+        },
+        computed: {
+            ...mapGetters(['mainContentLoading'])
+        },
+        watch: {
+        },
+        created () {
+            const platform = window.navigator.platform.toLowerCase()
+            if (platform.indexOf('win') === 0) {
+                this.systemCls = 'win'
+            }
+        },
+        methods: {
+            /**
+             * router 跳转
+             *
+             * @param {string} idx 页面指示
+             */
+            goPage (idx) {
+                this.$router.push({
+                    name: idx
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+    @import './css/reset.css';
+    @import './css/app.css';
+</style>
