d9171662142ad3e9591177af50eedb908b1bd784
homholueng <homholueng@gmail.com>
2019-04-10 18:03:17 +0800

    添加非标准导入器及 git 仓库导入器 (#46)
    * feature: 添加非标准存储代码导入器及 git 仓库代码导入器
    
    * minor: 将 urllib2 的使用改为 requests
    
    * minor: 移除测试代码中的关键字使用
    
    * minor: 内部包与第三方包之间添加空格
    
    * minor: 删除冗余编码声明; 第三方包引用间添加空行

--- pipeline/tests/mock_settings.py
+++ pipeline/tests/mock_settings.py
@@ -10,6 +10,12 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
+SYS_MODULES = 'sys.modules'
+IMP_ACQUIRE_LOCK = 'imp.acquire_lock'
+IMP_RELEASE_LOCK = 'imp.release_lock'
+REQUESTS_GET = 'requests.get'
+
 PIPELINE_CORE_GATEWAY_DEFORMAT = 'pipeline.core.flow.gateway.deformat_constant_key'
 PIPELINE_CORE_CONSTANT_RESOLVE = 'pipeline.core.data.expression.ConstantTemplate.resolve_data'
 
@@ -114,3 +120,10 @@ EXG_HYDRATE_NODE_DATA = 'pipeline.engine.core.handlers.exclusive_gateway.hydrate
 EXG_HYDRATE_DATA = 'pipeline.engine.core.handlers.exclusive_gateway.hydrate_data'
 
 CPG_HYDRATE_DATA = 'pipeline.engine.core.handlers.conditional_parallel.hydrate_data'
+
+UTILS_IMPORTER_BASE_EXECUTE_SRC_CODE = 'pipeline.utils.importer.base.NonstandardModuleImporter._execute_src_code'
+UTILS_IMPORTER_GIT__FETCH_REPO_FILE = 'pipeline.utils.importer.git.GitRepoModuleImporter._fetch_repo_file'
+UTILS_IMPORTER_GIT__FILE_URL = 'pipeline.utils.importer.git.GitRepoModuleImporter._file_url'
+UTILS_IMPORTER_GIT_GET_SOURCE = 'pipeline.utils.importer.git.GitRepoModuleImporter.get_source'
+UTILS_IMPORTER_GIT_GET_FILE = 'pipeline.utils.importer.git.GitRepoModuleImporter.get_file'
+UTILS_IMPORTER_GIT_IS_PACKAGE = 'pipeline.utils.importer.git.GitRepoModuleImporter.is_package'
