2ab2c8059f50351b4fa6818e8ac61d5efe37eca1
pagezz <zongpeizhou@gmail.com>
2019-04-11 22:01:51 +0800

    improve: 本地开发local_settings模板文件优化 #59 (#66)

--- scripts/develop/dev_settings.py
+++ scripts/develop/dev_settings.py
@@ -10,6 +10,8 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -21,5 +23,8 @@ DATABASES = {
     },
 }
 
-BK_CC_HOST = ''
-BK_JOB_HOST = ''
+REDIS = {
+    'host': 'localhost',
+    'port': 6379,
+    'db': 0
+}
