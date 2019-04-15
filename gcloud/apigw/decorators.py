538367d6d5e90083ad24f3c60ae6996835c022ca
homholueng <homholueng@gmail.com>
2019-04-15 14:51:20 +0800

    minor: 版本差异配置提取

--- gcloud/apigw/decorators.py
+++ gcloud/apigw/decorators.py
@@ -27,7 +27,7 @@ from gcloud.taskflow3.models import TaskFlowInstance
 from gcloud.tasktmpl3.models import TaskTemplate
 from gcloud.conf import settings
 
-if not sys.argv[1:2] == ['test'] and settings.RUN_VER != 'open':
+if not sys.argv[1:2] == ['test'] and settings.USE_BK_OAUTH:
     try:
         from bkoauth.decorators import apigw_required
     except ImportError:
