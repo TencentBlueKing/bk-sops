a0be56ea112eacd1ad0e469cc60c317d45fdb2bd
homholueng <homholueng@gmail.com>
2019-04-15 16:38:03 +0800

    minor: 异常信息优化

--- pipeline_plugins/components/collections/sites/open/job.py
+++ pipeline_plugins/components/collections/sites/open/job.py
@@ -134,7 +134,7 @@ class JobExecuteTaskService(JobService):
                     use_cache=False)
                 ip_list = [{'ip': _ip['InnerIP'], 'bk_cloud_id': _ip['Source']} for _ip in var_ip['ip_result']]
                 if _value['value'].strip() and not ip_list:
-                    data.outputs.ex_data = _(u"无法从 CC 查询到对应 IP，请确认输入的 IP 是否合法")
+                    data.outputs.ex_data = _(u"无法从配置平台(CMDB)查询到对应 IP，请确认输入的 IP 是否合法")
                     return False
                 if ip_list:
                     global_vars.append({
