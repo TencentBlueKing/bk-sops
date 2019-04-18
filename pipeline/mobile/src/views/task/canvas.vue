f9e30c2098a5e4f728150043ad6240e6d33292c9
liubihu <liubihu@yovole.com>
2019-04-18 18:28:14 +0800

    基于1.2页面调整

--- pipeline/mobile/src/views/task/canvas.vue
+++ pipeline/mobile/src/views/task/canvas.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="page-view">
-        <div class="task-status info">
-            执行中
-        </div>
-        <div class="task-status warning">
-            暂停
-        </div>
         <div class="task-status danger">
-            撤销
+            失败
         </div>
+        <!--演示试用 start-->
+        <div class="canvas-demo">
+            <div class="container">
+                <div class="box">演示</div>
+                <div class="tips-bar">
+                    <van-button type="default" class="">执行详情</van-button>
+                    <van-button type="default" class="">重试</van-button>
+                    <van-button type="default" class="">跳过</van-button>
+                </div>
+            </div>
+        </div>
+        <!--演示试用 end-->
         <van-tabbar>
             <van-tabbar-item>
                 <van-icon slot="icon" class-prefix="icon" name="pause" class="disabled" disabled />
@@ -53,53 +59,32 @@
     }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     @import '../../../static/style/app.scss';
-    .task-status{
-        border-bottom-style: solid;
-        border-bottom-width: 1px;
-        padding: 10px;
-        text-align: center;
-        font-size: $fs-14;
-    }
-    .task-status.info{
-        background: #CFDFFB;
-        border-bottom-color: #C0D4F8;
-        color: $blue;
-    }
-    .task-status.warning{
-        background: #FFE8C3;
-        border-bottom-color: #E6CFAA;
-        color: #D78300;
-    }
-    .task-status.danger{
-        background: #F2D0D3;
-        border-bottom-color: #EFB9BE;
-        color: #EA3636;
-    }
-    .van-tabbar{
-        background-color: $background-color;
-
-        .van-tabbar-item__icon{
-            font-size: 22px;
-            margin-bottom: 0;
-
-            .icon-pause,.icon-pause{
-                color: $blue;
+    /*以下为演示试用可删除*/
+    .canvas-demo{
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        height: calc(100% - 90px);
+        .container{
+            width: 100%;
+            text-align: center;
+            position: relative;
+            .box{
+                width: 120px;
+                height: 60px;
+                line-height: 60px;
+                border:1px solid #EA3636;
+                background: #fff;
+                margin: auto;
+                font-size: 14px;
             }
-            .icon-revoke{
-                color: #EA3636;
+            .tips-bar{
+                position: absolute;
+                width: 100%;
+                top: -60px;
             }
-            .icon-file{
-                color: #4E4E4E;
-            }
-            .disabled{
-                opacity: 0.4;
-            }
-        }
-
-        &:after{
-            border-color: #E5E5E5;
         }
     }
 </style>
