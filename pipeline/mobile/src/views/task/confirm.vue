011c3fa019905083789ee5f93deeef863a419ec3
liubihu <liubihu@yovole.com>
2019-04-18 15:59:39 +0800

    基于1.2页面调整

---
+++ pipeline/mobile/src/views/task/confirm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <van-button
+            type="primary"
+            plain
+            @click="onClickConfirm">
+            确定弹窗
+        </van-button>
+        <van-dialog
+            v-model="show"
+            title="标题"
+            show-cancel-button>
+        </van-dialog>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'confirm',
+        data () {
+            return {
+                show: false
+            }
+        },
+        methods: {
+            onClickConfirm () {
+                this.$dialog.confirm({
+                    message: '弹窗内容'
+                })
+            }
+        }
+    }
+</script>
---
+++ pipeline/mobile/src/views/task/detail2.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class="page-view">
+        <!-- 信息 -->
+        <section class="bk-block">
+            <van-cell>
+                <template slot="title">
+                    <div class="bk-text">定时</div>
+                </template>
+                <div class="status"><van-icon slot="right-icon" name="clear" class="task-icon close" /><span class="text">失败</span></div>
+            </van-cell>
+        </section>
+        <!-- 执行信息 -->
+        <section class="bk-block">
+            <h2 class="bk-text-title">执行信息</h2>
+            <div class="bk-text-list">
+                <van-cell title="开始时间" value="2018-10-17 17:35:45 +0800" />
+                <van-cell title="结束时间" value="2018-10-17 17:35:45 +0800" />
+                <van-cell title="耗时" value="34秒" />
+                <van-cell title="失败后跳过" value="否" />
+                <van-cell title="失败后自动忽略" value="34秒" />
+                <van-cell title="重试次数" value="0" />
+            </div>
+        </section>
+        <!-- 输入参数 -->
+        <section class="bk-block">
+            <h2 class="bk-text-title">输入参数</h2>
+            <div class="parameter-info">
+                { "activities":{ "error_ignorable":false, "component":{ "code":"bk_notify", "data":{ "bk_notify_type":{ "hook":false] }, { "activities":{ "error_ignorable":false, "component":{ "code":"bk_notify", "data":{ "bk_notify_type":{ "hook":false ] },
+                { "activities":{ "error_ignorable":false, "component":{ "code":"bk_notify", "data":{ "bk_notify_type":{ "hook":false] }, { "activities":{ "error_ignorable":false, "component":{ "code":"bk_notify", "data":{ "bk_notify_type":{ "hook":false ] },
+            </div>
+            <van-button type="default" class="view-btn">查看全部</van-button>
+        </section>
+        <!-- 输入参数 -->
+        <section class="bk-block">
+            <h2 class="bk-text-title">输入参数</h2>
+            <div class="bk-text-list">
+                <van-cell title="JOB任务ID" value="--" />
+                <van-cell title="JOB任务链接" value="--" />
+                <van-cell title="执行结果" value="false" />
+            </div>
+        </section>
+        <!-- 异常信息 -->
+        <section class="bk-block">
+            <h2 class="bk-text-title">异常信息</h2>
+            <div class="bk-text-list">
+                <van-cell value="Parameter [91global_vars] does not contain IP" />
+            </div>
+        </section>
+        <!-- 执行记录01 -->
+        <section class="bk-block">
+            <h2 class="bk-text-title">执行记录01</h2>
+            <div class="bk-text-list">
+                <van-cell title="开始时间" value="2018-10-17 17:35:45 +0800" />
+                <van-cell title="结束时间" value="2018-10-17 17:35:45 +0800" />
+                <van-cell title="耗时（s）" value="34" />
+            </div>
+        </section>
+        <!-- 执行记录02 -->
+        <section class="bk-block">
+            <h2 class="bk-text-title">执行记录02</h2>
+            <div class="bk-text-list">
+                <van-cell title="开始时间" value="2018-10-17 17:35:45 +0800" />
+                <van-cell title="结束时间" value="2018-10-17 17:35:45 +0800" />
+                <van-cell title="耗时（s）" value="34" />
+            </div>
+        </section>
+    </div>
+</template>
+<script>
+    import { mapActions } from 'vuex'
+
+    export default {
+        name: 'TaskDetail',
+        data () {
+            return {
+                task: {}
+            }
+        },
+        mounted () {
+            this.loadData()
+        },
+        methods: {
+            ...mapActions('task', [
+                'getTask'
+            ]),
+
+            async loadData () {
+                this.task = await this.getTask()
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../../../static/style/app.scss';
+    .bk-block .status{
+        .text{
+            margin-left: 5px;
+            vertical-align: text-bottom;
+            font-size: $fs-12;
+            color: $text-color-light;
+        }
+    }
+    .parameter-info{
+        background: #313238;
+        max-height: 100px;
+        color: $white;
+        font-size: $fs-14;
+        padding: 0 25px;
+        overflow: hidden;
+    }
+    .view-btn{
+        display: block;
+        background: none;
+        border-radius: 0;
+        margin: 15px 25px 0 25px;
+        width: calc(100% - 50px);
+        font-size: $fs-14;
+    }
+</style>
