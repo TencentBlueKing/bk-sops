a66740ca517a8c3abe1a081ececa0cc002a1bb3f
pagezhou <zongpeizhou@gmail.com>
2019-05-12 23:04:26 +0800

    improve: APP_HOST改为SITE_URL; 去掉RUN_MODE和DEVELOP的比较，改为IS_LOCAL; 去掉部分预发布环境专用变量 #285

--- pipeline_plugins/components/utils/sites/open/utils.py
+++ pipeline_plugins/components/utils/sites/open/utils.py
@@ -255,9 +255,5 @@ def get_job_instance_url(biz_cc_id, job_instance_id):
 
 def get_node_callback_url(node_id):
     f = Fernet(settings.CALLBACK_KEY)
-    if settings.RUN_MODE == 'PRODUCT':
-        callback_prefix = settings.APP_HOST
-    else:
-        callback_prefix = settings.TEST_APP_HOST
-    return "%staskflow/api/nodes/callback/%s/" % (callback_prefix,
+    return "%staskflow/api/nodes/callback/%s/" % (settings.SITE_URL,
                                                   f.encrypt(bytes(node_id)))
