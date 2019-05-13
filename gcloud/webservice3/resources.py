f34887f6260f1ca1af4d23f4dcf907ca550325c2
homholueng <homholueng@gmail.com>
2019-05-13 17:22:31 +0800

    minor: code padding

--- gcloud/webservice3/resources.py
+++ gcloud/webservice3/resources.py
@@ -300,7 +300,7 @@ class GCloudModelResource(ModelResource):
 class BusinessResource(GCloudModelResource):
     class Meta:
         queryset = Business.objects.exclude(life_cycle__in=[Business.LIFE_CYCLE_CLOSE_DOWN, _(u"停运")]) \
-            .exclude(status='disabled')
+                                   .exclude(status='disabled')
         list_allowed_methods = ['get']
         detail_allowed_methods = ['get']
         authorization = GCloudReadOnlyAuthorization()
