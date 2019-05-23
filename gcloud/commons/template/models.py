281f6005f269cd006aad798c15f653d19ff9679b
homholueng <homholueng@gmail.com>
2019-05-23 16:20:15 +0800

    minor: taskflow3 appmaker 模块移除业务模型相关逻辑

--- gcloud/commons/template/models.py
+++ gcloud/commons/template/models.py
@@ -28,15 +28,6 @@ from gcloud.core.constant import TASK_CATEGORY
 from gcloud.core.utils import convert_readable_username
 
 
-def get_common_permission_list():
-    permission_list = [
-        ('common_%s' % PermNm.CREATE_TASK_PERM_NAME, 'common template create task'),
-        ('common_%s' % PermNm.FILL_PARAMS_PERM_NAME, 'common template fill params'),
-        ('common_%s' % PermNm.EXECUTE_TASK_PERM_NAME, 'common template execute task'),
-    ]
-    return permission_list
-
-
 def replace_template_id(template_model, pipeline_data, reverse=False):
     activities = pipeline_data[PE.activities]
     for act_id, act in activities.iteritems():
@@ -354,7 +345,6 @@ class CommonTmplPerm(models.Model):
     biz_cc_id = models.CharField(_(u"通用流程模板ID"), max_length=255)
 
     class Meta:
-        permissions = get_common_permission_list()
         unique_together = ('common_template_id', 'biz_cc_id')
         index_together = ['common_template_id', 'biz_cc_id']
         verbose_name = _(u"公共流程模板权限 CommonTmplPerm")
