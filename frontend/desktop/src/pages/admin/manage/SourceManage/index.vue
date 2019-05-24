c10e39b4ae236a12e37f56214beb2ee1813e512d
fannluo <fannluowork@gmail.com>
2019-05-24 10:08:47 +0800

    feature: 包源编辑

--- frontend/desktop/src/pages/admin/manage/SourceManage/index.vue
+++ frontend/desktop/src/pages/admin/manage/SourceManage/index.vue
@@ -1,24 +1,65 @@
 <template>
     <div class="source-manage">
         <div class="operate-area">
-            <bk-button type="primary">{{i18n.create}}</bk-button>
-            <bk-button type="primary">{{i18n.edit}}</bk-button>
+            <router-link
+                to="/admin/manage/source_edit/package_edit/"
+                class="operate-btn bk-button bk-primary">
+                {{ false ? i18n.create : i18n.edit }}
+            </router-link>
         </div>
-        <div class="empty-data">
-            <p>{{i18n.noData}}<router-link to="/admin/source_manange/package/">{{i18n.create}}</router-link>{{i18n.sourceManage}}</p>
+        <div class="section-title">
+            <div class="title-wrapper">
+                <h4>{{i18n.mainSource}}</h4>
+                <i
+                    class="title-tooltip bk-icon icon-info-circle"
+                    v-bktooltips.right="i18n.sourceTip">
+                </i>
+            </div>
+        </div>
+        <template>
+            <package-table></package-table>
+            <package-table></package-table>
+        </template>
+        <NoData v-if="false"></NoData>
+        <div class="section-title">
+            <div class="title-wrapper">
+                <h4>{{i18n.localCache}}</h4>
+                <i
+                    class="title-tooltip bk-icon icon-info-circle"
+                    v-bktooltips.right="i18n.cacheTip">
+                </i>
+            </div>
+        </div>
+        <local-cache v-if="true"></local-cache>
+        <NoData v-else></NoData>
+        <div class="empty-data" v-if="false">
+            <p>{{i18n.noData}}<router-link to="/admin/manage/source_edit/package_edit/">{{i18n.create}}</router-link>{{i18n.sourceManage}}</p>
         </div>
     </div>
 </template>
 <script>
+    import PackageTable from './PackageTable.vue'
+    import LocalCache from './LocalCache.vue'
+    import NoData from '@/components/common/base/NoData.vue'
+
     export default {
         name: 'SourceSync',
+        components: {
+            PackageTable,
+            LocalCache,
+            NoData
+        },
         data () {
             return {
                 i18n: {
                     create: gettext('新建'),
                     edit: gettext('编辑'),
                     noData: gettext('暂无数据，'),
-                    sourceManage: gettext('包源管理')
+                    sourceManage: gettext('包源管理'),
+                    mainSource: gettext('主包源'),
+                    localCache: gettext('本地缓存'),
+                    sourceTip: gettext('Xxxxxxxx'),
+                    cacheTip: gettext('Yyyyyyyyy')
                 }
             }
         }
@@ -27,22 +68,51 @@
 <style lang="scss" scoped>
     .source-manage {
         height: calc(100% - 80px);
+        padding-bottom: 60px;
     }
     .operate-area {
-        .bk-button {
+        .operate-btn {
             height: 32px;
             line-height: 32px;
+        }
+    }
+    .section-title {
+        margin: 20px 0;
+        font-size: 14px;
+        .title-wrapper {
+            display: flex;
+            align-items: center;
+        }
+        h4 {
+            display: inline-block;
+            margin: 0;
+            color: #313238;
+            line-height: 1;
+        }
+        .title-tooltip {
+            display: inline-block;
+            margin-left: 10px;
+            color: #c4c6cc;
+            font-size: 16px;
+            &:hover {
+                color: #f4aa1a;
+            }
         }
     }
     .empty-data {
         display: flex;
         justify-content: center;
         align-items: center;
-        height: calc(100% - 32px);
+        height: calc(100% - 130px);
         font-size: 14px;
         color: #c4c6cc;
         a {
             color: #3a84ff;
         }
+    }
+    .no-data-wrapper {
+        margin-top: 30px;
+        background: transparent;
+        color: #c4c6cc;
     }
 </style>
