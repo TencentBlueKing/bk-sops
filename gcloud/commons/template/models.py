d68c841b964a0ae9408ba0ad857c6f3a124a673c
homholueng <homholueng@gmail.com>
2019-05-30 15:13:23 +0800

    minor: 修改模板导入api协议

--- gcloud/commons/template/models.py
+++ gcloud/commons/template/models.py
@@ -345,7 +345,7 @@ class CommonTemplateManager(BaseTemplateManager):
 
         return {
             'result': True,
-            'data': len(template),
+            'data': {'num_of_import': len(template)},
             'message': 'Successfully imported %s common flows' % len(template)
         }
 
