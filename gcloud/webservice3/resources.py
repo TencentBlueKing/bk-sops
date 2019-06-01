c1957f58b793812c8a967ff266c4bb3300a6dc13
homholueng <homholueng@gmail.com>
2019-06-01 14:42:35 +0800

    feature: 添加项目模型操作接口

--- gcloud/webservice3/resources.py
+++ gcloud/webservice3/resources.py
@@ -22,11 +22,12 @@ from guardian.shortcuts import get_objects_for_user
 from haystack.query import SearchQuerySet
 from tastypie import fields
 from tastypie.paginator import Paginator
-from tastypie.authorization import ReadOnlyAuthorization
+from tastypie.authorization import ReadOnlyAuthorization, Authorization
 from tastypie.constants import ALL
 from tastypie.exceptions import NotFound, ImmediateHttpResponse
 from tastypie.resources import ModelResource
 from tastypie.serializers import Serializer
+from tastypie.exceptions import BadRequest
 
 from pipeline.component_framework.library import ComponentLibrary
 from pipeline.component_framework.models import ComponentModel
@@ -336,16 +337,40 @@ class BusinessResource(GCloudModelResource):
 class ProjectResource(GCloudModelResource):
     name = fields.CharField(attribute='name')
     time_zone = fields.CharField(attribute='time_zone')
-    creator = fields.CharField(attribute='creator', readonly=True)
+    creator = fields.CharField(attribute='creator')
     desc = fields.CharField(attribute='desc')
     create_at = fields.DateTimeField(attribute='create_at', readonly=True)
     from_cmdb = fields.BooleanField(attribute='from_cmdb', readonly=True)
     cmdb_biz_id = fields.IntegerField(attribute='cmdb_biz_id', readonly=True)
-    is_disable = fields.BooleanField(attribute='is_disable', readonly=True)
-    
+    is_disable = fields.BooleanField(attribute='is_disable')
+
+    ALLOW_UPDATE_FIELD = {'desc', 'is_disable'}
+
     class Meta:
         queryset = Project.objects.all()
         resource_name = 'project'
+        authorization = Authorization()
+        always_return_data = True
+
+    def dehydrate(self, bundle):
+        bundle.data['create_at'] = bundle.data['create_at'].strftime('%Y-%m-%d %H:%M:%S')
+        return bundle
+
+    def obj_create(self, bundle, **kwargs):
+        bundle.data['creator'] = bundle.request.user.username
+        return super(ProjectResource, self).obj_create(bundle, **kwargs)
+
+    def obj_update(self, bundle, skip_errors=False, **kwargs):
+        update_data = {}
+        for field in self.ALLOW_UPDATE_FIELD:
+            update_data[field] = bundle.data.get(field, getattr(bundle.obj, field))
+
+        bundle.data = update_data
+
+        return super(ProjectResource, self).obj_update(bundle, skip_errors, **kwargs)
+
+    def obj_delete(self, bundle, **kwargs):
+        raise BadRequest("can not delete project")
 
 
 class ComponentModelResource(ModelResource):
