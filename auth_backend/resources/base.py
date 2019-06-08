23691ddd5f93645a520b20d0ab48a6d77f455938
homholueng <homholueng@gmail.com>
2019-06-08 16:54:30 +0800

    improvement: auth_backend 增强，添加无实例资源类型，添加鉴权接口

--- auth_backend/resources/base.py
+++ auth_backend/resources/base.py
@@ -13,6 +13,10 @@ specific language governing permissions and limitations under the License.
 
 import abc
 
+from ..exceptions import InvalidOperation
+
+resource_type_lib = {}
+
 
 class Action(object):
     def __init__(self, id, name, is_instance_related):
@@ -34,6 +38,12 @@ class Resource(object):
         self.backend = backend
         self.parent = parent
         self.parent_getter = parent_getter
+        self.actions_map = {act.id: act for act in actions}
+
+        if rtype in resource_type_lib:
+            raise KeyError('Resource with rtype: {rtype} already exist.'.format(rtype=rtype))
+
+        resource_type_lib[rtype] = self
 
     def resource_id(self, instance):
         return self.inspect.resource_id(instance)
@@ -50,6 +60,10 @@ class Resource(object):
     def parent_instance(self, child):
         return self.inspect.parent(child)
 
+    def is_instance_related_action(self, action_id):
+        return self.actions_map[action_id].is_instance_related
+
+    # api
     def register_instance(self, instance):
         return self.backend.register_instance(resource=self, instance=instance)
 
@@ -64,6 +78,61 @@ class Resource(object):
 
     def batch_delete_instance(self, instances):
         return self.backend.batch_delete_instance(resource=self, instances=instances)
+
+    def verify_principal_perms(self, principal_type, principal_id, action_ids, instance=None):
+        return self.backend.verify_principal_perms(principal_type=principal_type,
+                                                   principal_id=principal_id,
+                                                   resource=self,
+                                                   instance=instance,
+                                                   action_ids=action_ids)
+
+    def batch_verify_principal_perms(self, principal_type, principal_id, action_ids, instances=None):
+        return self.backend.verify_principal_perms(principal_type=principal_type,
+                                                   principal_id=principal_id,
+                                                   resource=self,
+                                                   instances=instances,
+                                                   action_ids=action_ids)
+
+
+class NeverInitiateResource(Resource):
+    def __init__(self, rtype, name, scope_type, scope_id, actions, backend):
+        super(NeverInitiateResource, self).__init__(rtype=rtype,
+                                                    name=name,
+                                                    actions=actions,
+                                                    scope_type=scope_type,
+                                                    scope_id=scope_id,
+                                                    backend=backend,
+                                                    inspect=None)
+
+    def resource_id(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def resource_name(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def creator_type(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def creator_id(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def parent_instance(self, child):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def register_instance(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def batch_register_instance(self, instances):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def update_instance(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def delete_instance(self, instance):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
+
+    def batch_delete_instance(self, instances):
+        raise InvalidOperation('can not perform instance related operation on NeverInitiateResource')
 
 
 class ObjectResource(Resource):
