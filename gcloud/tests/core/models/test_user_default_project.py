70f8236ff4e26210ddb5ee73da6dac6b22dd27b3
pagezhou <zongpeizhou@gmail.com>
2019-06-11 16:47:07 +0800

    minor: 更新项目资源的单元测试

--- gcloud/tests/core/models/test_user_default_project.py
+++ gcloud/tests/core/models/test_user_default_project.py
@@ -10,18 +10,20 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
+import factory
+from django.db.models import signals
 from django.test import TestCase
 
 from gcloud.core.models import Project, UserDefaultProject
 
 
 class UserDefaultProjectTestCase(TestCase):
-
+    @factory.django.mute_signals(signals.post_save, signals.post_delete)
     def tearDown(self):
         Project.objects.all().delete()
         UserDefaultProject.objects.all().delete()
 
+    @factory.django.mute_signals(signals.post_save, signals.post_delete)
     def test_init_user_default_project__first_set(self):
         project = Project.objects.create(name='name',
                                          creator='creator',
@@ -29,6 +31,7 @@ class UserDefaultProjectTestCase(TestCase):
         dp = UserDefaultProject.objects.init_user_default_project('username', project)
         self.assertEqual(dp.default_project.id, project.id)
 
+    @factory.django.mute_signals(signals.post_save, signals.post_delete)
     def test_init_user_default_project__second_set(self):
         project_1 = Project.objects.create(name='name',
                                            creator='creator',
