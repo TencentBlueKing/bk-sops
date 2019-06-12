436ec15b09135dd0cb71b14ee520daff181f33c5
840583540 <13206251194@163.com>
2019-06-12 19:45:59 +0800

    feature: 优化周期选择，基本结构已完成

--- frontend/desktop/src/components/common/Individualization/loopRuleSelect.vue
+++ frontend/desktop/src/components/common/Individualization/loopRuleSelect.vue
@@ -12,89 +12,91 @@
 <template>
     <div class="loop-rule-select">
         <div class="loop-rule-title">
-            <bk-button
-                :class="['rule-btn', { 'active-btn': currentWay === 'selectGeneration' }]"
+            <bk-button :class="['rule-btn', { 'active-btn': currentWay === 'selectGeneration' }]"
                 @click="onSwitchWay('selectGeneration')">
                 {{ i18n.selectGeneration }}
             </bk-button>
-            <bk-button
-                :class="['rule-btn', 'manual-input-btn', { 'active-btn': currentWay === 'manualInput' }]"
+            <bk-button :class="['rule-btn', 'manual-input-btn', { 'active-btn': currentWay === 'manualInput' }]"
                 @click="onSwitchWay('manualInput')">
                 {{ i18n.manualInput }}
             </bk-button>
         </div>
         <div class="content-wrapper">
-            <bk-tab
-                v-if="currentWay === 'selectGeneration'"
-                :type="'fill'" :size="'small'"
+            <bk-tab v-if="currentWay === 'selectGeneration'"
+                :type="'fill'"
+                :size="'small'"
                 :active-name="'min'"
                 @tab-changed="tabChanged">
-                <bk-tabpanel
-                    v-for="(item, index) in autoRuleList"
+                <bk-tabpanel v-for="(item, index) in autoRuleList"
                     :key="index"
                     :name="item.k"
                     :title="item.title">
                     <div class="tabpanel-container">
                         <div class="radio-group">
                             <div class="radio-item loop-radio">
-                                <input
-                                    class="ui-radio"
+                                <input class="ui-radio"
                                     type="radio"
                                     id="loop"
                                     v-model="item.radio"
                                     value="0"
                                     :name="item.k" />
-                                <label
-                                    class="ui-label"
+                                <label class="ui-label"
                                     for="loop"
-                                    @click.stop="onAutoWaySwitch(index, 0)">{{ autoWay.loop.name }}</label>
+                                    @click.stop="onAutoWaySwitch(index, '0')">
+                                    {{ autoWay.loop.name }}
+                                </label>
                             </div>
                             <div class="radio-item appoint-radio">
-                                <input
-                                    class="ui-radio"
+                                <input class="ui-radio"
                                     type="radio"
                                     id="appoint"
                                     v-model="item.radio"
                                     value="1"
                                     :name="item.k" />
-                                <label
-                                    class="ui-label"
+                                <label class="ui-label"
                                     for="appoint"
-                                    @click.stop="onAutoWaySwitch(index, 1)">{{ autoWay.appoint.name }}</label>
+                                    @click.stop="onAutoWaySwitch(index, '1')">
+                                    {{ autoWay.appoint.name }}
+                                </label>
                             </div>
                         </div>
-                        <div
-                            v-if="item.radio === 0"
+                        <div v-if="item.radio === '0'"
                             class="loop-select-bd">
                             111
                         </div>
-                        <div
-                            v-else
+                        <div v-else
                             class="appoint-select-bd">
-                            <div class="ui-checkbox-group">
-                                <input
-                                    type="checkbox"
-                                    name="1"
-                                    id="c1">
-                                <label for="c1">01</label>
+                            <div v-for="(box, i) in item.showTextList"
+                                :key="i"
+                                class="ui-checkbox-group">
+                                <input type="checkbox"
+                                    v-model="box.checked"
+                                    :name="box.name"
+                                    :id="box.name">
+                                <label class="ui-checkbox"
+                                    :for="box.name">
+                                    <!-- {{i|add0}} -->
+                                    {{ box.value }}
+                                </label>
                             </div>
+                        </div>
+                        <div class="expression">
+                            {{ i18n.expression }} {{ expressionShowText }}
+                            <span
+                                class="clear-selected">{{ i18n.clearSelected }}
+                            </span>
                         </div>
                     </div>
                 </bk-tabpanel>
             </bk-tab>
-            <!-- <div
-                v-if="currentWay === 'selectGeneration'"
-                class="auto-select-content">111</div> -->
-            <div
-                v-else
+            <div v-else
                 class="hand-input">
-                <BaseInput
-                    ref="canvasNameInput"
-                    :title="tName"
+                <BaseInput ref="canvasNameInput"
                     v-model="tName"
-                    :placeholder="i18n.placeholder"
-                    v-validate="templateNameRule"
                     data-vv-name="templateName"
+                    v-validate="templateNameRule"
+                    :title="tName"
+                    :placeholder="i18n.placeholder"
                     :name="'ruleName'"
                     @input="onInputName"
                     @blur="onInputBlur"
@@ -107,7 +109,6 @@
 <script>
     import '@/utils/i18n.js'
     import BaseInput from '@/components/common/base/BaseInput.vue'
-    // import TagRadio from '@/components/common/RenderForm/tags/TagRadio.vue'
     const autoRuleList = [
         {
             k: 'min',
@@ -151,11 +152,24 @@
             name: gettext('指定')
         }
     }
+    const numberMap = {
+        0: gettext('星期一'),
+        1: gettext('星期二'),
+        2: gettext('星期三'),
+        3: gettext('星期四'),
+        4: gettext('星期五'),
+        5: gettext('星期六'),
+        6: gettext('星期天')
+    }
     export default {
         name: 'loopRuleSelect',
         components: {
             BaseInput
-            // TagRadio
+        },
+        filters: {
+            add0 (v) {
+                return v < 10 ? '0' + v : v
+            }
         },
         data () {
             return {
@@ -164,19 +178,35 @@
                     placeholder: gettext('请输入 IP'),
                     selectGeneration: gettext('选择生成'),
                     manualInput: gettext('手动输入'),
+                    expression: gettext('表达式：'),
+                    clearSelected: gettext('清空已选'),
                     TimeNavTex: []
                 },
+                expressionList: ['0', '*', '11', '522'],
+                // 规则列表
                 autoRuleList: autoRuleList,
+                // 循环选择方式
                 autoWay: autoWay,
+                // manualInput 手动 / selectGeneration 选择生成
                 currentWay: 'selectGeneration',
                 currentRadio: 'loop',
                 tName: '',
                 templateNameRule: ''
             }
         },
+        computed: {
+            expressionShowText () {
+                return this.expressionList.join(',').replace(/\,/g, ' ')
+            }
+        },
         watch: {
         },
-        mounted () { },
+        created () {
+            // this.initializeAutoRuleListData()
+        },
+        mounted () {
+            this.initializeAutoRuleListData()
+        },
         methods: {
             onInputName () { },
             onInputBlur () { },
@@ -186,86 +216,156 @@
             tabChanged (e) {
                 console.log(e, 'sssssssss')
             },
+            /**
+             * 周期循环方式切换,循环/指定
+             * @param {Number} index - 下标
+             * @param {Number} value - 改变的值
+             */
             onAutoWaySwitch (index, value) {
-                const item = this.autoRuleList[index]
-                item.radio = value
-                this.$set(this.autoRuleList, index, item)
+                this.$set(this.autoRuleList[index], 'radio', value)
+            },
+            initializeAutoRuleListData () {
+                this.autoRuleList.forEach((item, index) => {
+                    const pushArr = []
+                    for (let i = 0; i < item.long; i++) {
+                        pushArr.push({
+                            name: `${item.k}${i}`,
+                            checked: false,
+                            value: item.k !== 'week'
+                                ? (i < 10 ? `0${i}`
+                                    : `${i}`) : numberMap[i]
+                        })
+                    }
+                    this.$set(this.autoRuleList[index], 'showTextList', pushArr)
+                })
             }
         }
     }
 </script>
 <style lang="scss" scoped>
 @import "@/scss/config.scss";
-$blueBtnBg:#C7DCFF;
+$blueBtnBg: #c7dcff;
 $colorBalck: #313238;
-$colorGrey: #63656E;
+$colorBlue: #3A84FF;
+$colorGrey: #63656e;
+.ui-checkbox-group {
+  display: inline-block;
+  position: relative;
+  margin-right: 24px;
+  margin-top: 20px;
+  font-size: 14px;
+  & > label {
+    cursor: pointer;
+    color: $colorGrey;
+  }
+  & > input {
+    position: absolute;
+    clip: rect(0, 0, 0, 0);
+  }
+  & > label::before {
+    content: "\a0"; /*不换行空格*/
+    display: inline-block;
+    vertical-align: middle;
+    width: 15px;
+    height: 16px;
+    margin-right: 8px;
+    border: 1px solid $commonBorderColor;
+    box-sizing: border-box;
+  }
+  & > input:checked + label::before {
+    background-color: $blueDefault;
+  }
+}
 .loop-rule-select {
   max-width: 500px;
-  .loop-rule-title{
-      .rule-btn{
-          width: 50%;
-          border: 1px solid $commonBorderColor;
-      }
-      .manual-input-btn{
-          margin-left: -5px;
-      }
-      .active-btn{
-          background-color: $blueBtnBg;
-          border-color: $blueDefault;
-          color: $blueDefault;
-      }
+  // 选择 title
+  .loop-rule-title {
+    .rule-btn {
+      width: 50%;
+      border: 1px solid $commonBorderColor;
+    }
+    .manual-input-btn {
+      margin-left: -5px;
+    }
+    .active-btn {
+      background-color: $blueBtnBg;
+      border-color: $blueDefault;
+      color: $blueDefault;
+    }
   }
-  .content-wrapper{
-      margin-top: 18px;
-      background-color: $whiteDefault;
-      /deep/ .tab2-nav-item{
-          width: 20%;
+  // content
+  .content-wrapper {
+    margin-top: 18px;
+    background-color: $whiteDefault;
+    /deep/ .tab2-nav-item {
+      width: 20%;
+      border-bottom: 1px solid $commonBorderColor;
+      &:not(:first-child) {
+        border-left: 1px solid $commonBorderColor !important;
       }
-      .tabpanel-container{
-          padding: 20px;
-          .radio-item{
-              display: inline-block;
-              &:not(:first-child){
-                margin-left: 48px;
-              }
-              .ui-label{
-                  font-size: 14px;
-                  color: $colorGrey;
-                  &::before{
-                    content: "\a0"; /*不换行空格*/
-                    display: inline-block;
-                    vertical-align: middle;
-                    width: 1em;
-                    height: 1em;
-                    margin-right: .4em;
-                    border-radius: 50%;
-                    border: 1px solid $commonBorderColor;
-                    text-indent: .15em;
-                    line-height: 1;
-                    box-sizing: border-box;
-                    font-size: 16px;
-                  }
-              }
-              .ui-radio{
-                  position: absolute;
-                  clip: rect(0, 0, 0, 0);
-              }
-              .ui-radio:checked + .ui-label::before{
-                    background-color: $blueDefault;
-                    background-clip: content-box;
-                    box-sizing: border-box;
-                    padding: .2em;
-              }
+    }
+    /deep/ .active {
+      border-bottom: none;
+    }
+    /deep/ .bk-tab2 {
+      border: 1px solid $commonBorderColor;
+    }
+    .tabpanel-container {
+      padding: 20px;
+      .radio-item {
+        display: inline-block;
+        &:not(:first-child) {
+          margin-left: 48px;
+        }
+        .ui-label {
+          font-size: 14px;
+          color: $colorGrey;
+          &::before {
+            content: "\a0"; /*不换行空格*/
+            display: inline-block;
+            vertical-align: middle;
+            width: 1em;
+            height: 1em;
+            margin-right: 0.4em;
+            border-radius: 50%;
+            border: 1px solid $commonBorderColor;
+            text-indent: 0.15em;
+            line-height: 1;
+            box-sizing: border-box;
+            font-size: 16px;
           }
-          .loop-select-bd{
-              margin-top: 18px;
-              padding: 20px;
-              border: 1px solid $commonBorderColor;
-          }
-          .appoint-select-bd{
-              margin-top: 18px;
+        }
+        .ui-radio {
+          position: absolute;
+          clip: rect(0, 0, 0, 0);
+        }
+        .ui-radio:checked + .ui-label::before {
+          background-color: $blueDefault;
+          background-clip: content-box;
+          box-sizing: border-box;
+          padding: 0.2em;
+        }
+      }
+      .loop-select-bd {
+        margin-top: 18px;
+        padding: 20px;
+        border: 1px solid $commonBorderColor;
+      }
+      .appoint-select-bd {
+        margin-top: 18px;
+        padding: 0 20px 20px 20px;
+        border: 1px solid $commonBorderColor;
+      }
+      .expression{
+          margin-top: 18px;
+          font-size: 14px;
+          .clear-selected{
+              margin-left: 20px;
+              color: $colorBlue;
+              cursor: pointer;
           }
       }
+    }
   }
 }
 </style>
