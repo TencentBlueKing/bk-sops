3a449104993f2619a8aa7b9cd32d8a1a5ea60e48
homholueng <homholueng@gmail.com>
2019-06-27 20:24:30 +0800

    minor: flake8 fix

--- config/default.py
+++ config/default.py
@@ -11,8 +11,6 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from django.utils.translation import ugettext_lazy
-
 from blueapps.conf.log import get_logging_config_dict
 from blueapps.conf.default_settings import *  # noqa
 
