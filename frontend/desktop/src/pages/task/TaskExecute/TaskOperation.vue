c07ad97a995b780e57234fd8bb7e8a7bcf63f8ab
840583540 <13206251194@163.com>
2019-07-15 19:53:26 +0800

    improve: 任务执行右侧操作icon尺寸偏小

--- frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
+++ frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
@@ -1320,6 +1320,9 @@
             .solid-eye {
                 font-size: 12px;
             }
+            .params-btn-icon, .params-btn {
+                font-size: 15px;
+            }
         }
     }
 }
