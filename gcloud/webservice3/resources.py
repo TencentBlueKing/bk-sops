78bc8e4642d1ce292871892336570a954c075c76
pagezhou <zongpeizhou@gmail.com>
2019-07-18 21:11:20 +0800

    minor: 修复合并master代码后异常问题

--- gcloud/webservice3/resources.py
+++ gcloud/webservice3/resources.py
@@ -18,11 +18,12 @@ from django.utils import timezone
 from django.db.models import Q
 from django.forms.fields import BooleanField
 from django.utils.translation import ugettext_lazy as _
+from django.http.response import HttpResponseForbidden
 from guardian.shortcuts import get_objects_for_user
 from haystack.query import SearchQuerySet
 from tastypie import fields
 from tastypie.paginator import Paginator
-from tastypie.authorization import ReadOnlyAuthorization
+from tastypie.authorization import ReadOnlyAuthorization, Authorization
 from tastypie.constants import ALL
 from tastypie.exceptions import NotFound, ImmediateHttpResponse
 from tastypie.resources import ModelResource
@@ -78,6 +79,37 @@ class AppSerializer(Serializer):
 
     def format_time(self, data):
         return datetime.time.strftime(data, "%H:%M:%S")
+
+
+class SuperAuthorization(Authorization):
+    """
+    @summary: common authorization
+        create/update/delete: only superuser
+        read: all users
+    """
+
+    def is_superuser(self, bundle):
+        if bundle.request.user.is_superuser:
+            return True
+        raise ImmediateHttpResponse(HttpResponseForbidden('you have no permission to write common flows'))
+
+    def create_list(self, object_list, bundle):
+        return []
+
+    def create_detail(self, object_list, bundle):
+        return self.is_superuser(bundle)
+
+    def update_list(self, object_list, bundle):
+        return self.is_superuser(bundle)
+
+    def update_detail(self, object_list, bundle):
+        return self.is_superuser(bundle)
+
+    def delete_list(self, object_list, bundle):
+        return self.is_superuser(bundle)
+
+    def delete_detail(self, object_list, bundle):
+        return self.is_superuser(bundle)
 
 
 class GCloudReadOnlyAuthorization(ReadOnlyAuthorization):
