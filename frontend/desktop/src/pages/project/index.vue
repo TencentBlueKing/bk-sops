a3645cb7294b12668cca159489a15a7c31c9807a
840583540 <13206251194@163.com>
2019-07-26 15:16:24 +0800

    improve: 新建项目导航栏项目列表未同步刷新

--- frontend/desktop/src/pages/project/index.vue
+++ frontend/desktop/src/pages/project/index.vue
@@ -305,7 +305,7 @@
                     if (this.searchStr !== '') {
                         data.q = this.searchStr
                     }
-
+                    
                     const projectList = await this.loadProjectList(data)
                     this.projectList = projectList.objects || []
                     this.totalCount = projectList.meta.total_count
@@ -349,6 +349,7 @@
                     await this.createProject(data)
                     this.isProjectDialogShow = false
                     this.getProjectList()
+                    this.loadProjectList({ limit: 0 })
                 } catch (err) {
                     errorHandler(err, this)
                 } finally {
