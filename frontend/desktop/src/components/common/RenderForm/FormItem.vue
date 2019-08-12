9f6cd6e7376f325a055958546c1c7b6c0250de06
fannluo <fannluowork@gmail.com>
2019-08-12 17:49:23 +0800

    feature: 标准插件表单勾选到全局变量checkbox组件替换为magicbox组件

--- frontend/desktop/src/components/common/RenderForm/FormItem.vue
+++ frontend/desktop/src/components/common/RenderForm/FormItem.vue
@@ -57,16 +57,16 @@
             @onHide="onHideForm">
         </component>
         <div class="rf-tag-hook" v-if="showHook">
-            <BaseCheckbox
+            <bk-checkbox
                 v-bk-tooltips="{
                     content: hook ? i18n.hooked : i18n.cancelHook,
                     placements: ['left'],
                     customClass: 'offset-left-tooltip',
                     zIndex: 2002
                 }"
-                :is-checked="hook"
-                @checkCallback="onHookForm">
-            </BaseCheckbox>
+                :value="hook"
+                @change="onHookForm">
+            </bk-checkbox>
         </div>
     </div>
 </template>
@@ -74,7 +74,6 @@
     import '@/utils/i18n.js'
     import tools from '@/utils/tools.js'
     import { checkDataType } from '@/utils/checkDataType.js'
-    import BaseCheckbox from '@/components/common/base/BaseCheckbox.vue'
     import FormGroup from './FormGroup.vue'
 
     // 导入 tag 文件注册为组件
@@ -113,7 +112,6 @@
     export default {
         name: 'FormItem',
         components: {
-            BaseCheckbox,
             FormGroup
         },
         props: {
@@ -350,7 +348,7 @@
     }
     .rf-tag-hook {
         position: absolute;
-        top: 11px;
+        top: 8px;
         right: 0;
         z-index: 1;
     }
