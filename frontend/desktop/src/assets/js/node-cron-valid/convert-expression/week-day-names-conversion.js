93a9e72d3052dc8027b529a916461bb7a1bd0b5b
840583540 <13206251194@163.com>
2019-08-13 20:16:32 +0800

    improve:  周期表达式验证性能优化

--- frontend/desktop/src/assets/js/node-cron-valid/convert-expression/week-day-names-conversion.js
+++ frontend/desktop/src/assets/js/node-cron-valid/convert-expression/week-day-names-conversion.js
@@ -6,7 +6,7 @@ module.exports = (() => {
   var shortWeekDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
   function convertWeekDayName(expression, items){
-    for(var i = 0; i < items.length; i++){
+    for(const i in items){
       expression = expression.replace(new RegExp(items[i], 'gi'), parseInt(i, 10));
     }
     return expression;
