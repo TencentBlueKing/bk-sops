dcea06c147ca342578b395e8b384cdcff4d3eb1d
fannluo <fannluowork@gmail.com>
2019-08-20 10:29:00 +0800

    improvement: 无查看权限任务执行状态显示

--- frontend/desktop/src/pages/task/TaskList/index.vue
+++ frontend/desktop/src/pages/task/TaskList/index.vue
@@ -400,22 +400,17 @@
                     }
                     this.executeStatus = list.map((item, index) => {
                         const status = {}
-                        if (item.auth_actions.indexOf('view') === -1) {
-                            status.cls = ''
-                            status.text = '--'
-                        } else {
-                            if (item.is_finished) {
-                                status.cls = 'finished bk-icon icon-check-circle-shape'
-                                status.text = gettext('完成')
-                            } else if (item.is_started) {
-                                status.cls = 'loading common-icon-loading'
-                                this.getExecuteDetail(item, index)
-                            } else {
-                                status.cls = 'created common-icon-dark-circle-shape'
-                                status.text = gettext('未执行')
-                            }
-                        }
                         
+                        if (item.is_finished) {
+                            status.cls = 'finished bk-icon icon-check-circle-shape'
+                            status.text = gettext('完成')
+                        } else if (item.is_started) {
+                            status.cls = 'loading common-icon-loading'
+                            this.getExecuteDetail(item, index)
+                        } else {
+                            status.cls = 'created common-icon-dark-circle-shape'
+                            status.text = gettext('未执行')
+                        }
                         return status
                     })
                     this.setTaskListData(list)
