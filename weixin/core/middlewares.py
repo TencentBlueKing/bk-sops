2e97d7bb97b6e100484a8d95162b8f197d75ba2e
pagezhou <zongpeizhou@gmail.com>
2019-08-27 14:36:41 +0800

    bugfix: 修复直接返回401导致登录失败时500问题 #981

--- weixin/core/middlewares.py
+++ weixin/core/middlewares.py
@@ -35,12 +35,10 @@ def get_user(request):
         checked_user_id = WeixinUserSession.objects.check_session_key(session_key)
         if checked_user_id is False:
             logger.error("weixin user[session_key=%s] login status is expired" % session_key)
-            return HttpResponse(status=401)
         try:
-            user = BkWeixinUser.objects.get(pk=checked_user_id)
+            user = BkWeixinUser.objects.get(userid=checked_user_id)
         except BkWeixinUser.DoesNotExist:
             logger.error("weixin user[user_id=%s] does not exist" % checked_user_id)
-            return HttpResponse(status=401)
     return user or AnonymousUser()
 
 
