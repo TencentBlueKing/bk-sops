4f23b91b72a6b0dc11a10a278e3f0fc877c7a52e
homholueng <homholueng@gmail.com>
2019-09-17 17:40:08 +0800

    minor: merge V3.3.X pipeline

--- pipeline/service/pipeline_engine_adapter/adapter_api.py
+++ pipeline/service/pipeline_engine_adapter/adapter_api.py
@@ -13,6 +13,7 @@ specific language governing permissions and limitations under the License.
 
 from pipeline.engine import api
 from pipeline.constants import PIPELINE_DEFAULT_PRIORITY
+from pipeline.log.models import LogEntry
 
 STATE_MAP = {
     'CREATED': 'RUNNING',
@@ -126,10 +127,11 @@ def _get_parent_state_from_children_state(parent_state, children_state_list):
     @param children_state_list:
     @return:
     """
+    children_state_set = set(children_state_list)
     if parent_state == 'BLOCKED':
-        if 'RUNNING' in children_state_list:
+        if 'RUNNING' in children_state_set:
             parent_state = 'RUNNING'
-        if 'FAILED' in children_state_list:
+        if 'FAILED' in children_state_set:
             parent_state = 'FAILED'
     return parent_state
 
@@ -160,3 +162,7 @@ def _map(tree):
         'retry': tree['retry'],
         'skip': tree['skip']
     }
+
+
+def get_plain_log_for_node(node_id, history_id):
+    return LogEntry.objects.plain_log_for_node(node_id=node_id, history_id=history_id)
