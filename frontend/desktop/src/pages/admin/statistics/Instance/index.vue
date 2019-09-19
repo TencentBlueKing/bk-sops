78c450a90cad77fe39a80c34e6379fd9b9e271d2
fannluo <fannluowork@gmail.com>
2019-09-19 19:53:03 +0800

    improvement: 项目无权限跳转权限中心或项目管理页逻辑优化

--- frontend/desktop/src/pages/admin/statistics/Instance/index.vue
+++ frontend/desktop/src/pages/admin/statistics/Instance/index.vue
@@ -284,7 +284,7 @@
     import tools from '@/utils/tools.js'
     import DataStatistics from '../dataStatistics/index.vue'
     import VerticalBarChart from '../verticalBarChart/index.vue'
-    import { mapActions, mapState } from 'vuex'
+    import { mapActions, mapState, mapGetters } from 'vuex'
     import { AnalysisMixins } from '@/mixins/js/analysisMixins.js'
     import DataTablePagination from '@/components/common/dataTable/DataTablePagination.vue'
     import { errorHandler } from '@/utils/errorHandler.js'
@@ -483,8 +483,8 @@
                 categorys: state => state.categorys,
                 site_url: state => state.site_url
             }),
-            ...mapState('project', {
-                projectList: state => state.projectList
+            ...mapGetters('project', {
+                projectList: 'userCanViewProjects'
             }),
             allProjectList () {
                 if (this.projectList.length === 0) {
