a9eb092184982cc943b570672d0ae0d0c060d2a9
fannluo <fannluowork@gmail.com>
2019-09-20 09:27:19 +0800

    minor: 语法格式不通过eslint规则修复

--- frontend/desktop/src/utils/nodeFilter.js
+++ frontend/desktop/src/utils/nodeFilter.js
@@ -9,7 +9,8 @@
 * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 * specific language governing permissions and limitations under the License.
 */
-import { uuid } from './uuid.js';
+import { uuid } from './uuid.js'
+
 const nodeFilter = {
     isNodeExisted (type, data) {
         if (type && data) {
