e6bafce197a2c50e57b234b4b3acfaa10ef3e4bf
840583540 <13206251194@163.com>
2019-09-20 17:16:32 +0800

    improve: 选中导航【流程模版】，再次点击时控制台报错

--- frontend/desktop/src/components/layout/Navigator.vue
+++ frontend/desktop/src/components/layout/Navigator.vue
@@ -354,6 +354,7 @@
              */
             jumpToFirstPath (route) {
                 const firstPath = this.getPath(route.children[0])
+                if (this.$route.path === firstPath.path) return
                 this.$router.push(firstPath)
             }
         }
