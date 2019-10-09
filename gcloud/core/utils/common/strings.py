af6741ca2e482da1af206305b1e046ff8d38777f
pagezhou <zongpeizhou@gmail.com>
2019-10-09 16:14:53 +0800

    sprintfix: 修复数据统计后台问题 #1019

--- gcloud/core/utils/common/strings.py
+++ gcloud/core/utils/common/strings.py
@@ -11,12 +11,13 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+import json
 import re
 import logging
 
 from django.utils import six
 
-from gcloud.core.constant import TEMPLATE_NODE_NAME_MAX_LENGTH
+from gcloud.core.constant import TEMPLATE_NODE_NAME_MAX_LENGTH, AE
 
 logger = logging.getLogger("root")
 
@@ -64,3 +65,39 @@ def camel_case_to_underscore_naming(source):
             else:
                 result += s
     return result
+
+
+def check_and_rename_params(conditions, group_by, group_by_check=AE.group_list):
+    """
+    检验参数是否正确
+    :param conditions:参数是一个dict
+    :param group_by:分组凭据
+    :param group_by_check:分组检查内容
+    :return:
+    """
+    # conditions 是否是一个dict.
+    # 本地测试时请注释该try
+    result_dict = {'success': False, 'content': None, "conditions": conditions, "group_by": None}
+    try:
+        conditions = json.loads(conditions)
+    except Exception:
+        message = u"param conditions[%s] cannot be converted to dict" % conditions
+        logger.error(message)
+        result_dict['content'] = message
+        return result_dict
+    if not isinstance(conditions, dict):
+        message = u"params conditions[%s] are invalid dict data" % conditions
+        logger.error(message)
+        result_dict['content'] = message
+        return result_dict
+    # 检查传递分组是否有误
+    if group_by not in group_by_check:
+        message = u"params group_by[%s] is invalid" % group_by
+        logger.error(message)
+        result_dict['content'] = message
+        return result_dict
+
+    result_dict['success'] = True
+    result_dict['group_by'] = group_by
+    result_dict['conditions'] = conditions
+    return result_dict
