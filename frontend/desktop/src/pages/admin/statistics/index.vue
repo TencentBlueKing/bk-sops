f76979b130737c58d877d5042f6e6a0a0d22035f
fannluo <fannluowork@gmail.com>
2019-10-21 17:37:44 +0800

    feature: 数据统计前端代码整理

--- frontend/desktop/src/pages/admin/statistics/index.vue
+++ frontend/desktop/src/pages/admin/statistics/index.vue
@@ -11,43 +11,29 @@
 */
 <template>
     <div class="page-statistics">
-        <div class="header-wrapper">
-            <div class="nav-content clearfix">
-                <div class="nav-title">
-                    <h3>{{i18n.operationData}}</h3>
-                </div>
-                <div class="data-dimension">
-                    <router-link
-                        v-for="dms in dataDimension"
-                        :key="dms.name"
-                        :class="['dms-item', { 'active': $route.name === dms.name }]"
-                        :to="dms.path">
-                        {{dms.text}}
-                    </router-link>
-                </div>
-                <div class="nav-date-range">
-                    <label class="content-detail-label">{{i18n.timeLimit}}</label>
-                    <bk-date-picker
-                        v-model="timeRange"
-                        :clearable="false"
-                        type="daterange"
-                        placement="top-end"
-                        @change="onChangeTimeRange">
-                    </bk-date-picker>
-                </div>
-            </div>
-        </div>
+        <navi-header
+            :routers="routers"
+            :title="i18n.title"
+            :show-date-picker="true"
+            :date-range="defaultDateRange"
+            @changeDateRange="changeDateRange">
+        </navi-header>
         <div class="statistics-content">
-            <div class="content-wrapper" v-if="reloadComponent">
-                <router-view :time-range="timeRange"></router-view>
-            </div>
+            <router-view
+                :date-range="dateStamp"
+                :project-list="projectList"
+                :category-list="categoryList">
+            </router-view>
         </div>
     </div>
 </template>
 <script>
+    import moment from 'moment'
+    import { mapActions, mapState } from 'vuex'
     import '@/utils/i18n.js'
-    import moment from 'moment-timezone'
-    const DATA_DIMENSION = [
+    import NaviHeader from '../common/NaviHeader.vue'
+
+    const ROUTERS = [
         {
             text: gettext('流程统计'),
             name: 'statisticsTemplate',
@@ -71,108 +57,87 @@
     ]
     export default {
         name: 'Statistics',
+        components: {
+            NaviHeader
+        },
         data () {
+            const format = 'YYYY-MM-DD'
+            const defaultDateRange = [moment().subtract(1, 'month').format(format), moment().format(format)]
             return {
-                dataDimension: DATA_DIMENSION,
+                routers: ROUTERS,
+                defaultDateRange,
+                dateRange: defaultDateRange.slice(0),
                 i18n: {
-                    operationData: gettext('运营数据'),
-                    timeLimit: gettext('时间范围')
-                },
-                path: this.$router.currentRoute.path,
-                reloadComponent: true,
-                timeRange: ['', '']
+                    title: gettext('运营数据')
+                }
             }
         },
-        watch: {
-            '$route' (to, from) {
-                this.reloadComponent = false
-                this.path = this.$router.currentRoute.path
-                this.reloadComponent = true
+        computed: {
+            ...mapState({
+                categorys: state => state.categorys
+            }),
+            ...mapState('project', {
+                projectList: state => state.projectList
+            }),
+            categoryList () {
+                return this.categorys.map(item => {
+                    return {
+                        id: item.value,
+                        name: item.name
+                    }
+                })
+            },
+            dateStamp () {
+                return this.dateRange.map(item => moment(item).valueOf())
             }
         },
         created () {
-            this.getTimeRange()
+            this.getCategorys()
         },
         methods: {
-            onGotoPath (path) {
-                this.$router.push(path)
-            },
-            onChangeTimeRange (value) {
-                this.timeRange = value
-            },
-            getTimeRange () {
-                const date = new Date()
-                this.timeRange[1] = moment(date).format('YYYY-MM-DD HH:mm:ss')
-                date.setTime(date.getTime() - 3600 * 1000 * 24 * 30)
-                this.timeRange[0] = moment(date).format('YYYY-MM-DD HH:mm:ss')
+            ...mapActions([
+                'getCategorys'
+            ]),
+            changeDateRange (dateRange) {
+                this.dateRange = dateRange
             }
         }
     }
 </script>
-<style lang="scss">
-@import "@/scss/config.scss";
-@import "@/scss/datastatistics/datastatistics.scss";
-.page-statistics {
-    .header-wrapper {
-        margin-bottom: 20px;
-        padding: 0px 60px 0 60px;
+<style lang="scss" scoped>
+    .page-statistics {
         min-width: 1320px;
-    }
-    .nav-content {
-        border-bottom: 1px solid #dde4eb;
-        .nav-title {
-            float: left;
-            margin-right: 20px;
-            padding: 20px 0;
-            h3 {
-                margin: 0;
-                padding: 0 25px 0 12px;
-                border-right: 1px solid #c4c6cc;
-                border-left: 2px solid #a3c5fd;
-                height: 20px;
-                line-height: 20px;
-                font-size: 14px;
-                font-weight: bold;
-                color: #313238;
+        height: 100%;
+        background: #f4f7fa;
+        .header-wrapper {
+            margin: 0 60px 0;
+        }
+        .statistics-content {
+            padding: 20px 60px 60px;
+        }
+        /deep/ .statistics-select {
+            width: 250px;
+        }
+        /deep/ .bar-chart-area {
+            display: flex;
+            justify-content: space-between;
+            .horizontal-bar-chart {
+                width: 49%;
             }
         }
-        .data-dimension {
-            float: left;
-            .dms-item {
-                display: inline-block;
-                margin-right: 34px;
-                height: 60px;
-                line-height: 60px;
-                font-size: 14px;
-                color: #63656E;
-                cursor: pointer;
-                &:hover {
-                    color: #3a84ff;
-                }
-                &.active {
-                    color: #3a84ff;
-                    border-bottom: 2px solid #3a84ff;
-                }
+        /deep/ .tab-content-area {
+            margin-top: 20px;
+            border-radius: 2px;
+            box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.06);
+            .bk-tab-section {
+                background: #ffffff;
             }
-        }
-        .nav-date-range {
-            font-size: 14px;
-            float: right;
-            margin: 15px 0 0 0;
-            .bk-date-picker {
-                width: 201px;
-                margin-left: 20px;
+            .tab-data-table {
+                margin-top: 20px;
+            }
+            .table-link {
+                color: #3a84ff;
             }
         }
     }
-    .statistics-content {
-        padding: 0 60px;
-        .template-router {
-            color: #3a84ff;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-        }
-    }
-}
 </style>
