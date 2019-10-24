492f078515f7235a6833948256ddcec37655742f
fannluo <fannluowork@gmail.com>
2019-10-24 10:24:46 +0800

    improvement: 数据统计content-type变更，任务统计表格合并，新增表格字段排序

--- frontend/desktop/src/pages/admin/statistics/Instance.vue
+++ frontend/desktop/src/pages/admin/statistics/Instance.vue
@@ -37,40 +37,44 @@
             </vertical-bar-chart>
         </div>
         <div class="tab-content-area">
-            <bk-tab :active.sync="activeTab" @tab-change="onTabChange">
-                <bk-form form-type="inline">
-                    <bk-form-item :label="i18n.projectBeLongTo">
-                        <bk-select
-                            v-model="instanceProject"
-                            class="statistics-select"
-                            :placeholder="i18n.selectProject"
-                            :disabled="projectList.length === 0"
-                            @change="instanceFilterChange">
-                            <bk-option
-                                v-for="option in projectList"
-                                :key="option.id"
-                                :name="option.name"
-                                :id="option.id">
-                            </bk-option>
-                        </bk-select>
-                    </bk-form-item>
-                    <bk-form-item :label="i18n.categoryBeLongTo">
-                        <bk-select
-                            v-model="instanceCategory"
-                            class="statistics-select"
-                            :placeholder="i18n.selectCategory"
-                            :disabled="categoryList.length === 0"
-                            @change="instanceFilterChange">
-                            <bk-option
-                                v-for="option in categoryList"
-                                :key="option.id"
-                                :name="option.name"
-                                :id="option.id">
-                            </bk-option>
-                        </bk-select>
-                    </bk-form-item>
-                </bk-form>
-                <bk-tab-panel v-for="tab in tabs" :key="tab.id" v-bind="{ name: tab.id, label: tab.name }">
+            <bk-tab>
+                <bk-tab-panel v-bind="{ name: 'instance', label: i18n.instanceTitle }">
+                    <bk-form form-type="inline">
+                        <bk-form-item :label="i18n.projectBeLongTo">
+                            <bk-select
+                                v-model="instanceProject"
+                                class="statistics-select"
+                                :placeholder="i18n.selectProject"
+                                :searchable="true"
+                                :clearable="true"
+                                :disabled="projectList.length === 0"
+                                @change="instanceFilterChange">
+                                <bk-option
+                                    v-for="option in projectList"
+                                    :key="option.id"
+                                    :name="option.name"
+                                    :id="option.id">
+                                </bk-option>
+                            </bk-select>
+                        </bk-form-item>
+                        <bk-form-item :label="i18n.categoryBeLongTo">
+                            <bk-select
+                                v-model="instanceCategory"
+                                class="statistics-select"
+                                :placeholder="i18n.selectCategory"
+                                :searchable="true"
+                                :clearable="true"
+                                :disabled="categoryList.length === 0"
+                                @change="instanceFilterChange">
+                                <bk-option
+                                    v-for="option in categoryList"
+                                    :key="option.id"
+                                    :name="option.name"
+                                    :id="option.id">
+                                </bk-option>
+                            </bk-select>
+                        </bk-form-item>
+                    </bk-form>
                     <bk-table
                         class="tab-data-table"
                         v-bkloading="{ isLoading: instanceDataLoading, opacity: 1 }"
@@ -79,7 +83,7 @@
                         @sort-change="handleSortChange"
                         @page-change="handlePageChange">
                         <bk-table-column
-                            v-for="item in tableColumn[activeTab]"
+                            v-for="item in tableColumn"
                             :key="item.prop"
                             :label="item.label"
                             :prop="item.prop"
@@ -151,91 +155,54 @@
             clearable: false
         }
     ]
-    
-    const TABS = [
+
+    const TABLE_COLUMN = [
         {
-            id: 'instance_details',
-            name: gettext('任务详情')
+            label: gettext('任务ID'),
+            prop: 'intanceId',
+            sortable: true
         },
         {
-            id: 'instance_node',
-            name: gettext('归档任务耗时')
+            label: gettext('任务名称'),
+            prop: 'instanceName'
+        },
+        {
+            label: gettext('项目'),
+            prop: 'projectName'
+        },
+        {
+            label: gettext('分类'),
+            prop: 'category'
+        },
+        {
+            label: gettext('创建人'),
+            prop: 'creator'
+        },
+        {
+            label: gettext('创建时间'),
+            prop: 'createTime'
+        },
+        {
+            label: gettext('标准插件数'),
+            prop: 'atomTotal',
+            sortable: true
+        },
+        {
+            label: gettext('子流程数'),
+            prop: 'subprocessTotal',
+            sortable: true
+        },
+        {
+            label: gettext('网关数'),
+            prop: 'gatewaysTotal',
+            sortable: true
+        },
+        {
+            label: gettext('耗时'),
+            prop: 'executeTime',
+            sortable: true
         }
     ]
-
-    const TABLE_COLUMN = {
-        instance_details: [
-            {
-                label: gettext('任务ID'),
-                prop: 'intanceId',
-                sortable: true
-            },
-            {
-                label: gettext('任务名称'),
-                prop: 'instanceName'
-            },
-            {
-                label: gettext('所属项目'),
-                prop: 'projectName'
-            },
-            {
-                label: gettext('分类'),
-                prop: 'category'
-            },
-            {
-                label: gettext('创建人'),
-                prop: 'creator'
-            },
-            {
-                label: gettext('创建时间'),
-                prop: 'createTime'
-            },
-            {
-                label: gettext('标准插件数'),
-                prop: 'atomTotal',
-                sortable: true
-            },
-            {
-                label: gettext('子流程数'),
-                prop: 'subprocessTotal'
-            },
-            {
-                label: gettext('网关数'),
-                prop: 'gatewaysTotal'
-            }
-        ],
-        instance_node: [
-            {
-                label: gettext('任务ID'),
-                prop: 'intanceId',
-                sortable: true
-            },
-            {
-                label: gettext('任务名称'),
-                prop: 'instanceName'
-            },
-            {
-                label: gettext('所属项目'),
-                prop: 'projectName'
-            },
-            {
-                label: gettext('分类'),
-                prop: 'category'
-            },
-            {
-                label: gettext('创建人'),
-                prop: 'creator'
-            },
-            {
-                label: gettext('创建时间'),
-                prop: 'createTime'
-            },
-            {
-                label: gettext('耗时'),
-                prop: 'executeTime'
-            }
-        ]
-    }
 
     export default {
         name: 'StatisticsIntance',
@@ -300,8 +267,6 @@
                 timeDataType: SELECTORS[2].options[0].id,
                 timeDataList: [],
                 timeDataLoading: true,
-                tabs: TABS,
-                activeTab: TABS[0].id,
                 instanceData: [],
                 instanceProject: '',
                 instanceCategory: '',
@@ -319,7 +284,7 @@
                     categoryTitle: gettext('分类统计'),
                     projectTitle: gettext('分项目统计'),
                     timeTitle: gettext('分时间统计'),
-                    templateTitle: gettext('流程详情'),
+                    instanceTitle: gettext('任务详情'),
                     projectBeLongTo: gettext('所属项目'),
                     categoryBeLongTo: gettext('所属分类'),
                     selectProject: gettext('请选择项目'),
@@ -379,11 +344,11 @@
                     this.categoryDataLoading = true
                     const query = {
                         group_by: 'category',
-                        conditions: JSON.stringify({
+                        conditions: {
                             create_time: this.dateRange[0],
                             finish_time: this.dateRange[1],
                             project_id: this.categoryDataProject
-                        })
+                        }
                     }
                     this.categoryData = await this.loadAnalysisData(query)
                 } catch (e) {
@@ -397,11 +362,11 @@
                     this.projectDataLoading = true
                     const query = {
                         group_by: 'project_id',
-                        conditions: JSON.stringify({
+                        conditions: {
                             create_time: this.dateRange[0],
                             finish_time: this.dateRange[1],
                             category: this.projectDataCategory
-                        })
+                        }
                     }
                     this.projectData = await this.loadAnalysisData(query)
                 } catch (e) {
@@ -415,13 +380,13 @@
                     this.timeDataLoading = true
                     const query = {
                         group_by: 'instance_time',
-                        conditions: JSON.stringify({
+                        conditions: {
                             create_time: this.dateRange[0],
                             finish_time: this.dateRange[1],
                             project: this.timeDataProject,
                             category: this.timeDataCategory,
                             type: this.timeDataType
-                        })
+                        }
                     }
                     this.timeDataList = await this.loadAnalysisData(query)
                 } catch (e) {
@@ -434,14 +399,14 @@
                 try {
                     this.instanceDataLoading = true
                     const query = {
-                        group_by: this.activeTab,
-                        conditions: JSON.stringify({
+                        group_by: 'instance_node',
+                        conditions: {
                             create_time: this.dateRange[0],
                             finish_time: this.dateRange[1],
                             project_id: this.instanceProject,
                             category: this.instanceCategory,
                             order_by: this.instanceSort
-                        }),
+                        },
                         pageIndex: this.pagination.current,
                         limit: this.pagination.limit
                     }
