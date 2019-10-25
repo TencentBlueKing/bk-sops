c23e59fd236faaa255c95dc0855f4561125e443b
pagezhou <zongpeizhou@gmail.com>
2019-10-25 16:22:42 +0800

    minor: 修复单元测试问题

--- gcloud/tests/apigw/test_apigw.py
+++ gcloud/tests/apigw/test_apigw.py
@@ -335,6 +335,7 @@ class APITest(TestCase):
                     category=tmpl.category,
                     pipeline_instance=TEST_DATA,
                     template_id=TEST_TEMPLATE_ID,
+                    template_source='business',
                     create_method='api',
                     create_info=TEST_APP_CODE,
                     flow_type='common',
