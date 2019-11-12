669c20c3cfe747bbed7a0db58f0207327dfbb015
840583540 <13206251194@163.com>
2019-11-12 22:19:53 +0800

    bugfix: 修复单击选中的节点，复制时位置不对

--- frontend/desktop/src/components/common/TemplateCanvas/index.vue
+++ frontend/desktop/src/components/common/TemplateCanvas/index.vue
@@ -745,20 +745,42 @@
             onCloseHotkeyInfo () {
                 this.isShowHotKey = false
             },
+            /**
+             * 单个添加选中节点
+             */
             addNodesToDragSelection (selectedNode) {
                 this.selectedNodes.push(selectedNode)
                 this.copyNodes.push(selectedNode)
                 const ids = this.selectedNodes.map(m => m.id)
                 this.$refs.jsFlow.addNodesToDragSelection(ids)
+                // 重新计算粘贴相对位置
+                this.selectionOriginPos = this.getNodesLocationOnLeftTop(this.selectedNodes)
                 document.addEventListener('keydown', this.nodeSelectedhandler)
                 document.addEventListener('mousedown', this.handleClearDragSelection, { once: true })
+                this.$refs.jsFlow.$el.addEventListener('mousemove', this.pasteMousePosHandler)
             },
+            /**
+             * 失焦时移出选中节点
+             */
             handleClearDragSelection () {
                 this.selectedNodes = []
                 this.copyNodes = []
                 this.$refs.jsFlow.clearNodesDragSelection()
                 document.removeEventListener('mousedown', this.handleClearDragSelection, { once: true })
                 document.removeEventListener('keydown', this.nodeSelectedhandler)
+                this.$refs.jsFlow.$el.removeEventListener('mousemove', this.pasteMousePosHandler)
+            },
+            /**
+             * 获取节点组里，相对画布靠左上角的点位置
+             */
+            getNodesLocationOnLeftTop (nodes) {
+                let x = 0
+                let y = 0
+                nodes.forEach((node, index) => {
+                    x = index === 0 ? node.x : Math.min(x, node.x)
+                    y = index === 0 ? node.y : Math.min(y, node.y)
+                })
+                return { x, y }
             },
             // 更新分支条件数据
             updataConditionCanvasData (data) {
