6744599bb18a2b93b61866de0467d37f42eb0856
fannluo <fannluowork@gmail.com>
2019-11-12 09:16:15 +0800

    improvement: 菜单栏开始、结束节点文字垂直居中

--- frontend/desktop/src/components/common/TemplateCanvas/PalettePanel/index.vue
+++ frontend/desktop/src/components/common/TemplateCanvas/PalettePanel/index.vue
@@ -220,11 +220,13 @@
             }
         }
         .node-type-text {
+            display: flex;
             margin: 0 auto;
             font-size: 12px;
             width: 32px;
             height: 32px;
-            line-height: 32px;
+            justify-content: center;
+            align-items: center;
             color: #52699d;
             border: 1px solid #546a9e;
             border-radius: 50%;
