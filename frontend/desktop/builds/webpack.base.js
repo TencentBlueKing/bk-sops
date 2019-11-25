b400b700d14b945c277059442050886b8d68431e
fannluo <fannluowork@gmail.com>
2019-11-25 15:49:02 +0800

    improvement：前端打包静态资源添加版本号

--- frontend/desktop/builds/webpack.base.js
+++ frontend/desktop/builds/webpack.base.js
@@ -17,10 +17,11 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 /**
  * 生产环境分版本打包命令
- * npm run build -- --SITE_URL="/o/bk_sops" --STATIC_ENV="open/prod"
+ * npm run build -- --SITE_URL="/o/bk_sops" --STATIC_ENV="open/prod" --VERSION="V3.5.3"
  */
 let SITE_URL = ''
 let STATIC_ENV = ''
+let VERSION = ''
 
 process.argv.forEach(val => {
     if (/--SITE_URL=/.test(val)) {
@@ -29,15 +30,20 @@ process.argv.forEach(val => {
     if (/--STATIC_ENV=/.test(val)) {
         STATIC_ENV = val.replace(/--STATIC_ENV=/, '')
     }
+    if (/--VERSION=/.test(val)) {
+        VERSION = val.replace(/--VERSION=/, '')
+    }
 })
 
 process.env.STATIC_ENV = STATIC_ENV
+process.env.VERSION = VERSION ? '.' + VERSION : ''
 
 const publicPath = path.posix.join(SITE_URL, '/static/')
 
 console.log('build mode:', process.env.NODE_ENV)
 console.log('SITE_URL:', SITE_URL)
 console.log('publicPath:', publicPath)
+console.log('version:', VERSION)
 
 module.exports = {
     entry: {
@@ -47,7 +53,7 @@ module.exports = {
         path: path.join(__dirname, '../static'),
         publicPath: publicPath,
         pathinfo: true,
-        filename: path.posix.join(process.env.STATIC_ENV, 'dist/js/[name].js')
+        filename: path.posix.join(process.env.STATIC_ENV, 'dist/js/[name]' + process.env.VERSION + '.js')
     },
     module: {
         rules: [
@@ -120,7 +126,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit: 10000,
-                    name: path.posix.join(process.env.STATIC_ENV, 'dist/videos/[name].[ext]')
+                    name: path.posix.join(process.env.STATIC_ENV, 'dist/videos/[name]'+ process.env.VERSION + '.[ext]')
                 }
             },
             {
@@ -132,7 +138,7 @@ module.exports = {
                 ],
                 options: {
                     limit: 10000,
-                    name: path.posix.join(process.env.STATIC_ENV, 'dist/fonts/[name].[ext]')
+                    name: path.posix.join(process.env.STATIC_ENV, 'dist/fonts/[name]'+ process.env.VERSION + '.[ext]')
                 }
             }
         ]
@@ -141,7 +147,7 @@ module.exports = {
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
             template: './src/assets/html/template.html',
-            filename: path.posix.join(process.env.STATIC_ENV, 'dist/index.html')
+            filename: path.posix.join(process.env.STATIC_ENV, 'dist/index' + process.env.VERSION + '.html')
         }),
         new webpack.ProvidePlugin({
             $: 'jquery',
