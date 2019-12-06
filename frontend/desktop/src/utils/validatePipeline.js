a7fd021595ff83e096ee22ae9e9af0c913989394
fannluo <fannluowork@gmail.com>
2019-12-06 14:22:27 +0800

    bugfix:流程节点校验规则回滚

--- frontend/desktop/src/utils/validatePipeline.js
+++ frontend/desktop/src/utils/validatePipeline.js
@@ -18,12 +18,12 @@ const NODE_RULE = {
         max_in: 0,
         min_out: 1,
         max_out: 1,
-        allowed_out: ['tasknode', 'branchgateway', 'parallelgateway', 'subflow'],
+        allowed_out: ['tasknode', 'branchgateway', 'parallelgateway', 'subflow', 'endpoint'],
         unique: true
     },
     'endpoint': {
         min_in: 1,
-        max_in: 1000,
+        max_in: 1,
         min_out: 0,
         max_out: 0,
         allowed_out: [],
@@ -31,7 +31,7 @@ const NODE_RULE = {
     },
     'tasknode': {
         min_in: 1,
-        max_in: 1000,
+        max_in: 1,
         min_out: 1,
         max_out: 1,
         allowed_out: ['tasknode', 'subflow', 'branchgateway', 'parallelgateway', 'convergegateway', 'endpoint'],
@@ -39,7 +39,7 @@ const NODE_RULE = {
     },
     'subflow': {
         min_in: 1,
-        max_in: 1000,
+        max_in: 1,
         min_out: 1,
         max_out: 1,
         allowed_out: ['tasknode', 'subflow', 'branchgateway', 'parallelgateway', 'convergegateway', 'endpoint'],
@@ -47,22 +47,22 @@ const NODE_RULE = {
     },
     'branchgateway': {
         min_in: 1,
-        max_in: 1000,
-        min_out: 1,
+        max_in: 1,
+        min_out: 2,
         max_out: 1000,
         allowed_out: ['tasknode', 'subflow', 'branchgateway', 'parallelgateway', 'convergegateway'],
         unique: false
     },
     'parallelgateway': {
         min_in: 1,
-        max_in: 1000,
-        min_out: 1,
+        max_in: 1,
+        min_out: 2,
         max_out: 1000,
         allowed_out: ['tasknode', 'subflow', 'branchgateway', 'parallelgateway', 'convergegateway'],
         unique: false
     },
     'convergegateway': {
-        min_in: 1,
+        min_in: 2,
         max_in: 1000,
         min_out: 1,
         max_out: 1,
@@ -172,6 +172,8 @@ const validatePipeline = {
      */
     isNodeLineNumValid (data) {
         let message
+        let branchAndParallelGateways = 0
+        let convergegateways = 0
         let tasknode = 0
         let subflow = 0
         const isLineNumValid = data.locations.every(loc => {
@@ -179,7 +181,11 @@ const validatePipeline = {
             const name = loc.name || NODE_DICT[loc.type]
             let sourceLinesLinked = 0
             let targetLinesLinked = 0
-            if (loc.type === 'tasknode') {
+            if (loc.type === 'branchgateway' || loc.type === 'parallelgateway') {
+                branchAndParallelGateways += 1
+            } else if (loc.type === 'convergegateway') {
+                convergegateways += 1
+            } else if (loc.type === 'tasknode') {
                 tasknode += 1
             } else if (loc.type === 'subflow') {
                 subflow += 1
@@ -212,6 +218,11 @@ const validatePipeline = {
 
         if ((tasknode + subflow) === 0) {
             message = gettext('请添加任务节点')
+            return this.getMessage(false, message)
+        }
+
+        if (branchAndParallelGateways !== convergegateways) {
+            message = gettext('并行网关、分支网关个数和汇聚网关个数必须一致，并且必须配对使用')
             return this.getMessage(false, message)
         }
 
