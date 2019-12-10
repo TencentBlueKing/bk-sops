5cfa1608d1c21d3b6bd38a5606ffe41e78b57623
fannluo <fannluowork@gmail.com>
2019-12-10 17:31:45 +0800

    improvement: 前端弹窗登录续期(V3.4.X)

--- frontend/desktop/src/App.vue
+++ frontend/desktop/src/App.vue
@@ -15,7 +15,6 @@
         <div class="main-container" v-bkloading="{ isloading: adminPermLoading, opacity: 1 }">
             <router-view v-if="isRouterViewShow"></router-view>
         </div>
-        <UserLoginModal ref="userLogin"></UserLoginModal>
         <ErrorCodeModal ref="errorModal"></ErrorCodeModal>
         <PermissionModal ref="permissionModal"></PermissionModal>
         <permissionApply
@@ -32,7 +31,6 @@
     import { errorHandler } from '@/utils/errorHandler.js'
     import { setConfigContext } from '@/config/setting.js'
     import permission from '@/mixins/permission.js'
-    import UserLoginModal from '@/components/common/modal/UserLoginModal.vue'
     import ErrorCodeModal from '@/components/common/modal/ErrorCodeModal.vue'
     import PermissionModal from '@/components/common/modal/PermissionModal.vue'
     import Navigator from '@/components/layout/Navigator.vue'
@@ -42,7 +40,6 @@
         name: 'App',
         components: {
             Navigator,
-            UserLoginModal,
             ErrorCodeModal,
             permissionApply,
             PermissionModal
