481a42315c8cfb15b38663339af5d8c03d67dd75
homholueng <homholueng@gmail.com>
2019-12-11 14:25:44 +0800

    minor: i18n extractor 恢复

--- config/default.py
+++ config/default.py
@@ -181,11 +181,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 # 程序会自动分析访客使用的语言，来显示相应的翻译结果
 LOCALEURL_USE_ACCEPT_LANGUAGE = True
 # 界面可选语言
-
-
-def _(s): return s  # noqa
-
-
+_ = lambda s: s  # noqa
 LANGUAGES = (
     ('en', _('English')),
     ('zh-hans', _('简体中文')),
