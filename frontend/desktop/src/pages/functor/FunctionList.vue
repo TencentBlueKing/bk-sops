935ad048d2634e3a3470a1527e2b6eb1122ae05b
840583540 <13206251194@163.com>
2020-02-12 09:25:33 +0800

    bugfix: 职能化高级搜索所属项目字段错误修复

--- frontend/desktop/src/pages/functor/FunctionList.vue
+++ frontend/desktop/src/pages/functor/FunctionList.vue
@@ -401,7 +401,7 @@
                         offset: (this.pagination.current - 1) * this.pagination.limit,
                         task__pipeline_instance__name__contains: flowName || undefined,
                         creator: creator || undefined,
-                        project__id: selectedProject || undefined,
+                        task__project__id: selectedProject || undefined,
                         status: statusSync || undefined
                     }
                     if (executeTime[0] && executeTime[1]) {
