1394fc434f64ff151a7695978b6e1f50ba8e4784
homholueng <homholueng@gmail.com>
2020-03-04 17:58:58 +0800

    bugfix: 修复全新部署时 DB migrate 失败的问题

--- data_migration/management/commands/migrate.py
+++ data_migration/management/commands/migrate.py
@@ -11,13 +11,13 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+import os
 import sys
 
 from django.conf import settings
 from django.db import connections
 from django.core.management.commands import migrate
 from django.db.migrations.recorder import MigrationRecorder
-from django.utils.module_loading import import_module
 
 
 class Command(migrate.Command):
@@ -27,20 +27,34 @@ class Command(migrate.Command):
         db = options['database']
         connection = connections[db]
 
-        is_auth_finish = False
-
         recorder = MigrationRecorder(connection)
         applied = recorder.applied_migrations()
-        for migration in applied:
 
-            if migration[0] == 'auth' and migration[1].startswith('0007'):
-                is_auth_finish = True
+        if applied:
+            is_auth_finish = False
+            target_init_path = os.path.join(
+                settings.BASE_DIR, 'blueapps', 'account', 'migrations', '__init__.py')
+            target_init_pyc_path = os.path.join(
+                settings.BASE_DIR, 'blueapps', 'account', 'migrations', '__init__.pyc')
 
-        if settings.RUN_VER == 'open' and not is_auth_finish:
-            sys.stdout.write('modify blueapps.account.0001_initial dependency.\n')
-            first_migration_module = import_module("blueapps.account.migrations.0001_initial")
-            first_migration_module.dependency = [
-                ('auth', '0006_require_contenttypes_0002'),
-            ]
+            for migration in applied:
+
+                if migration[0] == 'auth' and migration[1].startswith('0007'):
+                    is_auth_finish = True
+
+            if settings.RUN_VER == 'open' and not is_auth_finish:
+                sys.stdout.write('remove init for auth is not finish.\n')
+                try:
+                    os.remove(target_init_path)
+                    os.remove(target_init_pyc_path)
+                except FileNotFoundError:
+                    pass
+
+                sys.stdout.write('ready to execute the true migrate\n')
+                super(Command, self).handle(*args, **options)
+
+                if not is_auth_finish:
+                    open(target_init_path, 'w')
+                    sys.stdout.write('ready to execute the true migrate again\n')
 
         super(Command, self).handle(*args, **options)
