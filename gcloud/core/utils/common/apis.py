ff059acf33a3632fae809082f617970dceb377c3
homholueng <homholueng@gmail.com>
2020-03-12 14:43:17 +0800

    minor: BK_IAM_APPLY_URL_QUERY_URL format optimization

--- gcloud/core/utils/common/apis.py
+++ gcloud/core/utils/common/apis.py
@@ -17,12 +17,7 @@ from pipeline.utils.http import http_post_request
 
 from gcloud.conf import settings
 
-if settings.BK_IAM_HOST:
-    bk_iam_host = settings.BK_IAM_HOST
-else:
-    bk_iam_host = '{}/o/{}/'.format(settings.BK_PAAS_HOST, settings.BK_IAM_APP_CODE)
-
-BK_IAM_APPLY_URL_QUERY_URL = '{}api/v1/apply-permission/url/'.format(bk_iam_host)
+BK_IAM_APPLY_URL_QUERY_URL = '{}/api/v1/apply-permission/url/'.format(settings.BK_IAM_HOST)
 
 
 def apply_permission_url(permission):
