be298651ceb705b0bbd5d846d20eb80ba7761c71
840583540 <13206251194@163.com>
2020-03-16 19:34:24 +0800

    improve: 全局变量控制台报错

--- frontend/desktop/src/store/modules/template.js
+++ frontend/desktop/src/store/modules/template.js
@@ -746,18 +746,33 @@ const template = {
             const codeReg = /\$\{[0-9a-zA-Z\_\.]*\}/g
             if (state.activities[nodeId]) {
                 const item = state.activities[nodeId]
-                const nodeData = item.component.data
-                if (checkDataType(nodeData) === 'Object') {
-                    for (const code in nodeData) {
-                        const value = nodeData[code].value
-                        const matchArr = checkDataType(value) === 'String' ? value.match(codeReg) || [] : []
-                        matchArr.forEach(matchItem => {
-                            if (constantsCited[matchItem]) {
-                                constantsCited[matchItem] += 1
-                            } else {
-                                constantsCited[matchItem] = 1
+                if (item.type === 'ServiceActivity') {
+                    const nodeData = item.component.data
+                    if (checkDataType(nodeData) === 'Object') {
+                        for (const code in nodeData) {
+                            const value = nodeData[code].value
+                            const matchArr = checkDataType(value) === 'String' ? value.match(codeReg) || [] : []
+                            matchArr.forEach(matchItem => {
+                                if (constantsCited[matchItem]) {
+                                    constantsCited[matchItem] += 1
+                                } else {
+                                    constantsCited[matchItem] = 1
+                                }
+                            })
+                        }
+                    }
+                } else {
+                    const nodeData = item.constants
+                    if (checkDataType(nodeData) === 'Object') {
+                        for (const varKey in nodeData) {
+                            if (varKey in state.constants) {
+                                if (constantsCited[varKey]) {
+                                    constantsCited[varKey] += 1
+                                } else {
+                                    constantsCited[varKey] = 1
+                                }
                             }
-                        })
+                        }
                     }
                 }
             }
