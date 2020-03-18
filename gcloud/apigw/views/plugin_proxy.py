da452bbce3fc217b8cb81ab4a4a2d347348521e0
gmaclinuxer <gmaclinuxer@gmail.com>
2020-03-18 11:43:51 +0800

    feature: commit test

--- gcloud/apigw/views/plugin_proxy.py
+++ gcloud/apigw/views/plugin_proxy.py
@@ -35,7 +35,8 @@ except ImportError:
 @apigw_required
 @mark_request_whether_is_trust
 def dispatch_plugin_query(request):
-    """转发插件表单渲染资源请求，暂时仅考虑GET/POST请求
+    """
+        转发插件表单渲染资源请求，暂时仅考虑GET/POST请求
         body = {
             "url": 被转发资源请求url, 比如：/pipeline/job_get_script_list/4/?type=public
             "method": 'GET|POST',
