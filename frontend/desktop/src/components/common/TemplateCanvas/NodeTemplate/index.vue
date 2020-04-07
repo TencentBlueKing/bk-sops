ebd4f206570163ed28ec949a4caacfef4cc7d6f1
fannluo <fannluowork@gmail.com>
2020-04-07 21:24:13 +0800

    improvement: 节点删除icon增加白色背景

--- frontend/desktop/src/components/common/TemplateCanvas/NodeTemplate/index.vue
+++ frontend/desktop/src/components/common/TemplateCanvas/NodeTemplate/index.vue
@@ -236,6 +236,7 @@
             right: -8px;
             font-size: 16px;
             color: #63656e;
+            background: #ffffff;
             border-radius: 50%;
             z-index: 2;
             cursor: pointer;
