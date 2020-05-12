b81c4947ebf193c5fe8eb145b3b043a799a60fe3
sandrincai <873217631@qq.com>
2020-05-12 17:56:55 +0800

    fix: format缺少花括号/错拼导致的keyError错误

--- gcloud/apigw/views/node_callback.py
+++ gcloud/apigw/views/node_callback.py
@@ -61,7 +61,7 @@ def node_callback(request, task_id, project_id):
         task = TaskFlowInstance.objects.get(id=task_id, project_id=project.id)
     except TaskFlowInstance.DoesNotExist:
         message = "[API] node_callback task[id={task_id}] "
-        "of project[project_id={project_id, biz_id{biz_id}}] does not exist".format(
+        "of project[project_id={project_id}, biz_id{biz_id}}] does not exist".format(
             task_id=task_id, project_id=project.id, biz_id=project.bk_biz_id
         )
         logger.exception(message)
