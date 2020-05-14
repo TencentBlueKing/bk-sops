6cbb923bba6e4f4440d72243818b7f5c8a785415
840583540 <13206251194@163.com>
2020-05-14 17:10:22 +0800

    improve: 翻译文件去掉\\

--- frontend/desktop/src/config/i18n/index.js
+++ frontend/desktop/src/config/i18n/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import store from '../../store/index.js'
+// import store from '../../store/index.js'
 import en from './en.js'
 import cn from './cn.js'
 
@@ -10,8 +10,6 @@ const messages = {
     en,
     'zh-cn': cn
 }
-console.log(store, 'storestorestore')
-// const localeLang = store.state.lang === 'en' ? 'zh-cn' : 'en'
 // const localeLang = store.state.lang === 'en' ? 'en' : 'zh-cn'
 const localeLang = getCookie('blueking_language') === 'en' ? 'en' : 'zh-cn'
 
