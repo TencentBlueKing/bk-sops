68c3ad19d7fef175496d203283a87a5f32a1f75a
sandrincai <873217631@qq.com>
2020-05-15 21:07:09 +0800

    minor: 代码规范

--- pipeline_plugins/tests/components/collections/sites/open/cc_test/v1_0/test_cmdb_transfer_host_module.py
+++ pipeline_plugins/tests/components/collections/sites/open/cc_test/v1_0/test_cmdb_transfer_host_module.py
@@ -20,10 +20,10 @@ from pipeline.component_framework.test import (
     CallAssertion,
     ExecuteAssertion,
     Call,
-    Patcher
+    Patcher,
 )
 from pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0 import (
-    CCTransferHostModuleComponent
+    CCTransferHostModuleComponent,
 )
 
 
@@ -41,21 +41,25 @@ class CCTransferHostModuleComponentTest(TestCase, ComponentTestMixin):
 
 
 class MockClient(object):
-    def __init__(self, get_mainline_object_topo_return=None, search_biz_inst_topo_return=None,
-                 transfer_host_module_return=None):
+    def __init__(
+        self, get_mainline_object_topo_return=None, search_biz_inst_topo_return=None, transfer_host_module_return=None
+    ):
         self.cc = MagicMock()
         self.cc.get_mainline_object_topo = MagicMock(return_value=get_mainline_object_topo_return)
         self.cc.search_biz_inst_topo = MagicMock(return_value=search_biz_inst_topo_return)
         self.cc.transfer_host_module = MagicMock(return_value=transfer_host_module_return)
 
 
-GET_CLIENT_BY_USER = 'pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0' \
-                     '.get_client_by_user'
-GET_IP_BY_REGEX = 'pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0.get_ip_by_regex'
-CC_GET_HOST_ID_BY_INNERIP = 'pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0' \
-                            '.cc_get_host_id_by_innerip'
-CC_LIST_MATCH_NODE_INST_ID = 'pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0' \
-                             '.cc_list_match_node_inst_id'
+GET_CLIENT_BY_USER = (
+    "pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0" ".get_client_by_user"
+)
+GET_IP_BY_REGEX = "pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0.get_ip_by_regex"
+CC_GET_HOST_ID_BY_INNERIP = (
+    "pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0" ".cc_get_host_id_by_innerip"
+)
+CC_LIST_MATCH_NODE_INST_ID = (
+    "pipeline_plugins.components.collections.sites.open.cc_transfer_host_module.v1_0" ".cc_list_match_node_inst_id"
+)
 
 COMMON_MAINLINE = {
     "result": True,
@@ -69,7 +73,7 @@ COMMON_MAINLINE = {
             "bk_next_obj": "set",
             "bk_next_name": "集群",
             "bk_pre_obj_id": "",
-            "bk_pre_obj_name": ""
+            "bk_pre_obj_name": "",
         },
         {
             "bk_obj_id": "custom",
@@ -78,7 +82,7 @@ COMMON_MAINLINE = {
             "bk_next_obj": "set",
             "bk_next_name": "集群",
             "bk_pre_obj_id": "biz",
-            "bk_pre_obj_name": "业务"
+            "bk_pre_obj_name": "业务",
         },
         {
             "bk_obj_id": "set",
@@ -87,7 +91,7 @@ COMMON_MAINLINE = {
             "bk_next_obj": "module",
             "bk_next_name": "模块",
             "bk_pre_obj_id": "custom",
-            "bk_pre_obj_name": "自定义层级"
+            "bk_pre_obj_name": "自定义层级",
         },
         {
             "bk_obj_id": "module",
@@ -96,7 +100,7 @@ COMMON_MAINLINE = {
             "bk_next_obj": "host",
             "bk_next_name": "主机",
             "bk_pre_obj_id": "set",
-            "bk_pre_obj_name": "集群"
+            "bk_pre_obj_name": "集群",
         },
         {
             "bk_obj_id": "host",
@@ -105,9 +109,9 @@ COMMON_MAINLINE = {
             "bk_next_obj": "",
             "bk_next_name": "",
             "bk_pre_obj_id": "module",
-            "bk_pre_obj_name": "模块"
-        }
-    ]
+            "bk_pre_obj_name": "模块",
+        },
+    ],
 }
 
 COMMON_TOPO = {
@@ -144,196 +148,174 @@ COMMON_TOPO = {
                                             "bk_inst_name": "host",
                                             "bk_obj_id": "host",
                                             "bk_obj_name": "host",
-                                            "child": []
+                                            "child": [],
                                         }
-                                    ]
+                                    ],
                                 }
-                            ]
+                            ],
                         }
-                    ]
+                    ],
                 }
-            ]
+            ],
         }
-    ]
+    ],
 }
 
-COMMON_PARENT = {
-    'executor': 'admin',
-    'biz_cc_id': 2,
-    'biz_supplier_account': 0
-}
+COMMON_PARENT = {"executor": "admin", "biz_cc_id": 2, "biz_supplier_account": 0}
 
 SELECT_BY_TEXT_SUCCESS_CLIENT = MockClient(
     get_mainline_object_topo_return=COMMON_MAINLINE,
     search_biz_inst_topo_return=COMMON_TOPO,
-    transfer_host_module_return={
-        "result": True,
-        "code": 0,
-        "message": "",
-        "data": {}
-    }
+    transfer_host_module_return={"result": True, "code": 0, "message": "", "data": {}},
 )
 
 SELECT_BY_TEXT_SUCCESS_INPUTS = {
-    'biz_cc_id': 2,
-    'cc_module_select_method': 'text',
-    'cc_module_select_topo': [],
-    'cc_module_select_text': u'    蓝鲸>Tun>set>module\n\n',
-    'cc_host_ip': '1.1.1.1;2.2.2.2',
-    'cc_is_increment': 'false',
-    '_loop': 1
+    "biz_cc_id": 2,
+    "cc_module_select_method": "text",
+    "cc_module_select_topo": [],
+    "cc_module_select_text": u"    蓝鲸>Tun>set>module\n\n",
+    "cc_host_ip": "1.1.1.1;2.2.2.2",
+    "cc_is_increment": "false",
+    "_loop": 1,
 }
 
 SELECT_BY_TEXT_SUCCESS_CASE = ComponentTestCase(
-    name='success case: select module by text(include newline/space)',
+    name="success case: select module by text(include newline/space)",
     inputs=SELECT_BY_TEXT_SUCCESS_INPUTS,
     parent_data=COMMON_PARENT,
-    execute_assertion=ExecuteAssertion(
-        success=True,
-        outputs={}),
+    execute_assertion=ExecuteAssertion(success=True, outputs={}),
     schedule_assertion=None,
     execute_call_assertion=[
-        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call('admin', 2, ['1.1.1.1', '2.2.2.2'], 0)]),
+        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call("admin", 2, ["1.1.1.1", "2.2.2.2"], 0)]),
         CallAssertion(
             func=SELECT_BY_TEXT_SUCCESS_CLIENT.cc.transfer_host_module,
             calls=[
-                Call({
-                    'bk_biz_id': 2,
-                    'bk_supplier_account': 0,
-                    'bk_host_id': [2, 3],
-                    'bk_module_id': [7],
-                    'is_increment': False
-                })
-            ]
-        )
+                Call(
+                    {
+                        "bk_biz_id": 2,
+                        "bk_supplier_account": 0,
+                        "bk_host_id": [2, 3],
+                        "bk_module_id": [7],
+                        "is_increment": False,
+                    }
+                )
+            ],
+        ),
     ],
     patchers=[
         Patcher(target=GET_CLIENT_BY_USER, return_value=SELECT_BY_TEXT_SUCCESS_CLIENT),
-        Patcher(target=GET_IP_BY_REGEX, return_value=['1.1.1.1', '2.2.2.2']),
-        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={'result': True, 'data': ['2', '3']})
-    ]
+        Patcher(target=GET_IP_BY_REGEX, return_value=["1.1.1.1", "2.2.2.2"]),
+        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={"result": True, "data": ["2", "3"]}),
+    ],
 )
 
 
 SELECT_BY_TOPO_SUCCESS_CLIENT = MockClient(
     get_mainline_object_topo_return=COMMON_MAINLINE,
     search_biz_inst_topo_return=COMMON_TOPO,
-    transfer_host_module_return={
-        "result": True,
-        "code": 0,
-        "message": "",
-        "data": {}
-    }
+    transfer_host_module_return={"result": True, "code": 0, "message": "", "data": {}},
 )
 
 SELECT_BY_TOPO_SUCCESS_INPUTS = {
-    'biz_cc_id': 2,
-    'cc_module_select_method': 'topo',
-    'cc_module_select_topo': ['module_7'],
-    'cc_module_select_text': None,
-    'cc_host_ip': '1.1.1.1;2.2.2.2',
-    'cc_is_increment': 'false',
-    '_loop': 1
+    "biz_cc_id": 2,
+    "cc_module_select_method": "topo",
+    "cc_module_select_topo": ["module_7"],
+    "cc_module_select_text": None,
+    "cc_host_ip": "1.1.1.1;2.2.2.2",
+    "cc_is_increment": "false",
+    "_loop": 1,
 }
 
 SELECT_BY_TOPO_SUCCESS_CASE = ComponentTestCase(
-    name='success case: select module by topo',
+    name="success case: select module by topo",
     inputs=SELECT_BY_TOPO_SUCCESS_INPUTS,
     parent_data=COMMON_PARENT,
-    execute_assertion=ExecuteAssertion(
-        success=True,
-        outputs={}),
+    execute_assertion=ExecuteAssertion(success=True, outputs={}),
     schedule_assertion=None,
     execute_call_assertion=[
-        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call('admin', 2, ['1.1.1.1', '2.2.2.2'], 0)]),
+        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call("admin", 2, ["1.1.1.1", "2.2.2.2"], 0)]),
         CallAssertion(
             func=SELECT_BY_TOPO_SUCCESS_CLIENT.cc.transfer_host_module,
             calls=[
-                Call({
-                    'bk_biz_id': 2,
-                    'bk_supplier_account': 0,
-                    'bk_host_id': [2, 3],
-                    'bk_module_id': [7],
-                    'is_increment': False
-                })
-            ]
-        )
+                Call(
+                    {
+                        "bk_biz_id": 2,
+                        "bk_supplier_account": 0,
+                        "bk_host_id": [2, 3],
+                        "bk_module_id": [7],
+                        "is_increment": False,
+                    }
+                )
+            ],
+        ),
     ],
     patchers=[
         Patcher(target=GET_CLIENT_BY_USER, return_value=SELECT_BY_TOPO_SUCCESS_CLIENT),
-        Patcher(target=GET_IP_BY_REGEX, return_value=['1.1.1.1', '2.2.2.2']),
-        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={'result': True, 'data': ['2', '3']})
-    ]
+        Patcher(target=GET_IP_BY_REGEX, return_value=["1.1.1.1", "2.2.2.2"]),
+        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={"result": True, "data": ["2", "3"]}),
+    ],
 )
 
 
 SELECT_BY_TEXT_ERROR_PATH_FAIL_CLIENT = MockClient(
-    get_mainline_object_topo_return=COMMON_MAINLINE,
-    search_biz_inst_topo_return=COMMON_TOPO
+    get_mainline_object_topo_return=COMMON_MAINLINE, search_biz_inst_topo_return=COMMON_TOPO
 )
 
 
 SELECT_BY_TEXT_ERROR_PATH_FAIL_INPUTS = {
-    'biz_cc_id': 2,
-    'cc_module_select_method': 'text',
-    'cc_module_select_topo': [],
-    'cc_module_select_text': u'    蓝鲸>Yun >set>module\n\n',
-    'cc_host_ip': '1.1.1.1;2.2.2.2',
-    'cc_is_increment': 'false',
-    '_loop': 1
+    "biz_cc_id": 2,
+    "cc_module_select_method": "text",
+    "cc_module_select_topo": [],
+    "cc_module_select_text": u"    蓝鲸>Yun >set>module\n\n",
+    "cc_host_ip": "1.1.1.1;2.2.2.2",
+    "cc_is_increment": "false",
+    "_loop": 1,
 }
 
 SELECT_BY_TEXT_ERROR_PATH_FAIL_CASE = ComponentTestCase(
-    name='fail case: select module by text text with error path',
+    name="fail case: select module by text text with error path",
     inputs=SELECT_BY_TEXT_ERROR_PATH_FAIL_INPUTS,
     parent_data=COMMON_PARENT,
-    execute_assertion=ExecuteAssertion(
-        success=False, outputs={
-            'ex_data': u'不存在该拓扑路径：蓝鲸>Yun>set>module'
-        }),
+    execute_assertion=ExecuteAssertion(success=False, outputs={"ex_data": u"不存在该拓扑路径：蓝鲸>Yun>set>module"}),
     schedule_assertion=None,
     execute_call_assertion=[
-        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call('admin', 2, ['1.1.1.1', '2.2.2.2'], 0)])
+        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call("admin", 2, ["1.1.1.1", "2.2.2.2"], 0)])
     ],
     patchers=[
         Patcher(target=GET_CLIENT_BY_USER, return_value=SELECT_BY_TEXT_ERROR_PATH_FAIL_CLIENT),
-        Patcher(target=GET_IP_BY_REGEX, return_value=['1.1.1.1', '2.2.2.2']),
-        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={'result': True, 'data': ['2', '3']})
-    ]
+        Patcher(target=GET_IP_BY_REGEX, return_value=["1.1.1.1", "2.2.2.2"]),
+        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={"result": True, "data": ["2", "3"]}),
+    ],
 )
 
 
 SELECT_BY_TEXT_ERROR_LEVEL_FAIL_CLIENT = MockClient(
-    get_mainline_object_topo_return=COMMON_MAINLINE,
-    search_biz_inst_topo_return=COMMON_TOPO
+    get_mainline_object_topo_return=COMMON_MAINLINE, search_biz_inst_topo_return=COMMON_TOPO
 )
 
 
 SELECT_BY_TEXT_ERROR_LEVEL_FAIL_INPUTS = {
-    'biz_cc_id': 2,
-    'cc_module_select_method': 'text',
-    'cc_module_select_topo': [],
-    'cc_module_select_text': u'    蓝鲸>Yun >   module\n\n',
-    'cc_host_ip': '1.1.1.1;2.2.2.2',
-    'cc_is_increment': 'false',
-    '_loop': 1
+    "biz_cc_id": 2,
+    "cc_module_select_method": "text",
+    "cc_module_select_topo": [],
+    "cc_module_select_text": u"    蓝鲸>Yun >   module\n\n",
+    "cc_host_ip": "1.1.1.1;2.2.2.2",
+    "cc_is_increment": "false",
+    "_loop": 1,
 }
 
 SELECT_BY_TEXT_ERROR_LEVEL_FAIL_CASE = ComponentTestCase(
-    name='fail case: select module by text text with error level',
+    name="fail case: select module by text text with error level",
     inputs=SELECT_BY_TEXT_ERROR_LEVEL_FAIL_INPUTS,
     parent_data=COMMON_PARENT,
-    execute_assertion=ExecuteAssertion(
-        success=False, outputs={
-            'ex_data': u'输入文本路径[蓝鲸>Yun>module]与业务拓扑层级不匹配'
-        }),
+    execute_assertion=ExecuteAssertion(success=False, outputs={"ex_data": u"输入文本路径[蓝鲸>Yun>module]与业务拓扑层级不匹配"}),
     schedule_assertion=None,
     execute_call_assertion=[
-        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call('admin', 2, ['1.1.1.1', '2.2.2.2'], 0)])
+        CallAssertion(func=CC_GET_HOST_ID_BY_INNERIP, calls=[Call("admin", 2, ["1.1.1.1", "2.2.2.2"], 0)])
     ],
     patchers=[
         Patcher(target=GET_CLIENT_BY_USER, return_value=SELECT_BY_TEXT_ERROR_LEVEL_FAIL_CLIENT),
-        Patcher(target=GET_IP_BY_REGEX, return_value=['1.1.1.1', '2.2.2.2']),
-        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={'result': True, 'data': ['2', '3']})
-    ]
+        Patcher(target=GET_IP_BY_REGEX, return_value=["1.1.1.1", "2.2.2.2"]),
+        Patcher(target=CC_GET_HOST_ID_BY_INNERIP, return_value={"result": True, "data": ["2", "3"]}),
+    ],
 )
