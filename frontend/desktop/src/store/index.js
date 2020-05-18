ec786710151d705d767bd4e73b161e9889a3f991
840583540 <13206251194@163.com>
2020-05-18 15:46:40 +0800

    improve: blueking_language 兼容处理

--- frontend/desktop/src/store/index.js
+++ frontend/desktop/src/store/index.js
@@ -17,7 +17,7 @@ import api from '@/api'
 Vue.use(Vuex)
 
 function getAppLang () {
-    return getCookie('blueking_language') || 'zh-cn'
+    return getCookie('blueking_language') === 'en' ? 'en' : 'zh-cn'
 }
 
 const store = new Vuex.Store({
