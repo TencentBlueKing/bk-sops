9faa910f4c877805057b860894fc82f4e49ae949
homholueng <homholueng@gmail.com>
2020-05-21 11:23:02 +0800

    optimization: 1. 支持请求耗时分析中间件 2. tastypie 列表权限信息缓存优化

--- auth_backend/plugins/utils.py
+++ auth_backend/plugins/utils.py
@@ -18,40 +18,44 @@ from builtins import str
 
 import six
 
+from django.core.cache import caches
+
 from auth_backend.plugins.constants import PRINCIPAL_TYPE_USER
 from blueapps.utils.cache import with_cache
 
 
-logger = logging.getLogger('root')
-CACHE_PREFIX = __name__.replace('.', '_')
+logger = logging.getLogger("root")
+CACHE_PREFIX = __name__.replace(".", "_")
 
 
 def build_need_permission(auth_resource, action_id, instance=None, scope_id=None):
     base_info = auth_resource.base_info()
-    resource = base_info.pop('resource')
+    resource = base_info.pop("resource")
     resources = []
     if instance is not None:
         resource_id = instance if isinstance(instance, (six.string_types, int)) else auth_resource.resource_id(instance)
-        resource.update({
-            'resource_id': resource_id,
-            'resource_name': auth_resource.resource_name(instance)
-        })
+        resource.update({"resource_id": resource_id, "resource_name": auth_resource.resource_name(instance)})
         resources.append([resource])
 
-    base_info.update({
-        'resource_type': resource['resource_type'],
-        'resource_type_name': resource['resource_type_name'],
-        'action_id': action_id,
-        'scope_id': scope_id or base_info['scope_id'],
-        'action_name': auth_resource.actions_map[action_id].name,
-        'resources': resources
-    })
+    base_info.update(
+        {
+            "resource_type": resource["resource_type"],
+            "resource_type_name": resource["resource_type_name"],
+            "action_id": action_id,
+            "scope_id": scope_id or base_info["scope_id"],
+            "action_name": auth_resource.actions_map[action_id].name,
+            "resources": resources,
+        }
+    )
     return base_info
 
 
-@with_cache(seconds=10,
-            prefix='{}_search_all_resources_authorized_actions'.format(CACHE_PREFIX),
-            ex=['username', 'resource_type', 'action_ids'])
+@with_cache(
+    seconds=10,
+    prefix="{}_search_all_resources_authorized_actions".format(CACHE_PREFIX),
+    ex=["username", "resource_type", "action_ids"],
+    cache=caches["locmem"],
+)
 def search_all_resources_authorized_actions(username, resource_type, auth_resource, action_ids=None, scope_id=None):
     """
     @summary: 获取所有用户对某个资源类型的所有资源实例的权限
@@ -69,35 +73,34 @@ def search_all_resources_authorized_actions(username, resource_type, auth_resour
         principal_type=PRINCIPAL_TYPE_USER,
         principal_id=username,
         action_ids=action_ids,
-        scope_id=scope_id)
-    if not authorized_result['result']:
-        logger.error("Search authorized resources of Resource[{resource}] return error: {error}".format(
-            resource=auth_resource.name,
-            error=authorized_result['message']
-        ))
+        scope_id=scope_id,
+    )
+    if not authorized_result["result"]:
+        logger.error(
+            "Search authorized resources of Resource[{resource}] return error: {error}".format(
+                resource=auth_resource.name, error=authorized_result["message"]
+            )
+        )
         return {}
-    authorized_resources = authorized_result['data']
+    authorized_resources = authorized_result["data"]
     resources_perms = {}
     for action_resource in authorized_resources:
-        for absolute_resource in action_resource['resource_ids']:
+        for absolute_resource in action_resource["resource_ids"]:
             for relative_resource in absolute_resource:
-                if relative_resource['resource_type'] == auth_resource.rtype:
-                    resource_id = str(relative_resource['resource_id'])
-                    resources_perms.setdefault(resource_id, []).append(action_resource['action_id'])
+                if relative_resource["resource_type"] == auth_resource.rtype:
+                    resource_id = str(relative_resource["resource_id"])
+                    resources_perms.setdefault(resource_id, []).append(action_resource["action_id"])
     return resources_perms
 
 
-@with_cache(seconds=10,
-            prefix='{}_search_instance_authorized_actions'.format(CACHE_PREFIX),
-            ex=['username', 'resource_type', 'instance_id', 'action_ids'])
+@with_cache(
+    seconds=10,
+    prefix="{}_search_instance_authorized_actions".format(CACHE_PREFIX),
+    ex=["username", "resource_type", "instance_id", "action_ids"],
+    cache=caches["locmem"],
+)
 def search_instance_authorized_actions(
-    username,
-    resource_type,
-    auth_resource,
-    instance_id,
-    instance,
-    action_ids=None,
-    scope_id=None
+    username, resource_type, auth_resource, instance_id, instance, action_ids=None, scope_id=None
 ):
     if action_ids is None:
         action_ids = list(auth_resource.actions_map.keys())
@@ -107,18 +110,19 @@ def search_instance_authorized_actions(
         principal_id=username,
         action_ids=action_ids,
         instance=instance,
-        scope_id=scope_id
+        scope_id=scope_id,
     )
-    if not verify_result['result']:
-        logger.error("Search authorized actions of Resource[{resource}] return error: {error}".format(
-            resource=auth_resource.name,
-            error=verify_result['message']
-        ))
+    if not verify_result["result"]:
+        logger.error(
+            "Search authorized actions of Resource[{resource}] return error: {error}".format(
+                resource=auth_resource.name, error=verify_result["message"]
+            )
+        )
         return []
 
     authorized_actions = []
-    for action_info in verify_result['data']:
-        if action_info['is_pass']:
-            authorized_actions.append(action_info['action_id'])
+    for action_info in verify_result["data"]:
+        if action_info["is_pass"]:
+            authorized_actions.append(action_info["action_id"])
 
     return authorized_actions
