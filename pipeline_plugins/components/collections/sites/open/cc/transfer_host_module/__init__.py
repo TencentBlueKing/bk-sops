7751a1dab551d424a0e6f24b75b8a122f6e0c8c0
pagezhou <zongpeizhou@gmail.com>
2020-05-26 18:12:17 +0800

    bugfix: 修复升级标准运维版本到V3.5.10，导致处于暂停或定时中包含CMDB插件的流程任务无法继续的问题 #2080

--- pipeline_plugins/components/collections/sites/open/cc/transfer_host_module/__init__.py
+++ pipeline_plugins/components/collections/sites/open/cc/transfer_host_module/__init__.py
@@ -11,4 +11,4 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from .legacy import CCTransferHostModuleComponent  # noqa
+from .legacy import CCTransferHostModuleComponent, CCTransferHostModuleService  # noqa
