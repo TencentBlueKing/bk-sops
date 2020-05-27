294ab4a633e19796afd10331d29620066c0fac8c
homholueng <homholueng@gmail.com>
2020-05-27 15:29:49 +0800

    minor: iam initial.json modification

--- gcloud/commons/tastypie/resources.py
+++ gcloud/commons/tastypie/resources.py
@@ -11,9 +11,9 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from tastypie.http import HttpForbidden, NotFound
+from tastypie.http import HttpForbidden
 from tastypie.resources import ModelResource
-from tastypie.exceptions import ImmediateHttpResponse
+from tastypie.exceptions import ImmediateHttpResponse, NotFound
 from django.db.models import Q
 from haystack.query import SearchQuerySet
 
