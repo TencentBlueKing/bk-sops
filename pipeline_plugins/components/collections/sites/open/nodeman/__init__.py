1c2e27af30320208b56b40b857c298d6c9286247
homholueng <homholueng@gmail.com>
2020-06-09 17:38:04 +0800

    bugfix: 修复节点管理插件升级时没有兼容旧插件路径的问题

--- pipeline_plugins/components/collections/sites/open/nodeman/__init__.py
+++ pipeline_plugins/components/collections/sites/open/nodeman/__init__.py
@@ -11,4 +11,5 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
+from .create_task.nodeman import NodemanCreateTaskService  # noqa
 from .create_task.nodeman import NodemanCreateTaskComponent  # noqa
