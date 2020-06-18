3ccaae839e83f89df52969b2c45cf0addb2c72d7
Tanghaohaohao <2094710432@qq.com>
2020-06-18 17:05:30 +0800

    feature:新增主机加锁与解锁插件

--- pipeline_plugins/components/collections/sites/open/cc/host_lock/add_host_lock/legacy.py
+++ pipeline_plugins/components/collections/sites/open/cc/host_lock/add_host_lock/legacy.py
@@ -2,7 +2,7 @@
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
 Edition) available.
-Copyright (C) 2017-2020 THL A29 Limited, a Tencent company. All rights reserved.
+Copyright (C) 2017-2019 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://opensource.org/licenses/MIT
@@ -10,23 +10,22 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
 from django.utils.translation import ugettext_lazy as _
+
+from gcloud.conf import settings
+
+from pipeline.component_framework.component import Component
+from pipeline_plugins.components.collections.sites.open.cc.host_lock.base import CCHostLockBaseService, AddHostLockMixin
 
 __group_name__ = _("配置平台(CMDB)")
 
 
-from .batch_delete_set import *  # noqa
-from .create_set import *  # noqa
-from .empty_set_hosts import *  # noqa
-from .replace_fault_machine import *  # noqa
-from .transfer_fault_host import *  # noqa
-from .transfer_host_module import *  # noqa
-from .transfer_host_resource import *  # noqa
-from .transfer_to_idle import *  # noqa
-from .update_host import *  # noqa
-from .update_module import *  # noqa
-from .update_set import *  # noqa
-from .update_set_service_status import *  # noqa
-from .host_lock.add_host_lock import *  # noqa
-from .host_lock.delete_host_lock import *  # noqa
+class CmdbAddHostLockService(AddHostLockMixin, CCHostLockBaseService):
+    pass
+
+
+class CmdbAddHostLockComponent(Component):
+    name = _("主机加锁")
+    code = "cmdb_add_host_lock"
+    bound_service = CmdbAddHostLockService
+    form = "%scomponents/atoms/cc/cmdb_add_host_lock.js" % settings.STATIC_URL
---
+++ pipeline_plugins/components/collections/sites/open/docs/cc/delete_host_lock/legacy.md
@@ -0,0 +1,26 @@
+# 主机解锁
+> 版本`legacy`
+
+## 介绍
+
+主机解锁
+
+
+## 标签
+`cc` `cmdb` `delete_host_lock`
+
+## 参数说明
+
+* `biz_cc_id` 业务id
+
+* `cc_host_ip` 主机内网IP
+
+## 输出参数说明
+
+* 执行结果：
+
+  系统展示的插件执行结果
+
+## 样例
+
+![](../images/delete_host_lock.png)
