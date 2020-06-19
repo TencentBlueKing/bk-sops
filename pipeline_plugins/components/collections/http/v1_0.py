f47bfa9ce2e9417ce11685e030f2b64aa429a2ce
pagezz <zongpeizhou@gmail.com>
2020-06-19 11:19:07 +0800

    feature: 蓝鲸服务(BK)-发送通知插件升级到v1.0，通知方式支持在蓝鲸PaaS API网关配置后并自动生效 #2190 (#2191)
    feature: 蓝鲸服务(BK)-发送通知插件升级到v1.0，通知方式支持在蓝鲸PaaS API网关配置后并自动生效 #2190

--- pipeline_plugins/components/collections/http/v1_0.py
+++ pipeline_plugins/components/collections/http/v1_0.py
@@ -25,7 +25,7 @@ from pipeline.core.flow.activity import Service
 from pipeline.core.flow.io import StringItemSchema, IntItemSchema, ObjectItemSchema, ArrayItemSchema
 from pipeline.component_framework.component import Component
 
-__group_name__ = _(u"蓝鲸服务(BK)")
+__group_name__ = _("蓝鲸服务(BK)")
 logger = logging.getLogger(__name__)
 
 
@@ -33,53 +33,53 @@ class HttpRequestService(Service):
 
     def inputs_format(self):
         return [
-            self.InputItem(name=_(u'HTTP 请求方法'),
-                           key='bk_http_request_method',
-                           type='string',
-                           schema=StringItemSchema(description=_(u'HTTP 请求方法'))),
-            self.InputItem(name=_(u'HTTP 请求目标地址'),
-                           key='bk_http_request_url',
-                           type='string',
-                           schema=StringItemSchema(description=_(u'HTTP 请求目标地址'))),
-            self.InputItem(name=_(u'HTTP 请求 header'),
-                           key='bk_http_request_header',
-                           type='array',
+            self.InputItem(name=_("HTTP 请求方法"),
+                           key="bk_http_request_method",
+                           type="string",
+                           schema=StringItemSchema(description=_("HTTP 请求方法"))),
+            self.InputItem(name=_("HTTP 请求目标地址"),
+                           key="bk_http_request_url",
+                           type="string",
+                           schema=StringItemSchema(description=_("HTTP 请求目标地址"))),
+            self.InputItem(name=_("HTTP 请求 header"),
+                           key="bk_http_request_header",
+                           type="array",
                            schema=ArrayItemSchema(
-                               description=_(u'HTTP 请求头部列表'),
+                               description=_("HTTP 请求头部列表"),
                                item_schema=ObjectItemSchema(
-                                   description=_(u'单个头部信息'),
-                                   property_schemas={'name': StringItemSchema(description=_(u'请求头名称')),
-                                                     'value': StringItemSchema(description=_(u'请求头值'))}))),
-            self.InputItem(name=_(u'HTTP 请求 body'),
-                           key='bk_http_request_body',
-                           type='string',
-                           schema=StringItemSchema(description=_(u'HTTP 请求 body'))),
-            self.InputItem(name=_(u'HTTP 请求超时时间'),
-                           key='bk_http_request_timeout',
-                           type='int',
-                           schema=IntItemSchema(description=_(u'HTTP 请求超时时间'))),
-            self.InputItem(name=_(u'HTTP 请求成功条件'),
-                           key='bk_http_success_exp',
-                           type='string',
-                           schema=StringItemSchema(description=_(u'根据返回的 JSON 的数据来控制节点的成功或失败, '
-                                                                 u'使用 resp 引用返回的 JSON 对象，例 resp.result==True')))
+                                   description=_("单个头部信息"),
+                                   property_schemas={"name": StringItemSchema(description=_("请求头名称")),
+                                                     "value": StringItemSchema(description=_("请求头值"))}))),
+            self.InputItem(name=_("HTTP 请求 body"),
+                           key="bk_http_request_body",
+                           type="string",
+                           schema=StringItemSchema(description=_("HTTP 请求 body"))),
+            self.InputItem(name=_("HTTP 请求超时时间"),
+                           key="bk_http_request_timeout",
+                           type="int",
+                           schema=IntItemSchema(description=_("HTTP 请求超时时间"))),
+            self.InputItem(name=_("HTTP 请求成功条件"),
+                           key="bk_http_success_exp",
+                           type="string",
+                           schema=StringItemSchema(description=_("根据返回的 JSON 的数据来控制节点的成功或失败, "
+                                                                 "使用 resp 引用返回的 JSON 对象，例 resp.result==True")))
         ]
 
     def outputs_format(self):
         return [
-            self.OutputItem(name=_(u'响应内容'),
-                            key='data',
-                            type='object',
-                            schema=ObjectItemSchema(description=_(u'HTTP 请求响应内容，内部结构不固定'),
+            self.OutputItem(name=_("响应内容"),
+                            key="data",
+                            type="object",
+                            schema=ObjectItemSchema(description=_("HTTP 请求响应内容，内部结构不固定"),
                                                     property_schemas={})),
-            self.OutputItem(name=_(u'状态码'),
-                            key='status_code',
-                            type='int',
-                            schema=IntItemSchema(description=_(u'HTTP 请求响应状态码')))]
+            self.OutputItem(name=_("状态码"),
+                            key="status_code",
+                            type="int",
+                            schema=IntItemSchema(description=_("HTTP 请求响应状态码")))]
 
     def execute(self, data, parent_data):
-        if parent_data.get_one_of_inputs('language'):
-            translation.activate(parent_data.get_one_of_inputs('language'))
+        if parent_data.get_one_of_inputs("language"):
+            translation.activate(parent_data.get_one_of_inputs("language"))
 
         method = data.inputs.bk_http_request_method
         url = data.inputs.bk_http_request_url
@@ -90,17 +90,17 @@ class HttpRequestService(Service):
         other = {"headers": {}}
 
         if method.upper() not in ["GET", "HEAD"]:
-            other["data"] = body.encode('utf-8')
-            other["headers"] = {'Content-type': 'application/json'}
+            other["data"] = body.encode("utf-8")
+            other["headers"] = {"Content-type": "application/json"}
 
         if request_header:
-            headers = {header['name']: header['value'] for header in request_header}
+            headers = {header["name"]: header["value"] for header in request_header}
             other["headers"].update(headers)
 
         if timeout:
             other["timeout"] = timeout
 
-        self.logger.info('send {} request to {}'.format(method, url))
+        self.logger.info("send {} request to {}".format(method, url))
 
         try:
             response = request(
@@ -110,7 +110,7 @@ class HttpRequestService(Service):
                 **other
             )
         except Exception as e:
-            err = _(u"请求异常，详细信息: {}")
+            err = _("请求异常，详细信息: {}")
             self.logger.error(err.format(traceback.format_exc()))
             data.outputs.ex_data = err.format(e)
             return False
@@ -122,16 +122,16 @@ class HttpRequestService(Service):
         except Exception:
             try:
                 content = response.content.decode(response.encoding)
-                self.logger.error(u"response data is not a valid JSON: {}".format(content[:500]))
+                self.logger.error("response data is not a valid JSON: {}".format(content[:500]))
             except Exception:
-                self.logger.error('response data is not a valid JSON')
-            data.outputs.ex_data = _(u"请求响应数据格式非 JSON")
+                self.logger.error("response data is not a valid JSON")
+            data.outputs.ex_data = _("请求响应数据格式非 JSON")
             return False
 
         data.outputs.data = resp
 
         if not (200 <= response.status_code < 300):
-            err = _(u"请求失败，状态码: {}，响应: {}").format(response.status_code, resp)
+            err = _("请求失败，状态码: {}，响应: {}").format(response.status_code, resp)
             self.logger.error(err)
             data.outputs.ex_data = err
             return False
@@ -139,11 +139,11 @@ class HttpRequestService(Service):
         if success_exp:
             try:
                 rule = BoolRule(success_exp)
-                if not rule.test(context={'resp': resp}):
-                    data.outputs.ex_data = _(u'请求成功判定失败')
+                if not rule.test(context={"resp": resp}):
+                    data.outputs.ex_data = _("请求成功判定失败")
                     return False
             except Exception as e:
-                err = _(u'请求成功条件判定出错: {}')
+                err = _("请求成功条件判定出错: {}")
                 self.logger.error(err.format(traceback.format_exc()))
                 data.outputs.ex_data = err.format(e)
                 return False
@@ -152,10 +152,10 @@ class HttpRequestService(Service):
 
 
 class HttpComponent(Component):
-    name = _(u'HTTP 请求')
-    desc = _(u"提示: 1.请求URL需要在当前网络下可以访问，否则会超时失败 "
-             u"2.响应状态码在200-300(不包括300)之间，并且相应内容是 JSON 格式才会执行成功")
-    code = 'bk_http_request'
+    name = _("HTTP 请求")
+    desc = _("提示: 1.请求URL需要在当前网络下可以访问，否则会超时失败 "
+             "2.响应状态码在200-300(不包括300)之间，并且相应内容是 JSON 格式才会执行成功")
+    code = "bk_http_request"
     bound_service = HttpRequestService
-    form = settings.STATIC_URL + 'components/atoms/bk/v1.0/http.js'
-    version = 'v1.0'
+    version = "v1.0"
+    form = "%scomponents/atoms/bk/http/v1_0.js" % settings.STATIC_URL
