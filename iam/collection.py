0c427f1e879b1e0730a371bc619ddf1cff7bc672
homholueng <homholueng@gmail.com>
2020-06-29 15:24:07 +0800

    bugfix: 修复部分方法 super 调用失败的问题

--- iam/collection.py
+++ iam/collection.py
@@ -22,7 +22,7 @@ class FancyDict(dict):
     def __setattr__(self, key, value):
         # 内建属性不放入 key 中
         if key.startswith("__") and key.endswith("__"):
-            super(FancyDict).__setattr__(key, value)
+            super(FancyDict, self).__setattr__(key, value)
         else:
             self[key] = value
 
