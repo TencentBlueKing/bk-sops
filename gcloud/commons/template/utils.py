936c1031dd86c88ed171ddf26ee3e1d6a097f7e9
homholueng <homholueng@gmail.com>
2020-07-16 11:44:32 +0800

    hot fix subprocess bug (#2312) (#2327)
    * bugfix: 修复流程中存在嵌套子流程时创建任务 404 的问题
    * bugfix: 修复子流程参数传递过程中内外层存在同名 key 时导致参数传递失败的问题

--- gcloud/commons/template/utils.py
+++ gcloud/commons/template/utils.py
@@ -17,6 +17,8 @@ import logging
 
 import ujson as json
 
+from pipeline.core.constants import PE
+
 from gcloud import err_code
 from gcloud.conf import settings
 
@@ -27,37 +29,33 @@ def read_encoded_template_data(content):
     try:
         data = json.loads(base64.b64decode(content))
     except Exception:
-        return {
-            'result': False,
-            'message': 'Template data is corrupt',
-            'code': err_code.REQUEST_PARAM_INVALID.code
-        }
+        return {"result": False, "message": "Template data is corrupt", "code": err_code.REQUEST_PARAM_INVALID.code}
 
     # check the validation of file
-    templates_data = data['template_data']
-    data_string = (json.dumps(templates_data, sort_keys=True) + settings.TEMPLATE_DATA_SALT).encode('utf-8')
+    templates_data = data["template_data"]
+    data_string = (json.dumps(templates_data, sort_keys=True) + settings.TEMPLATE_DATA_SALT).encode("utf-8")
     digest = hashlib.md5(data_string).hexdigest()
 
-    is_data_valid = (digest == data['digest'])
+    is_data_valid = digest == data["digest"]
     if not is_data_valid:
-        return {
-            'result': False,
-            'message': 'Invalid template data',
-            'code': err_code.VALIDATION_ERROR.code
-        }
+        return {"result": False, "message": "Invalid template data", "code": err_code.VALIDATION_ERROR.code}
 
-    return {
-        'result': True,
-        'data': data,
-        'code': err_code.SUCCESS.code
-    }
+    return {"result": True, "data": data, "code": err_code.SUCCESS.code}
 
 
 def read_template_data_file(f):
     if not f:
-        return {
-            'result': False,
-            'message': 'Upload template dat file please.'
-        }
+        return {"result": False, "message": "Upload template dat file please."}
 
     return read_encoded_template_data(content=f.read())
+
+
+def replace_template_id(template_model, pipeline_data, reverse=False):
+    activities = pipeline_data[PE.activities]
+    for act_id, act in list(activities.items()):
+        if act["type"] == PE.SubProcess:
+            if not reverse:
+                act["template_id"] = template_model.objects.get(pk=act["template_id"]).pipeline_template.template_id
+            else:
+                template = template_model.objects.get(pipeline_template__template_id=act["template_id"])
+                act["template_id"] = str(template.pk)
