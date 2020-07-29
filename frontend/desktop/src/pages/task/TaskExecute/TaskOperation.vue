5a031a53be73bcd8d9e3f7911e700fb4fec7436c
zhoudafa <941071842@qq.com>
2020-07-29 20:20:58 +0800

    minor: 修改事件名

--- frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
+++ frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
@@ -65,7 +65,7 @@
                     :instance-operations="instanceOperations"
                     :instance-name="instanceName"
                     :instance_id="instance_id"
-                    @hasBtn="judgeMaskClose"
+                    @hasBtn="hideOperateBtn"
                     @packUp="packUp">
                 </ModifyParams>
                 <ExecuteInfo
@@ -837,7 +837,7 @@
                 }
             },
 
-            judgeMaskClose (val) {
+            hideOperateBtn (val) {
                 this.quickClose = !val
             },
             
