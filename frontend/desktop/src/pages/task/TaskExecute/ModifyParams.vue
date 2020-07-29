953815456205c9c8dff906c9b9ff2942c0bf1fad
v-xugzhou <67539158+v-xugzhou@users.noreply.github.com>
2020-07-29 19:33:47 +0800

    develop: 任务全局变量修改保存按钮添加 loading 状态 (#2393)
    * improve: 任务全局变量修改保存按钮添加 loading 状态
    
    * minor: 给侧滑组件所有保存按钮添加loading效果
    
    * minor:  删除日志

--- frontend/desktop/src/pages/task/TaskExecute/ModifyParams.vue
+++ frontend/desktop/src/pages/task/TaskExecute/ModifyParams.vue
@@ -39,6 +39,7 @@
                 :class="{
                     'btn-permission-disable': !hasSavePermission
                 }"
+                :loading="pending"
                 v-cursor="{ active: !hasSavePermission }"
                 @click="onModifyParams">
                 {{ $t('保存') }}
