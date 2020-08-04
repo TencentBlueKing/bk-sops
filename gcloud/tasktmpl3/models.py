80243cf1cc050c79e270986a094d254573b7a007
homholueng <homholueng@gmail.com>
2020-08-04 20:48:47 +0800

    minor: 导入流程时支持从导入数据中读取模板覆盖设定

--- gcloud/tasktmpl3/models.py
+++ gcloud/tasktmpl3/models.py
@@ -29,16 +29,15 @@ logger = logging.getLogger("root")
 
 
 class TaskTemplateManager(BaseTemplateManager, TaskTmplStatisticsMixin):
-
     def create(self, **kwargs):
         pipeline_template = self.create_pipeline_template(**kwargs)
         task_template = self.model(
-            project=kwargs['project'],
-            category=kwargs['category'],
+            project=kwargs["project"],
+            category=kwargs["category"],
             pipeline_template=pipeline_template,
-            notify_type=kwargs['notify_type'],
-            notify_receivers=kwargs['notify_receivers'],
-            time_out=kwargs['time_out'],
+            notify_type=kwargs["notify_type"],
+            notify_receivers=kwargs["notify_receivers"],
+            time_out=kwargs["time_out"],
         )
         task_template.save()
         return task_template
@@ -52,24 +51,28 @@ class TaskTemplateManager(BaseTemplateManager, TaskTmplStatisticsMixin):
     def import_operation_check(self, template_data, project_id):
         data = super(TaskTemplateManager, self).import_operation_check(template_data)
 
-        template = template_data['template']
+        template = template_data["template"]
 
-        relate_project_ids = self.filter(id__in=list(template.keys()),
-                                         is_deleted=False
-                                         ).values_list('project_id', flat=True)
+        relate_project_ids = self.filter(id__in=list(template.keys()), is_deleted=False).values_list(
+            "project_id", flat=True
+        )
         is_multiple_relate = len(set(relate_project_ids)) > 1
         is_across_override = relate_project_ids and relate_project_ids[0] != int(project_id)
-        has_common_template = not all([tmpl.get('project_id') for _, tmpl in list(template_data['template'].items())])
 
+        has_common_template = not all([tmpl.get("project_id") for _, tmpl in list(template_data["template"].items())])
         can_override = not (is_multiple_relate or is_across_override or has_common_template)
 
+        # 以文件中设定的禁止覆盖设定为最高优先级
+        if template_data.get("override_forbidden", False):
+            can_override = False
+
         if not can_override:
-            data['override_template'] = []
+            data["override_template"] = []
 
         result = {
-            'can_override': can_override,
-            'new_template': data['new_template'],
-            'override_template': data['override_template']
+            "can_override": can_override,
+            "new_template": data["new_template"],
+            "override_template": data["override_template"],
         }
         return result
 
@@ -78,31 +81,33 @@ class TaskTemplateManager(BaseTemplateManager, TaskTmplStatisticsMixin):
         check_info = self.import_operation_check(template_data, project_id)
 
         # operation validation check
-        if override and (not check_info['can_override']):
+        if override and (not check_info["can_override"]):
             return {
-                'result': False,
-                'message': 'Unable to override flows across project',
-                'data': 0,
-                'code': err_code.INVALID_OPERATION.code
+                "result": False,
+                "message": "Unable to override flows across project",
+                "data": 0,
+                "code": err_code.INVALID_OPERATION.code,
             }
 
         def defaults_getter(template_dict):
             return {
-                'project': project,
-                'category': template_dict['category'],
-                'notify_type': template_dict['notify_type'],
-                'notify_receivers': template_dict['notify_receivers'],
-                'time_out': template_dict['time_out'],
-                'pipeline_template_id': template_dict['pipeline_template_id'],
-                'is_deleted': False
+                "project": project,
+                "category": template_dict["category"],
+                "notify_type": template_dict["notify_type"],
+                "notify_receivers": template_dict["notify_receivers"],
+                "time_out": template_dict["time_out"],
+                "pipeline_template_id": template_dict["pipeline_template_id"],
+                "is_deleted": False,
             }
 
-        return super(TaskTemplateManager, self)._perform_import(template_data=template_data,
-                                                                check_info=check_info,
-                                                                override=override,
-                                                                defaults_getter=defaults_getter,
-                                                                resource=resource_type_lib['flow'],
-                                                                operator=operator)
+        return super(TaskTemplateManager, self)._perform_import(
+            template_data=template_data,
+            check_info=check_info,
+            override=override,
+            defaults_getter=defaults_getter,
+            resource=resource_type_lib["flow"],
+            operator=operator,
+        )
 
     def replace_all_template_tree_node_id(self):
         templates = self.filter(is_deleted=False)
@@ -119,32 +124,25 @@ class TaskTemplateManager(BaseTemplateManager, TaskTmplStatisticsMixin):
 
     def get_collect_template(self, project_id, username):
         user_model = get_user_model()
-        collected_templates = user_model.objects.get(username=username).tasktemplate_set.values_list('id', flat=True)
+        collected_templates = user_model.objects.get(username=username).tasktemplate_set.values_list("id", flat=True)
         collected_templates_list = []
         template_list = self.filter(is_deleted=False, project_id=project_id, id__in=list(collected_templates))
         for template in template_list:
-            collected_templates_list.append({
-                'id': template.id,
-                'name': template.name
-            })
+            collected_templates_list.append({"id": template.id, "name": template.name})
         return True, collected_templates_list
 
     def get_templates_with_expired_subprocess(self, project_id):
-        tmpl_and_pipeline_id = self.filter(project_id=project_id, is_deleted=False).values('id', 'pipeline_template_id')
+        tmpl_and_pipeline_id = self.filter(project_id=project_id, is_deleted=False).values("id", "pipeline_template_id")
         return self.check_templates_subprocess_expired(tmpl_and_pipeline_id)
 
 
 class TaskTemplate(BaseTemplate):
-    project = models.ForeignKey(Project,
-                                verbose_name=_("所属项目"),
-                                null=True,
-                                blank=True,
-                                on_delete=models.SET_NULL)
+    project = models.ForeignKey(Project, verbose_name=_("所属项目"), null=True, blank=True, on_delete=models.SET_NULL)
 
     objects = TaskTemplateManager()
 
     def __unicode__(self):
-        return '%s_%s' % (self.project, self.pipeline_template)
+        return "%s_%s" % (self.project, self.pipeline_template)
 
     class Meta(BaseTemplate.Meta):
         verbose_name = _("流程模板 TaskTemplate")
