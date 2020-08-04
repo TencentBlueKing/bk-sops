89ec4bbc3d2e66f221052cb53d09f18e87199d47
homholueng <homholueng@gmail.com>
2020-08-04 11:34:21 +0800

    minor: 优化 api_prefix 变量名, 增加 bk_iam 前缀

--- iam/contrib/django/middlewares.py
+++ iam/contrib/django/middlewares.py
@@ -20,6 +20,6 @@ from iam.contrib.django.response import IAMAuthFailedResponse
 class AuthFailedExceptionMiddleware(MiddlewareMixin):
     def process_exception(self, request, exception):
         if isinstance(exception, AuthFailedBaseException):
-            api_prefix = getattr(settings, "API_PREFIX", "")
+            api_prefix = getattr(settings, "BK_IAM_API_PREFIX", "")
             status_code = 200 if api_prefix and request.path.startswith(api_prefix) else 499
             return IAMAuthFailedResponse(exception, status=status_code)
