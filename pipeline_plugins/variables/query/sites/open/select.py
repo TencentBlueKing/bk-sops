643de28ee53d4d541ecf55ee13b163978ff1426d
normal-wls <1158341873@qq.com>
2020-08-11 21:12:30 +0800

    improvement:开发者对于下拉框远程数据源可配置自定义转换函数

--- pipeline_plugins/variables/query/sites/open/select.py
+++ pipeline_plugins/variables/query/sites/open/select.py
@@ -14,11 +14,12 @@ specific language governing permissions and limitations under the License.
 import logging
 import requests
 
+from django.conf import settings
 from django.conf.urls import url
 from django.http import JsonResponse
 from django.utils.translation import ugettext_lazy as _
 
-logger = logging.getLogger('root')
+logger = logging.getLogger("root")
 
 
 def variable_select_source_data_proxy(request):
@@ -27,30 +28,35 @@ def variable_select_source_data_proxy(request):
     @param request:
     @return:
     """
-    url = request.GET.get('url')
+    url = request.GET.get("url")
     try:
-        response = requests.get(
-            url=url,
-            verify=False
-        )
+        response = requests.get(url=url, verify=False)
     except Exception as e:
-        logger.exception('variable select get data from url[url={url}] raise error: {error}'.format(url=url, error=e))
-        text = _('请求数据异常: {error}').format(error=e)
-        data = [{'text': text, 'value': ''}]
+        logger.exception("variable select get data from url[url={url}] raise error: {error}".format(url=url, error=e))
+        text = _("请求数据异常: {error}").format(error=e)
+        data = [{"text": text, "value": ""}]
         return JsonResponse(data, safe=False)
 
     try:
         data = response.json()
+
+        # 支持开发者对远程数据源数据配置处理函数，进行再处理
+        post_process_function = getattr(settings, "REMOTE_SOURCE_DATA_TRANSFORM_FUNCTION", None)
+        if post_process_function and callable(post_process_function):
+            data = post_process_function(data)
+
     except Exception:
         try:
             content = response.content.decode(response.encoding)
-            logger.exception('variable select get data from url[url={url}] is not a valid JSON: {data}'.format(
-                url=url, data=content[:500])
+            logger.exception(
+                "variable select get data from url[url={url}] is not a valid JSON: {data}".format(
+                    url=url, data=content[:500]
+                )
             )
         except Exception:
-            logger.exception('variable select get data from url[url={url}] data is not a valid JSON'.format(url=url))
-        text = _('返回数据格式错误，不是合法 JSON 格式')
-        data = [{'text': text, 'value': ''}]
+            logger.exception("variable select get data from url[url={url}] data is not a valid JSON".format(url=url))
+        text = _("返回数据格式错误，不是合法 JSON 格式")
+        data = [{"text": text, "value": ""}]
 
     return JsonResponse(data, safe=False)
 
