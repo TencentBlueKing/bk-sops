ae76c8542d95196ad2ea97c924b1a23dedcb4a37
feifeizhao <gg47549390@126.com>
2020-08-28 16:50:39 +0800

    minor:恢复消息通知分组字段

--- pipeline_plugins/components/static/components/atoms/bk/notify/v1_0.js
+++ pipeline_plugins/components/static/components/atoms/bk/notify/v1_0.js
@@ -96,12 +96,12 @@
                         attrs: {
                             name: gettext("通知分组"),
                             items: [
-                                {name: gettext("运维人员"), value: "bk_biz_maintainer"},
-                                {name: gettext("产品人员"), value: "bk_biz_productor"},
-                                {name: gettext("开发人员"), value: "bk_biz_developer"},
-                                {name: gettext("测试人员"), value: "bk_biz_tester"},
+                                {name: gettext("运维人员"), value: "Maintainers"},
+                                {name: gettext("产品人员"), value: "ProductPm"},
+                                {name: gettext("开发人员"), value: "Developer"},
+                                {name: gettext("测试人员"), value: "Tester"},
                             ],
-                            default: ["bk_biz_maintainer"],
+                            default: ["Maintainers"],
                             validation: [
                                 {
                                     type: "custom",
