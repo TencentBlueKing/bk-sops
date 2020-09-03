bded1a460c6185db76a43596b130301fe9af45da
zhoudafa <941071842@qq.com>
2020-09-03 18:05:05 +0800

    minor: 样式规则在调用组件的地方覆盖

--- frontend/desktop/src/components/common/TemplateCanvas/index.vue
+++ frontend/desktop/src/components/common/TemplateCanvas/index.vue
@@ -1455,11 +1455,6 @@
             z-index: 4;
         }
     }
-    .task-management-page {
-        .canvas-flow-wrap {
-            padding-left: 60px;
-        }
-    }
     .drag-reference-line {
         display: none;
         position: absolute;
