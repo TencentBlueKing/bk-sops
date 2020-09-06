f0857a46d31f3a008e205ec5ba86defee79f864f
luofan <fannluowork@gmail.com>
2020-09-06 13:58:53 +0800

    minor：表格单元格事件交互默认值根据type类型取值

--- frontend/desktop/src/components/common/RenderForm/FormItem.vue
+++ frontend/desktop/src/components/common/RenderForm/FormItem.vue
@@ -70,7 +70,7 @@
                 :atom-methods="scheme.methods"
                 :value="formValue"
                 :parent-value="parentValue"
-                @init="$emit('init')"
+                @init="$emit('init', $event)"
                 @change="updateForm"
                 @onShow="onShowForm"
                 @onHide="onHideForm">
