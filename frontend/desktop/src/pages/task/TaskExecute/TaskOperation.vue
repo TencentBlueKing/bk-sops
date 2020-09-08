4c914d1d9c3ccffb2eaef365037dee25ec788aa2
zhoudafa <941071842@qq.com>
2020-09-08 20:50:27 +0800

    minor: 去掉commin-icon

--- frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
+++ frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
@@ -892,7 +892,7 @@
                     this.defaultActiveId = firstNodeId
                     this.setNodeDetailConfig(firstNodeId, firstNodeData)
                 }
-                if (name === i18n.t('流程模板数据')) {
+                if (type === 'templateData') {
                     this.transPipelineTreeStr()
                 }
             },
