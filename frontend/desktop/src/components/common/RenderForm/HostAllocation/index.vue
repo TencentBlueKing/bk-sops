22acaab95094d189b168b60cf259d344aeb6cd51
zhoudafa <941071842@qq.com>
2020-09-16 16:44:14 +0800

    improve: 修复导入导出数据格式问题

--- frontend/desktop/src/components/common/RenderForm/HostAllocation/index.vue
+++ frontend/desktop/src/components/common/RenderForm/HostAllocation/index.vue
@@ -120,13 +120,10 @@
                 data.forEach(item => {
                     const dataItem = {}
                     Object.keys(item).forEach(key => {
+                        console.log(item[key])
                         // renderForm 组件 value 需要接受 object 类型数据
-                        if (item[key] && item[key].length > 0) {
-                            item[key].forEach(md => {
-                                dataItem[md.key] = {
-                                    [md.key]: md.value.join('\n')
-                                }
-                            })
+                        dataItem[key] = {
+                            [key]: item[key]
                         }
                     })
                     localData.push(dataItem)
