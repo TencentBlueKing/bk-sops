4ccc3fb2005b0715ae67d75ff633f66310a679b8
rykerli <rykerli@tencent.com>
2020-09-17 09:07:00 +0800

    bugfix:修改筛选集群判断

--- pipeline_plugins/variables/collections/sites/open/cmdb/var_cmdb_set_module_ip_selector.py
+++ pipeline_plugins/variables/collections/sites/open/cmdb/var_cmdb_set_module_ip_selector.py
@@ -128,17 +128,18 @@ def get_module_id_list(
     @param filter_service_template_names: 需要筛选的服务模板名称，多个用英文分隔
     @return:
     """
-    filter_set_name_list = filter_set_names.split(",")
-    filter_service_template_name_list = filter_service_template_names.split(",")
-
-    if filter_set_name_list is None or filter_service_template_name_list is None:
-        return []
-    set_ids = [set_item["bk_set_id"] for set_item in set_list if set_item["bk_set_name"] in filter_set_name_list]
-    service_template_ids = [
-        service_template_item["id"]
-        for service_template_item in service_template_list
-        if service_template_item["name"] in filter_service_template_name_list
-    ]
+    if not filter_set_names:
+        set_ids = [set_item["bk_set_id"] for set_item in set_list]
+    else:
+        set_ids = [set_item["bk_set_id"] for set_item in set_list if set_item["bk_set_name"] in filter_set_names.split(",")]
+    if not filter_service_template_names:
+        service_template_ids = [service_template_item["id"] for service_template_item in service_template_list]
+    else:
+        service_template_ids = [
+            service_template_item["id"]
+            for service_template_item in service_template_list
+            if service_template_item["name"] in filter_service_template_names.split(",")
+        ]
 
     # 调用find_module_with_relation接口根据set id list, service_template_id_list查询模块id
     module_id_list = find_module_with_relation(bk_biz_id, username, set_ids, service_template_ids, ["bk_module_id"])
