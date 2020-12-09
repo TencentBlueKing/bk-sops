7ee98aff100df4b87f91d89a5b8bdd55d7c761da
352671669 <352671669@qq.com>
2020-12-09 10:40:42 +0800

    review

--- pipeline_plugins/components/utils/sites/open/choose_time_tools.py
+++ pipeline_plugins/components/utils/sites/open/choose_time_tools.py
@@ -5,14 +5,14 @@ import re
 import time
 
 # 正则表达式预编译
-date_regx = re.compile(r"(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2})")
+date_regx = re.compile(r"(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})")
 
 logger = logging.getLogger("component")
 
 
 def get_end_time_by_duration(start_time, duration):
-    dt = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M")
-    end_time = (dt + datetime.timedelta(minutes=duration)).strftime("%Y-%m-%d %H:%M")
+    dt = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    end_time = (dt + datetime.timedelta(minutes=duration)).strftime("%Y-%m-%d %H:%M:%S")
     return end_time
 
 
@@ -27,7 +27,7 @@ def choose_time(time_type, start_time, end_time, duration, date_regx=date_regx):
         end_time = end_match[0]
     # 从当前时间开始，仅输入持续时间
     elif time_type == 1:
-        start_time = time.strftime("%Y-%m-%d %H:%M", time.localtime(time.time()))
+        start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         end_time = get_end_time_by_duration(start_time, int(duration))
     # 输入开始时间和持续时间
     elif time_type == 2:
