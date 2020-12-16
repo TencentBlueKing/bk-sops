446f6cc249710463f75eb2ea17fa8371f00d8ce8
luofan <fannluowork@gmail.com>
2020-12-16 21:39:50 +0800

    minor:打开移动端调试

--- frontend/mobile/src/main.js
+++ frontend/mobile/src/main.js
@@ -43,9 +43,12 @@ import {
 import enUS from 'vant/lib/locale/lang/en-US'
 import zhCN from 'vant/lib/locale/lang/zh-CN'
 import 'amfe-flexible'
+import Vconsole from 'vconsole'
 
 import '../static/style/app.scss'
 
+const vconsole = new Vconsole()
+console.log(vconsole)
 Vue.use(NavBar)
     .use(Search)
     .use(Button)
