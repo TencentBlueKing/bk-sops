36a1b66dcbf076467be830544952b6256a09c4db
v-xugzhou <941071842@qq.com>
2020-12-29 10:01:31 +0800

    minor: 删除多余注释

--- frontend/desktop/src/pages/audit/AuditList.vue
+++ frontend/desktop/src/pages/audit/AuditList.vue
@@ -403,7 +403,6 @@
                 }
             },
             onSearchFormSubmit (data) {
-                // this.requestData = data
                 this.requestData = Object.assign({}, this.requestData, data)
                 this.pagination.current = 1
                 this.updateUrl()
