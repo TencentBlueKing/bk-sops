10c2102f8ce887aa85285720df6cf20986b2a8d9
352671669 <352671669@qq.com>
2021-01-13 16:53:50 +0800

    前端缩进

--- pipeline_plugins/components/static/components/atoms/job/job_execute_task.js
+++ pipeline_plugins/components/static/components/atoms/job/job_execute_task.js
@@ -164,48 +164,48 @@
                     type: "init",
                     action: function () {
                         this.table_buttons = [{
-                    text: gettext("刷新全局变量"),
-                    callback: function() {
-                        const job_id = this.get_parent().get_child("job_task_id").value;
-                        var $this = this;
-                        this.changeHook(false);
-                        if (job_id === '') {
-                            this._set_value([]);
-                            return;
-                        }
-                        this.set_loading(true);
-                        const cc_id = this.get_parent && this.get_parent().get_child('biz_cc_id')._get_value();
-                        $.ajax({
-                            url: $.context.get('site_url') + 'pipeline/job_get_job_detail_by_biz/' + cc_id + '/' + job_id + '/',
-                            type: 'GET',
-                            dataType: 'json',
-                            success: function (resp) {
-                                var global_var = $this._get_value()
-                                if (global_var) {
-                                    var new_global_var = resp.data.global_var.map(function (item) {
-                                        var target = global_var.find(function (old_item) {
-                                            return old_item.name === item.name;
-                                        });
-                                        if (target) {
-                                            item.value = target.value;
+                            text: gettext("刷新全局变量"),
+                            callback: function () {
+                                const job_id = this.get_parent().get_child("job_task_id").value;
+                                var $this = this;
+                                this.changeHook(false);
+                                if (job_id === '') {
+                                    this._set_value([]);
+                                    return;
+                                }
+                                this.set_loading(true);
+                                const cc_id = this.get_parent && this.get_parent().get_child('biz_cc_id')._get_value();
+                                $.ajax({
+                                    url: $.context.get('site_url') + 'pipeline/job_get_job_detail_by_biz/' + cc_id + '/' + job_id + '/',
+                                    type: 'GET',
+                                    dataType: 'json',
+                                    success: function (resp) {
+                                        var global_var = $this._get_value()
+                                        if (global_var) {
+                                            var new_global_var = resp.data.global_var.map(function (item) {
+                                                var target = global_var.find(function (old_item) {
+                                                    return old_item.name === item.name;
+                                                });
+                                                if (target) {
+                                                    item.value = target.value;
+                                                }
+                                                return item;
+                                            })
+                                            $this._set_value(new_global_var);
                                         }
-                                        return item;
-                                    })
-                                    $this._set_value(new_global_var);
-                                }
-                                $this.set_loading(false);
-                                if (resp.result === false) {
-                                    show_msg(resp.message, 'error');
-                                }
-                            },
-                            error: function () {
-                                $this._set_value([]);
-                                $this.set_loading(false);
-                                show_msg('request job detail error', 'error');
+                                        $this.set_loading(false);
+                                        if (resp.result === false) {
+                                            show_msg(resp.message, 'error');
+                                        }
+                                    },
+                                    error: function () {
+                                        $this._set_value([]);
+                                        $this.set_loading(false);
+                                        show_msg('request job detail error', 'error');
+                                    }
+                                });
                             }
-                        });
-                    }
-                }]
+                        }]
                     }
                 },
                 {
