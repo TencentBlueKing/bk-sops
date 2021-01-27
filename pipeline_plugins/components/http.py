546fb4b4f3fac6251141b94cc7b27a1e4becb697
352671669 <352671669@qq.com>
2021-01-27 11:19:39 +0800

    修改review内容

--- pipeline_plugins/components/http.py
+++ pipeline_plugins/components/http.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
 Edition) available.
 Copyright (C) 2017-2020 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the 'License'); you may not use this file except in compliance with the License.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://opensource.org/licenses/MIT
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
-'''
+"""
 
 import requests
 import ujson as json
@@ -28,7 +28,6 @@ def get(url, params, logger, headers=None):
 
 
 def http_do(method, url, params, logger, headers=None):
-
     session_request = requests.Session()
     if headers:
         headers.update({'Content-Type': 'application/json'})
