4840b2a56c46e4411359fe799505babac62244af
fannluo <fannluowork@gmail.com>
2021-03-10 16:28:45 +0800

    bugfix:集群资源筛选变量主机数量手动输入数字不生效问题修复

--- frontend/desktop/src/components/common/RenderForm/SetAllocation/ResourceFilter.vue
+++ frontend/desktop/src/components/common/RenderForm/SetAllocation/ResourceFilter.vue
@@ -945,14 +945,18 @@
                         topo,
                         search_host_lock: this.formData.filterLock || undefined
                     })
-                    if (this.formData.filterLock) { // 过滤已经加锁主机
-                        hostData.data = hostData.data.filter(item => !item.bk_host_lock_status)
+                    if (hostData.result) {
+                        if (this.formData.filterLock) { // 过滤已经加锁主机
+                            hostData.data = hostData.data.filter(item => !item.bk_host_lock_status)
+                        }
+    
+                        const moduleHosts = this.filterModuleHost(hostData.data)
+                        const configData = this.getConfigData()
+                        this.$emit('update', configData, moduleHosts)
+                        this.$emit('update:showFilter', false)
+                    } else {
+                        errorHandler(hostData, this)
                     }
-
-                    const moduleHosts = this.filterModuleHost(hostData.data)
-                    const configData = this.getConfigData()
-                    this.$emit('update', configData, moduleHosts)
-                    this.$emit('update:showFilter', false)
                 } catch (error) {
                     errorHandler(error, this)
                 } finally {
@@ -985,7 +989,7 @@
 
                         if (md.count > 0) {
                             item.list.some(h => {
-                                if (moduleHosts[md.name].length === md.count) {
+                                if (moduleHosts[md.name].length === Number(md.count)) {
                                     return true
                                 }
                                 if (!usedHosts.includes(h.bk_host_innerip) && !mutedHostAttrs.includes(h[this.formData.muteAttribute])) {
