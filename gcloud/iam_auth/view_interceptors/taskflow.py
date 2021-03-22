890393b3a0157343ffd48a567d69111a67e8fdc2
normal-wls <1158341873@qq.com>
2021-03-22 19:26:13 +0800

    minor: 任务相关iam请求使用is_allowed_with_cache

--- gcloud/iam_auth/view_interceptors/taskflow.py
+++ gcloud/iam_auth/view_interceptors/taskflow.py
@@ -38,7 +38,7 @@ class TaskSingleActionInterceptor(ViewInterceptor, metaclass=abc.ABCMeta):
         resources = res_factory.resources_for_task(task_id)
 
         request = Request(IAMMeta.SYSTEM_ID, subject, action, resources, {})
-        allowed = iam.is_allowed(request)
+        allowed = iam.is_allowed_with_cache(request)
 
         if not allowed:
             raise AuthFailedException(IAMMeta.SYSTEM_ID, subject, action, resources)
