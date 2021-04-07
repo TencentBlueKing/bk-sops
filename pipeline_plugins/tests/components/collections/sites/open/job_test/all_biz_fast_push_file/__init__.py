c4243c6e3e1e924dd4ca0a543baa2792ff468bba
feifeizhao <gg47549390@126.com>
2021-04-07 11:32:53 +0800

    minor; 新增全业务快速分发文件，支持批量分发

--- pipeline_plugins/tests/components/collections/sites/open/job_test/all_biz_fast_push_file/__init__.py
+++ pipeline_plugins/tests/components/collections/sites/open/job_test/all_biz_fast_push_file/__init__.py
@@ -2,7 +2,7 @@
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
 Edition) available.
-Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Copyright (C) 2017-2019 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://opensource.org/licenses/MIT
@@ -10,16 +10,3 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
-from django.utils.translation import ugettext_lazy as _
-
-__group_name__ = _("作业平台(JOB)")
-
-from .base import JobService  # noqa
-from .cron_task import *  # noqa
-from .execute_task import *  # noqa
-from .fast_execute_script import *  # noqa
-from .fast_push_file import *  # noqa
-from .push_local_files import *  # noqa
-from .local_content_upload import *  # noqa
-from .all_biz_fast_push_file import *  # noqa
