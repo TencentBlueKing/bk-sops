520b86149468704cd54dbf7486ccb69b72ec607e
v-xugzhou <67539158+v-xugzhou@users.noreply.github.com>
2021-05-25 10:46:51 +0800

    improvement: 流水操作记录操作时间显示不全优化&& 流水操作节点name前端实现 (#3583)
    * improvement:  流水操作记录操作时间显示不全优化
    
    * improvement:  操作流水节点name前端实现
    
    * minor: add log
    
    * minor:  查看节点详情不展示节点名称

--- frontend/desktop/src/pages/task/TaskExecute/OperationFlow.vue
+++ frontend/desktop/src/pages/task/TaskExecute/OperationFlow.vue
@@ -3,9 +3,9 @@
         <bk-table
             ext-cls="operate-flow-table"
             :data="operateFlowData">
-            <bk-table-column :label="$t('操作时间')" prop="operate_date"></bk-table-column>
+            <bk-table-column min-width="130" :label="$t('操作时间')" prop="operate_date"></bk-table-column>
             <bk-table-column :label="$t('操作名称')" prop="operate_type_name"></bk-table-column>
-            <bk-table-column :label="$t('节点名称')" prop="node_name"></bk-table-column>
+            <bk-table-column v-if="!nodeId" :label="$t('节点名称')" prop="node_name"></bk-table-column>
             <bk-table-column :label="$t('操作来源')" prop="operate_source_name"></bk-table-column>
             <bk-table-column :label="$t('操作人')" prop="operator"></bk-table-column>
         </bk-table>
@@ -14,8 +14,13 @@
 
 <script>
     import { mapActions } from 'vuex'
+    import moment from 'moment'
     export default {
         props: {
+            locations: {
+                type: Array,
+                default: () => []
+            },
             nodeId: {
                 type: String,
                 default: ''
@@ -46,7 +51,18 @@
                         instance_id: query.instance_id,
                         node_id: this.nodeId || undefined
                     })
-                    this.operateFlowData = resp.data || []
+                    this.operateFlowData = resp.data.map(item => {
+                        item.operate_date = moment(item.operate_date).format('YYYY-MM-DD HH:mm:ss')
+                        let nodeName = '--'
+                        if (item.node_id) {
+                            const node = this.locations.find(node => node.id === item.node_id)
+                            if (node) {
+                                nodeName = node.name
+                            }
+                        }
+                        item.node_name = nodeName
+                        return item
+                    }) || []
                 } catch (error) {
                     console.warn(error)
                 }
