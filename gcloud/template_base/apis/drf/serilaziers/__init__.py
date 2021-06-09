d8c871c60e6a0407a22c9964bffbd8ab46265acd
wheelwang <wheelwang@tencent.com>
2021-06-09 16:40:37 +0800

    'minor:实现公共流程执行方案'

--- gcloud/template_base/apis/drf/serilaziers/__init__.py
+++ gcloud/template_base/apis/drf/serilaziers/__init__.py
@@ -11,4 +11,4 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from .template_scheme import * # noqa
+from .template_scheme import *  # noqa
