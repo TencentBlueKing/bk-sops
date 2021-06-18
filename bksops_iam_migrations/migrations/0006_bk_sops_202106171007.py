477b9fe618b12f40988d6ef99972289328e361e9
waylonshi <1158341873@qq.com>
2021-06-18 15:06:12 +0800

    minor: code review fix

--- bksops_iam_migrations/migrations/0006_bk_sops_202106171007.py
+++ bksops_iam_migrations/migrations/0006_bk_sops_202106171007.py
@@ -1,23 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import os
-import json
-import codecs
-
 from django.db import migrations
-from django.conf import settings
 
 from iam.contrib.iam_migration.migrator import IAMMigrator
 
-from bksops_iam_migrations.utils import finished_old_iam_migrations
-
 
 def forward_func(apps, schema_editor):
-
-    if "0006_bk_sops_202106171007" in finished_old_iam_migrations():
-        print("0006_bk_sops_202106171007 already run at iam_migrations, skip.")
-        return
-
     migrator = IAMMigrator(Migration.migration_json)
     migrator.migrate()
 
