dd941ea116c112cd6d7f70f3708155b7b33d5f6f
v-xugzhou <941071842@qq.com>
2021-07-07 10:10:49 +0800

    minor: 修复错误字段

--- frontend/desktop/src/pages/task/TaskList/index.vue
+++ frontend/desktop/src/pages/task/TaskList/index.vue
@@ -497,7 +497,7 @@
                     this.setting.size = size || 'small'
                     selectedFields = fieldList || this.defaultSelected
                     if (!fieldList || !size) {
-                        localStorage.removeItem('commonTemplateList')
+                        localStorage.removeItem('TaskList')
                     }
                 } else {
                     selectedFields = this.defaultSelected
