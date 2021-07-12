2ba7cec9afa4f1c0ab25f9aca79cc4f9ecd553f0
core <64761294@qq.com>
2021-07-12 12:17:07 +0800

    feature: 执行作业插件和快速执行脚本插件支持失败自动重试策略以及继承历史成功状态

--- pipeline_plugins/components/static/components/atoms/job/fast_execute_script/v2_0.js
+++ pipeline_plugins/components/static/components/atoms/job/fast_execute_script/v2_0.js
@@ -29,7 +29,7 @@
                 },
                 disabled: !$.context.canSelectBiz(),
                 validation: [
-                    {
+                    { 
                         type: "required"
                     }
                 ]
