1267c9eae01e3325138fa09b618fe0b4118c2152
waylonshi <1158341873@qq.com>
2021-07-28 15:49:49 +0800

    feature: 添加JOB获取任务日志插件

---
+++ static/components/atoms/job/fetch_task_log/v1_0.js
@@ -0,0 +1,65 @@
+/**
+ * Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
+ * Edition) available.
+ * Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+(function () {
+    $.atoms.job_fetch_task_log = [
+        {
+            tag_code: "biz_cc_id",
+            type: "select",
+            attrs: {
+                name: gettext("业务"),
+                allowCreate: true,
+                hookable: true,
+                remote: true,
+                remote_url: $.context.get('site_url') + 'pipeline/cc_get_business_list/',
+                remote_data_init: function (resp) {
+                    if (resp.result === false) {
+                        show_msg(resp.message, 'error');
+                    }
+                    return resp.data;
+                },
+                disabled: !$.context.canSelectBiz(),
+                validation: [
+                    {
+                        type: "required"
+                    }
+                ]
+            },
+            methods: {
+                _tag_init: function () {
+                    if (this.value) {
+                        return
+                    }
+                    this._set_value($.context.getBkBizId())
+                }
+            }
+        },
+        {
+            tag_code: "job_task_id",
+            type: "input",
+            attrs: {
+                name: gettext("任务ID"),
+                placeholder: gettext("只支持单个任务ID"),
+                hookable: true,
+                validation: [{type: "required"}]
+            }
+        },
+        {
+            tag_code: "job_target_ip",
+            type: "input",
+            attrs: {
+                name: gettext("目标IP"),
+                placeholder: gettext("非必填，只支持单个目标IP"),
+                hookable: true,
+            }
+        }
+    ]
+})();
