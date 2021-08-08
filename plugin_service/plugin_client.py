a179ee35e209dda225a2d9abdfca387b8713d7d4
waylon <1158341873@qq.com>
2021-08-09 00:31:33 +0800

    feature: 新增对接远程插件服务

---
+++ plugin_service/plugin_client.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community
+Edition) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+import functools
+import json
+import logging
+import os
+
+import requests
+
+from . import env
+
+logger = logging.getLogger("root")
+
+
+def response_parser(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if not result.get("result"):
+            logger.error(f"{func.__name__} request error: {result.get('message')}")
+            data = {"message": result.get("message")}
+            if "trace_id" in result:
+                data["trace_id"] = result["trace_id"]
+            return False, data
+        else:
+            data = result.get("data")
+            if "trace_id" in result and isinstance(data, dict):
+                data["trace_id"] = result["trace_id"]
+            return True, data
+
+    return wrapper
+
+
+class PluginServiceApiClient:
+    def __init__(self, plugin_code, plugin_host=None):
+        self.plugin_code = plugin_code
+        if not plugin_host:
+            # TODO: PaaS根据plugin_code获取host
+            self.plugin_host = env.TEST_PLUGIN_HOST.rstrip("/")
+
+    @response_parser
+    def invoke(self, version, data):
+        url = os.path.join(
+            f"http://{self.plugin_code}.{env.APIGW_URL_SUFFIX}", env.APIGW_ENVIRONMENT, "invoke", version
+        )
+        headers = {
+            "X-Bkapi-Authorization": json.dumps(
+                {"bk_app_code": env.PLUGIN_SERVICE_APP_CODE, "bk_app_secret": env.PLUGIN_SERVICE_APP_SECRET}
+            ),
+            "Content-Type": "application/json",
+        }
+        return requests.post(url, data=json.dumps(data), headers=headers).json()
+
+    def get_logs(self, trace_id):
+        url = os.path.join(self.plugin_host, "logs", trace_id)
+        return requests.get(url).json()
+
+    def get_meta(self):
+        url = os.path.join(self.plugin_host, "meta")
+        return requests.get(url).json()
+
+    def get_detail(self, version):
+        url = os.path.join(self.plugin_host, "detail", version)
+        return requests.get(url).json()
+
+    @response_parser
+    def get_schedule(self, trace_id):
+        url = os.path.join(self.plugin_host, "schedule", trace_id)
+        return requests.get(url).json()
+
+    @staticmethod
+    def get_plugin_list():
+        # TODO: delete mock data
+        return {"result": True, "message": None, "data": ["bk-plugin-demo"]}
