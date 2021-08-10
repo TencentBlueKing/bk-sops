7b34b4d8fb44dbd2b7a9e5807a970b63e9783424
fannluo <fannluowork@gmail.com>
2021-08-10 10:06:26 +0800

    feature: 节点拖动自动调整端点

--- frontend/desktop/src/components/common/TemplateCanvas/options.js
+++ frontend/desktop/src/components/common/TemplateCanvas/options.js
@@ -29,6 +29,7 @@
  */
 export const endpointOptions = {
     endpoint: 'Rectangle',
+    anchor: ['Left', 'Right', 'Top', 'Bottom'],
     connector: ['Flowchart', { stub: [20, 20], alwaysRespectStub: true, gap: 0, cornerRadius: 10 }], // 'Bezier'、'Straight'、'Flowchart'、'State Machine'，格式 [ type, params]
     connectorOverlays: [
         ['PlainArrow', { width: 8, length: 6, location: 1, id: 'arrow' }]
