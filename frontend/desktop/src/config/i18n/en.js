e6dc83ed1d878cdb9f7779c0bd90a7b1448a81e4
Luo Fan <44999219+fannluo@users.noreply.github.com>
2021-08-16 12:06:05 +0800

    improvement: 集群资源筛选变量存量数据兼容cmdb属性配置变更情况 (#3850)

--- frontend/desktop/src/config/i18n/en.js
+++ frontend/desktop/src/config/i18n/en.js
@@ -1132,7 +1132,10 @@ const en = {
     '个': '',
     '待更新的子流程': 'subprocesss to be updated',
     '引擎版本': 'Engine Version',
-    '打开该开关后，每次创建任务会尝试使用子流程的最新版本，并且不会再提示该节点需要更新，如果子流程中增加了新的变量，在不更新子流程版本的情况下，会使用变量默认值': 'With this switch on, each time a task is created it will try to use the latest version of the subprocess and will no longer prompt that the node needs to be updated. If a new variable is added to the subprocess, the default value of the variable will be used if the version of the subprocess is not updated'
+    '打开该开关后，每次创建任务会尝试使用子流程的最新版本，并且不会再提示该节点需要更新，如果子流程中增加了新的变量，在不更新子流程版本的情况下，会使用变量默认值': 'With this switch on, each time a task is created it will try to use the latest version of the subprocess and will no longer prompt that the node needs to be updated. If a new variable is added to the subprocess, the default value of the variable will be used if the version of the subprocess is not updated',
+    '任务状态': 'Task Status',
+    '成功': 'Success',
+    '变量保存数据与最新的CMDB集群配置存在差异，是否更新变量数据？': 'Variable data is different from the latest CMDB configuration, determine whether to update'
 }
 
 export default en
