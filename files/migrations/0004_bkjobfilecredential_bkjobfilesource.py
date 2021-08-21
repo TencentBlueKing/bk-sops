bf45d76c8720afdd82fa4107553cffa528bdbde3
waylon <1158341873@qq.com>
2021-08-21 21:02:59 +0800

    minor: add migrations for uploadticket is_available field

--- files/migrations/0004_bkjobfilecredential_bkjobfilesource.py
+++ files/migrations/0004_bkjobfilecredential_bkjobfilesource.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("files", "0003_auto_20200907_1754"),
+        ("files", "0003_uploadticket_isavailable_default_true"),
     ]
 
     operations = [
