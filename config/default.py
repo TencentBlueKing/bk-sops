b4c4578ef52bf4ed70d345a8ab6f02ee55819128
homholueng <homholueng@gmail.com>
2021-08-25 15:15:25 +0800

    minor: release V3.7.0

--- config/default.py
+++ config/default.py
@@ -163,7 +163,7 @@ LOGGING = get_logging_config_dict(locals())
 # Django模板中：<script src="/a.js?v="></script>
 # mako模板中：<script src="/a.js?v=${ STATIC_VERSION }"></script>
 # 如果静态资源修改了以后，上线前改这个版本号即可
-STATIC_VERSION = "3.6.50"
+STATIC_VERSION = "3.7.0"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
