4f2e2fdea3593106b1f454f2aaed861f8065cdf4
yovafeng <64761294@qq.com>
2021-09-15 20:41:23 +0800

    bugfix:修复ci单元测试卡顿

--- gcloud/tests/apigw/views/test_create_and_start_task.py
+++ gcloud/tests/apigw/views/test_create_and_start_task.py
@@ -31,7 +31,6 @@ TEST_PROJECT_ID = "123"
 TEST_PROJECT_NAME = "biz name"
 TEST_BIZ_CC_ID = "123"
 TEST_PIPELINE_TREE = {"constants": {"key1": {"value": ""}}}
-TEST_USERNAME = "username"
 
 
 class CreateAndStartTaskAPITest(APITest):
@@ -74,7 +73,6 @@ class CreateAndStartTaskAPITest(APITest):
                     ),
                     content_type="application/json",
                     HTTP_BK_APP_CODE=TEST_APP_CODE,
-                    HTTP_BK_USERNAME=TEST_USERNAME,
                 )
 
                 TaskFlowInstance.objects.create_pipeline_instance_exclude_task_nodes.assert_called_once_with(
@@ -118,7 +116,6 @@ class CreateAndStartTaskAPITest(APITest):
             data=json.dumps({"name": "name", "constants": {}, "exclude_task_node_id": "exclude_task_node_id"}),
             content_type="application/json",
             HTTP_BK_APP_CODE=TEST_APP_CODE,
-            HTTP_BK_USERNAME=TEST_USERNAME,
         )
 
         data = json.loads(response.content)
@@ -146,7 +143,6 @@ class CreateAndStartTaskAPITest(APITest):
             data=json.dumps({"name": "name", "constants": {}, "exclude_task_node_id": "exclude_task_node_id"}),
             content_type="application/json",
             HTPP_BK_APP_CODE=TEST_APP_CODE,
-            HTTP_BK_USERNAME=TEST_USERNAME,
         )
 
         data = json.loads(response.content)
@@ -174,7 +170,6 @@ class CreateAndStartTaskAPITest(APITest):
             data=json.dumps({"name": "name", "constants": {}, "exclude_task_node_id": "exclude_task_node_id"}),
             content_type="application/json",
             HTTP_BK_APP_CODE=TEST_APP_CODE,
-            HTTP_BK_USERNAME=TEST_USERNAME,
         )
         data = json.loads(response.content)
 
