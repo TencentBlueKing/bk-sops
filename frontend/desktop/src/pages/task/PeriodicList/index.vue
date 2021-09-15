9415662d2f0a440caeb4c80bd4bd1ad2f5abc202
v-xugzhou <941071842@qq.com>
2021-09-15 14:33:09 +0800

    minor: 周期任务添加ui自动化测试属性data-test-id

--- frontend/desktop/src/pages/task/PeriodicList/index.vue
+++ frontend/desktop/src/pages/task/PeriodicList/index.vue
@@ -26,12 +26,13 @@
                             theme="primary"
                             size="normal"
                             style="min-width: 120px;"
+                            data-test-id="periodicList-from-creatTask"
                             @click="onCreatePeriodTask">
                             {{$t('新建')}}
                         </bk-button>
                     </template>
                 </advance-search-form>
-                <div class="periodic-table-content">
+                <div class="periodic-table-content" data-test-id="periodicList-from-taskList">
                     <bk-table
                         :data="periodicList"
                         :pagination="pagination"
