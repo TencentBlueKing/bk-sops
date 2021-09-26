20797a9e3408cc9889102591d725ec74cb1a1d96
fannluo <fannluowork@gmail.com>
2021-09-26 18:00:28 +0800

    feature: 任务节点增加循环次数角标

--- frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
+++ frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
@@ -831,7 +831,17 @@
                         errorIgnorable = nodeActivities.error_ignorable
                     }
 
-                    const data = { status: currentNode.state, code, skippable, retryable, skip: currentNode.skip, retry: currentNode.retry, error_ignorable: errorIgnorable, error_ignored: currentNode.error_ignored }
+                    const data = {
+                        code,
+                        skippable,
+                        retryable,
+                        loop: currentNode.loop,
+                        status: currentNode.state,
+                        skip: currentNode.skip,
+                        retry: currentNode.retry,
+                        error_ignorable: errorIgnorable,
+                        error_ignored: currentNode.error_ignored
+                    }
 
                     this.setTaskNodeStatus(id, data)
                 }
