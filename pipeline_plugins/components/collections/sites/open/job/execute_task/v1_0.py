c8761094120cd8862fc0f1bbbc4c9517b001cfa4
yovafeng <64761294@qq.com>
2021-11-09 16:42:05 +0800

    bugfix:修复job执行作业1.0插件输入表单路径错误导致无法渲染

--- pipeline_plugins/components/collections/sites/open/job/execute_task/v1_0.py
+++ pipeline_plugins/components/collections/sites/open/job/execute_task/v1_0.py
@@ -34,7 +34,7 @@ class JobExecuteTaskComponent(Component):
     name = _("执行作业")
     code = "job_execute_task"
     bound_service = JobExecuteTaskService
-    form = "%scomponents/atoms/job/job_execute_task/v1_0.js" % settings.STATIC_URL
+    form = "%scomponents/atoms/job/execute_task/v1_0.js" % settings.STATIC_URL
     output_form = "%scomponents/atoms/job/job_execute_task_output.js" % settings.STATIC_URL
     version = "1.0"
     desc = "在接收到用户编辑的全局变量后，v1.0版本会默认用英文双引号将默认变量值包裹起来，再将得到的字符串作为一个整体在调用API时进行传参。\n" \
