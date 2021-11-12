aecbbabfb76f3ef8f4c38e30bcfff1167a730797
yovafeng <64761294@qq.com>
2021-11-12 15:45:58 +0800

    bugfixL:修复job插件执行历史下拉框无法跳转至JOB平台

--- pipeline_plugins/components/static/components/atoms/job/execute_task/v1_1.js
+++ pipeline_plugins/components/static/components/atoms/job/execute_task/v1_1.js
@@ -324,7 +324,7 @@
                     }
                     let biz_cc_id = this.get_parent && this.get_parent().get_child('biz_cc_id')._get_value();
                     let bk_job_host = window.BK_JOB_HOST;
-                    let url = bk_job_host + '/' + biz_cc_id + "/api_plan/" + this.value;
+                    let url = bk_job_host + '/' + biz_cc_id + "/execute/task/" + this.value;
                     window.open(url, '_blank')
                 },
                 cols: 10
