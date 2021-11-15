b3199446f0d7e2a7fe253886f0635f61afc06311
waylon <1158341873@qq.com>
2021-11-15 10:08:28 +0800

    bugfix: 修复接口创建任务参数未传元变量时table类型变量默认值获取失败问题

--- gcloud/taskflow3/models.py
+++ gcloud/taskflow3/models.py
@@ -555,7 +555,8 @@ class TaskFlowInstanceManager(models.Manager, TaskFlowStatisticsMixin):
             # set meta field for meta var, so frontend can render meta form
             if constant.get("is_meta"):
                 constant["meta"] = deepcopy(constant)
-                constant["value"] = constant["value"]["default"]
+                # 下拉框类型默认值字段为default，表格类型为default_text
+                constant["value"] = constant["value"].get("default") or constant["value"].get("default_text", "")
             if key in constants:
                 constant["value"] = constants[key]
 
