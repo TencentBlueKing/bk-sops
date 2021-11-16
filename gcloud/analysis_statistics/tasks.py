fe75a39482c2024bc3c7f4e2d9834ad325e955ce
yovafeng <64761294@qq.com>
2021-11-16 16:25:22 +0800

    bugfix:修复带子流程的模板流程创建失败

--- gcloud/analysis_statistics/tasks.py
+++ gcloud/analysis_statistics/tasks.py
@@ -23,6 +23,7 @@ from bamboo_engine import api as bamboo_engine_api
 from pipeline.component_framework.constants import LEGACY_PLUGINS_VERSION
 from pipeline.contrib.statistics.utils import count_pipeline_tree_nodes
 from pipeline.core.constants import PE
+from pipeline.models import PipelineTemplate
 from pipeline.engine import api as pipeline_api
 from pipeline.engine import states
 from pipeline.engine.utils import calculate_elapsed_time
@@ -211,7 +212,7 @@ def tasktemplate_post_save_statistics_task(template_id):
         # 子流程节点
         else:
             try:
-                template_id = act["template_id"]
+                template_id = PipelineTemplate.objects.get(template_id=act["template_id"]).id
             except KeyError:
                 logger.error(
                     "[tasktemplate_post_save_statistics_task]template_id={}的流程保存运营数据失败,子流程数据缺少template_id。".format(
