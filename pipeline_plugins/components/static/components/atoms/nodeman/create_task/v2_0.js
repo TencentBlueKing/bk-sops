616e9bdd7bb17623b8dde80a2cdb66fa93d8c675
yovafeng <64761294@qq.com>
2021-11-24 11:25:12 +0800

    minor:节点管理新建任务2.0插件认证密钥表单支持通过全局变量引用配置

--- pipeline_plugins/components/static/components/atoms/nodeman/create_task/v2_0.js
+++ pipeline_plugins/components/static/components/atoms/nodeman/create_task/v2_0.js
@@ -466,7 +466,7 @@
                                 },
                                 {
                                     tag_code: "auth_key",
-                                    type: "password",
+                                    type: "input",
                                     attrs: {
                                         name: gettext("认证密钥"),
                                         width: "400px",
@@ -475,7 +475,8 @@
                                             {
                                                 type: "required"
                                             }
-                                        ]
+                                        ],
+                                        showPassword: true
                                     },
                                     events: [
                                         {
