a82f9e99ed2c64c426509f84451693a00090b413
homholueng <homholueng@gmail.com>
2021-12-02 12:09:23 +0800

    minor: remove parserPreset in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,42 +4,31 @@ const Configuration = {
    * Referenced packages must be installed
    */
   extends: [
-    '@commitlint/config-angular',
-    '@commitlint/config-conventional',
-    '@commitlint/config-lerna-scopes',
-    '@commitlint/config-patternplate',
-    'conventional-changelog-lint-config-canonical',
-    'commitlint-config-jira',
-    'commitlint-config-function-rules',
+    '@commitlint/config-conventional'
   ],
   /*
    * Resolve and load conventional-changelog-atom from node_modules.
    * Referenced packages must be installed
-   */
-  parserPreset: 'conventional-changelog-atom',
-  /*
-   * Resolve and load @commitlint/format from node_modules.
-   * Referenced package must be installed
    */
   formatter: '@commitlint/format',
   /*
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feature',
-        'bugfix',
-        'minor',
-        'optimization',
-        'sprintfix',
-        'refactor',
-        'test',
-        'docs',
-        'merge',
-      ],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feature',
+				'bugfix',
+				'minor',
+				'optimization',
+				'sprintfix',
+				'refactor',
+				'test',
+				'docs',
+				'merge',
+			],
     ]
   },
   /*
