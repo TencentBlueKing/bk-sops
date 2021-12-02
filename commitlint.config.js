638df9d1bc14691e9d2a05d0c1d5813d31aaf433
homholueng <homholueng@gmail.com>
2021-12-02 12:10:55 +0800

    minor: remove parserPreset in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,15 +3,12 @@ const Configuration = {
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
    */
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional'
+  ],
   /*
    * Resolve and load conventional-changelog-atom from node_modules.
    * Referenced packages must be installed
-   */
-  parserPreset: 'conventional-changelog-atom',
-  /*
-   * Resolve and load @commitlint/format from node_modules.
-   * Referenced package must be installed
    */
   formatter: '@commitlint/format',
   /*
