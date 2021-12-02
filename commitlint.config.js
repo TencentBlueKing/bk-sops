18ae3a5beb454b54646e4247ed9b4e9a352185b0
homholueng <homholueng@gmail.com>
2021-12-02 12:02:23 +0800

    minor: add commitlint extends

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,15 @@ const Configuration = {
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
    */
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-angular',
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes',
+    '@commitlint/config-patternplate',
+    'conventional-changelog-lint-config-canonical',
+    'commitlint-config-jira',
+    'commitlint-config-function-rules',
+  ],
   /*
    * Resolve and load conventional-changelog-atom from node_modules.
    * Referenced packages must be installed
@@ -18,20 +26,20 @@ const Configuration = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-		'type-enum': [
-			2,
-			'always',
-			[
-				'feature',
-				'bugfix',
-				'minor',
-				'optimization',
-				'sprintfix',
-				'refactor',
-				'test',
-				'docs',
-				'merge',
-			],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feature',
+        'bugfix',
+        'minor',
+        'optimization',
+        'sprintfix',
+        'refactor',
+        'test',
+        'docs',
+        'merge',
+      ],
     ]
   },
   /*
