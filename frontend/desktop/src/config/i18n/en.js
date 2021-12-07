cdd8e86bd1f9d735f0883f35ed1c9a04c5587c8b
v-xugzhou <941071842@qq.com>
2021-12-07 12:51:52 +0800

    minor: 添加国际化

--- frontend/desktop/src/config/i18n/en.js
+++ frontend/desktop/src/config/i18n/en.js
@@ -1259,7 +1259,8 @@ const en = {
     '至少保留一条触发条件': 'Keep at least one trigger condition',
     '自动隐藏': 'Auto hide',
     '自动隐藏在显示状态下触发，当触发条件都满足时，才会在编辑页面隐藏，但是不会对传参产生影响': 'Auto-hide is triggered in the display state. When the trigger conditions are met, it will be hidden on the editing page, but it will not affect the parameter transfer.',
-    '启动失败': 'Failed to activate'
+    '启动失败': 'Failed to activate',
+    '节点ID': 'Node ID'
 }
 
 export default en
