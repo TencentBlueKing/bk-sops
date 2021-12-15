271a59991f5e93b878f87cc4b368f76a6e00f7fd
yuulong <yuulong@tencent.com>
2021-12-15 20:23:02 +0800

    optimization: 按照review修改代码 1. 定义了符合实际的请求和结果序列化器, 并且使用了请求序列化器处理请求 2. 对传进来的用户进行了iam校验,判断请求的用户是否为职能化用户 3. 判断了当filter为空时的处理 4. 修改了api路径,使其可以通过function_task_claimant_transfer调用

--- gcloud/contrib/function/api.py
+++ gcloud/contrib/function/api.py
@@ -1,35 +1,47 @@
 # -*- coding: utf-8 -*-
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from gcloud.contrib.function.models import FunctionTask
-
-
-class FunctionTaskClaimantTransferRequest(serializers.Serializer):
-    key = serializers.CharField(read_only=True, help_text="变量KEY")
-    value = serializers.CharField(read_only=True, help_text="变量值")
-
-
-class FunctionTaskClaimantTransferResponse(serializers.Serializer):
-    key = serializers.CharField(read_only=True, help_text="变量KEY")
-    value = serializers.CharField(read_only=True, help_text="变量值")
+from gcloud.contrib.function.serializers import FunctionTaskClaimantTransferRequestSerializer, \
+    FunctionTaskClaimantTransferResponse
+from gcloud.core.api_adapter.user_role import is_user_role
+from gcloud.iam_auth import IAMMeta
 
 
 class FunctionTaskClaimantTransferView(APIView):
+
     @swagger_auto_schema(
-        method="POST", operation_summary="职能转交", request_body=FunctionTaskClaimantTransferRequest,
+        method="POST", operation_summary="职能转交", request_body=FunctionTaskClaimantTransferRequestSerializer,
         responses={200: FunctionTaskClaimantTransferResponse},
     )
     @action(methods=["POST"], detail=False)
-    def function_task_claimant_transfer(self, request):
-        claimant = FunctionTask.objects.filter(id=request.data["id"]).values("claimant").first().get("claimant")
+    def post(self, request):
+        # 获取用户名鉴权是否拥有职能化权限
         username = request.user.username
+        if is_user_role(username, IAMMeta.FUNCTION_VIEW_ACTION):
+            return Response({"result": False, "message": "没有查看职能化任务权限"})
+
+        # 获取请求参数并校验
+        serializer = FunctionTaskClaimantTransferRequestSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({"result": False, "message": serializer.errors})
+
+        # 查询传进来的id职能化任务是否有效
+        serializer_data = serializer.data
+        function_task_query = FunctionTask.objects.filter(id=serializer_data["id"]).values("claimant")
+        if not function_task_query.count():
+            return Response({"result": False, "message": "该id查询不到职能化任务"})
+
+        # 查询当前任务是否有认领人判断是否已认领,并且请求的用户是否是认领人
+        claimant = function_task_query.first().get("claimant")
         if not claimant:
-            return Response({"result": False, "message": "查询不到该项目认领人"})
+            return Response({"result": False, "message": "查询不到该职能化任务认领人"})
         elif claimant != username:
-            return Response({"result": False, "message": "非该职能化认领人无法转交"})
-        FunctionTask.objects.filter(id=request.data["id"]).update(claimant=username)
+            return Response({"result": False, "message": "非该职能化任务认领人无法转交"})
+
+        # 修改并返回结果
+        FunctionTask.objects.filter(id=serializer_data["id"]).update(claimant=serializer_data["claimant"])
         return Response({"result": True, "data": []})
