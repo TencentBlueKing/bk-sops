f495e0500a4e30760db9666aea9493b82d2059b2
v-xugzhou <941071842@qq.com>
2021-12-31 12:30:16 +0800

    minor: 设置默认id逻辑调整

--- frontend/desktop/src/components/layout/NavigatorHeadRight.vue
+++ frontend/desktop/src/components/layout/NavigatorHeadRight.vue
@@ -35,7 +35,7 @@
     </div>
 </template>
 <script>
-    import { mapActions, mapState } from 'vuex'
+    import { mapActions, mapMutations, mapState } from 'vuex'
     import ProjectSelector from './ProjectSelector.vue'
     import VersionLog from './VersionLog.vue'
 
@@ -63,6 +63,7 @@
                 username: state => state.username
             }),
             ...mapState('project', {
+                project_id: state => state.project_id,
                 projectList: state => state.userProjectList
             }),
             // 隐藏右侧项目信息
@@ -82,6 +83,17 @@
                 return paths.some(path => this.$route.path.startsWith(path))
             }
         },
+        watch: {
+            projectList: {
+                handler (val) {
+                    if (val.length && !this.project_id) {
+                        const projectId = val[0].id
+                        this.setProjectId(projectId)
+                    }
+                },
+                immediate: true
+            }
+        },
         created () {
             if (this.view_mode !== 'appmaker') {
                 this.loadUserProjectList({ limit: 0, is_disable: false })
@@ -94,6 +106,9 @@
             ]),
             ...mapActions('project', [
                 'loadUserProjectList'
+            ]),
+            ...mapMutations('project', [
+                'setProjectId'
             ]),
             goToHelpDoc () {
                 window.open(this.bkDocUrl, '_blank')
