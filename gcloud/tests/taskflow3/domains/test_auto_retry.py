7701b0e07fb6fd93239d2ddfef38a558973b9de3
homholueng <homholueng@gmail.com>
2022-01-04 17:20:28 +0800

    feature: 后台支持配置节点自动重试间隔

--- gcloud/tests/taskflow3/domains/test_auto_retry.py
+++ gcloud/tests/taskflow3/domains/test_auto_retry.py
@@ -21,14 +21,17 @@ class AutoRetryNodeStrategyCreatorTestCase(TestCase):
         pipeline_tree = {
             "activities": {
                 "a1": {"type": "ServiceActivity", "auto_retry": {"enable": True, "times": 2}},
-                "a2": {"type": "ServiceActivity", "auto_retry": {"enable": True, "times": 12}},
+                "a2": {"type": "ServiceActivity", "auto_retry": {"enable": True, "times": 12, "interval": 4}},
                 "a3": {"type": "ServiceActivity"},
                 "s1": {
                     "type": "SubProcess",
                     "pipeline": {
                         "activities": {
                             "a4": {"type": "ServiceActivity", "auto_retry": {"enable": True, "times": -1}},
-                            "a5": {"type": "ServiceActivity", "auto_retry": {"enable": True, "times": 20}},
+                            "a5": {
+                                "type": "ServiceActivity",
+                                "auto_retry": {"enable": True, "times": 20, "interval": 30},
+                            },
                             "a6": {"type": "ServiceActivity"},
                         }
                     },
@@ -51,23 +54,52 @@ class AutoRetryNodeStrategyCreatorTestCase(TestCase):
                 "node_id": s.node_id,
                 "retry_times": s.retry_times,
                 "max_retry_times": s.max_retry_times,
+                "interval": s.interval,
             }
             for s in strategies
         }
 
         self.assertEqual(
             strategies_map["a1"],
-            {"taskflow_id": 1, "root_pipeline_id": "root", "node_id": "a1", "retry_times": 0, "max_retry_times": 2},
+            {
+                "taskflow_id": 1,
+                "root_pipeline_id": "root",
+                "node_id": "a1",
+                "retry_times": 0,
+                "max_retry_times": 2,
+                "interval": 0,
+            },
         )
         self.assertEqual(
             strategies_map["a2"],
-            {"taskflow_id": 1, "root_pipeline_id": "root", "node_id": "a2", "retry_times": 0, "max_retry_times": 10},
+            {
+                "taskflow_id": 1,
+                "root_pipeline_id": "root",
+                "node_id": "a2",
+                "retry_times": 0,
+                "max_retry_times": 10,
+                "interval": 4,
+            },
         )
         self.assertEqual(
             strategies_map["a4"],
-            {"taskflow_id": 1, "root_pipeline_id": "root", "node_id": "a4", "retry_times": 0, "max_retry_times": 1},
+            {
+                "taskflow_id": 1,
+                "root_pipeline_id": "root",
+                "node_id": "a4",
+                "retry_times": 0,
+                "max_retry_times": 1,
+                "interval": 0,
+            },
         )
         self.assertEqual(
             strategies_map["a5"],
-            {"taskflow_id": 1, "root_pipeline_id": "root", "node_id": "a5", "retry_times": 0, "max_retry_times": 10},
+            {
+                "taskflow_id": 1,
+                "root_pipeline_id": "root",
+                "node_id": "a5",
+                "retry_times": 0,
+                "max_retry_times": 10,
+                "interval": 10,
+            },
         )
