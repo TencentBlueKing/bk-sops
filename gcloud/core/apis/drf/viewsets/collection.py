51bfcbf11a4352e722f9a0b8508ba347cd607bf2
yovafeng <64761294@qq.com>
2022-01-24 15:07:21 +0800

    minor: 我的收藏接口优化get_queryset逻辑

--- gcloud/core/apis/drf/viewsets/collection.py
+++ gcloud/core/apis/drf/viewsets/collection.py
@@ -26,21 +26,9 @@ class CollectionViewSet(GcloudModelViewSet):
     serializer_class = CollectionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def list(self, request, *args, **kwargs):
-        self.queryset = self.queryset.filter(username=request.user.username)
-        return super().list(request, *args, **kwargs)
-
-    def retrieve(self, request, *args, **kwargs):
-        self.queryset = self.queryset.filter(username=request.user.username)
-        return super().retrieve(request, *args, **kwargs)
-
-    def update(self, request, *args, **kwargs):
-        self.queryset = self.queryset.filter(username=request.user.username)
-        return super().update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        self.queryset = self.queryset.filter(username=request.user.username)
-        return super().destroy(request, *args, **kwargs)
+    def get_queryset(self):
+        querySet = super().get_queryset()
+        return querySet.filter(username=self.request.user.username)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
