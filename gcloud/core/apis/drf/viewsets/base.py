0bee4fcb1fd0e0dc949d905be976c6af462e066b
Core <64761294@qq.com>
2022-01-25 15:11:14 +0800

    公共list处理逻辑支持使用方配置不分页 (#5034)
    * minor: 公共list处理逻辑支持使用方配置不分页

--- gcloud/core/apis/drf/viewsets/base.py
+++ gcloud/core/apis/drf/viewsets/base.py
@@ -61,9 +61,9 @@ class GcloudCommonMixin(IAMMixin, ApiMixin):
 class GcloudListViewSet(GcloudCommonMixin):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
+        # 支持使用方配置不分页
         page = self.paginate_queryset(queryset)
-
-        serializer = self.get_serializer(page, many=True)
+        serializer = self.get_serializer(page if page else queryset, many=True)
         # 注入权限
         data = self.injection_auth_actions(request, serializer.data, queryset)
         return self.get_paginated_response(data) if page is not None else Response(data)
