2aef1845fad2ca1da3c4dc0bdced50584044611c
DomineCore <64761294@qq.com>
2022-02-22 20:55:25 +0800

    minor: 联调问题修改

--- gcloud/core/apis/drf/serilaziers/task_template.py
+++ gcloud/core/apis/drf/serilaziers/task_template.py
@@ -43,16 +43,16 @@ class TaskTemplateSerializer(serializers.ModelSerializer):
 
     def get_notify_type(self, obj):
         if not getattr(obj, "notify_type") or not obj.notify_type:
-            return dict()
+            return json.loads(dict())
         return json.loads(obj.notify_type)
 
     def set_notify_type(self, data):
-        return {"notify_type": json.dumps(data)}
+        return {"notify_type": json.loads(data)}
 
     def get_notify_receivers(self, obj):
         if not getattr(obj, "notify_receivers") or not obj.notify_receivers:
-            return dict()
-        return json.loads(obj.notify_receivers)
+            return json.dumps(dict())
+        return json.dumps(obj.notify_receivers)
 
     def set_notify_receivers(self, data):
         return {"notify_receivers": json.dumps(data)}
