9bc3097ca980eb109f897be3ff6d34bb5686518e
v-xugzhou <941071842@qq.com>
2022-02-23 10:37:11 +0800

    minor: 创建公共流程的任务使用选择项目的timezone

--- frontend/desktop/src/pages/task/TaskCreate/TaskParamFill.vue
+++ frontend/desktop/src/pages/task/TaskCreate/TaskParamFill.vue
@@ -401,17 +401,7 @@
                 }
             },
             getDefaultTaskName () {
-                let nowTime = ''
-                if (this.common) {
-                    if (window.TIMEZONE) {
-                        nowTime = moment.tz(window.TIMEZONE).format('YYYYMMDDHHmmss')
-                    } else {
-                        // 无时区的公共流程使用本地的时间
-                        nowTime = moment().format('YYYYMMDDHHmmss')
-                    }
-                } else {
-                    nowTime = moment.tz(this.timeZone).format('YYYYMMDDHHmmss')
-                }
+                const nowTime = moment.tz(this.timeZone).format('YYYYMMDDHHmmss')
                 if (this.viewMode === 'appmaker') {
                     return this.appmakerTaskName + '_' + nowTime
                 }
