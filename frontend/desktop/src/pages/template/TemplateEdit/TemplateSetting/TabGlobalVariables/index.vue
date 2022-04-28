7fd70fcdd9f15c5f46bc5bbdb0ed1366598df84d
v-xugzhou <941071842@qq.com>
2022-04-28 16:58:31 +0800

    minor: 全局变量loading优化

--- frontend/desktop/src/pages/template/TemplateEdit/TemplateSetting/TabGlobalVariables/index.vue
+++ frontend/desktop/src/pages/template/TemplateEdit/TemplateSetting/TabGlobalVariables/index.vue
@@ -150,36 +150,39 @@
                         <span class="col-operation t-head">{{ $t('操作') }}</span>
                         <span class="col-more t-head"></span>
                     </div>
-                    <div class="variable-list" v-bkloading="{ isLoading: varListLoading, zIndex: 10 }">
-                        <draggable
-                            class="variable-drag"
-                            handle=".col-item-drag"
-                            :list="variableList"
-                            :disabled="isViewMode"
-                            @end="onDragEnd($event)">
-                            <variable-item
-                                v-for="constant in variableList"
-                                :key="constant.key"
-                                :outputed="outputs.indexOf(constant.key) > -1"
-                                :variable-data="constant"
-                                :variable-cited="variableCited"
-                                :variable-checked="!!(deleteVarList.find(item => item.key === constant.key))"
-                                :common="common"
-                                :is-view-mode="isViewMode"
-                                @viewClick="viewClick"
-                                @onEditVariable="onEditVariable"
-                                @onDeleteVariable="onDeleteVariable"
-                                @onCloneVariable="onCloneVariable"
-                                @onChooseVariable="onChooseVariable"
-                                @onChangeVariableShow="onChangeVariableShow"
-                                @onChangeVariableOutput="onChangeVariableOutput"
-                                @onCitedNodeClick="onCitedNodeClick">
-                            </variable-item>
-                        </draggable>
-                        <div v-if="variableList.length === 0" class="empty-variable-tips">
-                            <NoData>
-                                <p>{{$t('无数据，请手动新增变量或者勾选标准插件参数自动生成')}}</p>
-                            </NoData>
+                    <!-- 加一层div用来放bkLoading -->
+                    <div v-bkloading="{ isLoading: varListLoading, zIndex: 10 }">
+                        <div class="variable-list">
+                            <draggable
+                                class="variable-drag"
+                                handle=".col-item-drag"
+                                :list="variableList"
+                                :disabled="isViewMode"
+                                @end="onDragEnd($event)">
+                                <variable-item
+                                    v-for="constant in variableList"
+                                    :key="constant.key"
+                                    :outputed="outputs.indexOf(constant.key) > -1"
+                                    :variable-data="constant"
+                                    :variable-cited="variableCited"
+                                    :variable-checked="!!(deleteVarList.find(item => item.key === constant.key))"
+                                    :common="common"
+                                    :is-view-mode="isViewMode"
+                                    @viewClick="viewClick"
+                                    @onEditVariable="onEditVariable"
+                                    @onDeleteVariable="onDeleteVariable"
+                                    @onCloneVariable="onCloneVariable"
+                                    @onChooseVariable="onChooseVariable"
+                                    @onChangeVariableShow="onChangeVariableShow"
+                                    @onChangeVariableOutput="onChangeVariableOutput"
+                                    @onCitedNodeClick="onCitedNodeClick">
+                                </variable-item>
+                            </draggable>
+                            <div v-if="variableList.length === 0" class="empty-variable-tips">
+                                <NoData>
+                                    <p>{{$t('无数据，请手动新增变量或者勾选标准插件参数自动生成')}}</p>
+                                </NoData>
+                            </div>
                         </div>
                     </div>
                 </div>
