c976392e0cda0e3e8d530aa82ab23696c3f44891
waylon <1158341873@qq.com>
2022-05-26 19:49:06 +0800

    feature: 用户自定义流程表排序配置支持公共流程管理页

--- gcloud/resources/urls.py
+++ gcloud/resources/urls.py
@@ -81,9 +81,11 @@ v4_drf_router = DefaultRouter()
 v4_drf_router.register(r"project_template/(?P<project_id>\d+)", ProjectTemplateViewSet, basename="project_template")
 v4_drf_router.register(r"common_template", CommonTemplateViewSet, basename="common_template")
 v4_drf_router.register(r"clocked_task", ClockedTaskViewSet, basename="clocked_task")
-v4_drf_router.register(r"user_custom_config/(?P<project_id>\d+)", UserCustomConfViewset, basename="user_custom_config")
 v4_drf_router.register(
-    r"user_custom_config_options/(?P<project_id>\d+)", UserCustomConfigOptions, basename="user_custom_config_options"
+    r"user_custom_config/(?P<project_id>-?\d+)", UserCustomConfViewset, basename="user_custom_config"
+)
+v4_drf_router.register(
+    r"user_custom_config_options/(?P<project_id>-?\d+)", UserCustomConfigOptions, basename="user_custom_config_options"
 )
 
 # Standard bits...
