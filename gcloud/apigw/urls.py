290994c16061f99605e3cf2253b8541ae5ff0a41
waylon <1158341873@qq.com>
2022-05-31 10:29:01 +0800

    feature: 新增get_mini_app_list接口

--- gcloud/apigw/urls.py
+++ gcloud/apigw/urls.py
@@ -13,6 +13,7 @@ specific language governing permissions and limitations under the License.
 
 from django.conf.urls import url
 
+from gcloud.apigw.views.get_mini_app_list import get_mini_app_list
 from gcloud.apigw.views.register_project import register_project
 from gcloud.apigw.views.get_plugin_detail import get_plugin_detail
 from gcloud.apigw.views.get_functionalization_task_list import get_functionalization_task_list
@@ -56,10 +57,7 @@ from gcloud.apigw.views.create_and_start_task import create_and_start_task
 urlpatterns = [
     url(r"^dispatch_plugin_query/$", dispatch_plugin_query),
     url(r"^get_template_list/(?P<project_id>\d+)/$", get_template_list),
-    url(
-        r"^get_template_info/(?P<template_id>\d+)/(?P<project_id>\d+)/$",
-        get_template_info,
-    ),
+    url(r"^get_template_info/(?P<template_id>\d+)/(?P<project_id>\d+)/$", get_template_info,),
     url(r"^get_common_template_list/$", get_common_template_list),
     url(r"^get_common_template_info/(?P<template_id>\d+)/$", get_common_template_info),
     url(r"^create_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_task),
@@ -69,22 +67,10 @@ urlpatterns = [
     url(r"^get_task_status/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_task_status),
     url(r"^query_task_count/(?P<project_id>\d+)/$", query_task_count),
     url(r"^get_periodic_task_list/(?P<project_id>\d+)/$", get_periodic_task_list),
-    url(
-        r"^get_periodic_task_info/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
-        get_periodic_task_info,
-    ),
-    url(
-        r"^create_periodic_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$",
-        create_periodic_task,
-    ),
-    url(
-        r"^set_periodic_task_enabled/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
-        set_periodic_task_enabled,
-    ),
-    url(
-        r"^modify_cron_for_periodic_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
-        modify_cron_for_periodic_task,
-    ),
+    url(r"^get_periodic_task_info/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_periodic_task_info,),
+    url(r"^create_periodic_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_periodic_task,),
+    url(r"^set_periodic_task_enabled/(?P<task_id>\d+)/(?P<project_id>\d+)/$", set_periodic_task_enabled,),
+    url(r"^modify_cron_for_periodic_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$", modify_cron_for_periodic_task,),
     url(r"^modify_constants_for_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$", modify_constants_for_task),
     url(
         r"^modify_constants_for_periodic_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
@@ -92,28 +78,16 @@ urlpatterns = [
     ),
     url(r"^get_task_list/(?P<project_id>\d+)/$", get_task_list),
     url(r"^get_task_detail/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_task_detail),
-    url(
-        r"^get_task_node_detail/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
-        get_task_node_detail,
-    ),
+    url(r"^get_task_node_detail/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_task_node_detail,),
     url(r"^node_callback/(?P<task_id>\d+)/(?P<project_id>\d+)/$", node_callback),
     url(r"^import_common_template/$", import_common_template),
     url(r"^get_plugin_list/(?P<project_id>\d+)/$", get_plugin_list),
     url(r"^get_plugin_detail/(?P<project_id>\d+)/$", get_plugin_detail),
     url(r"^get_user_project_list/$", get_user_project_list),
     url(r"^get_user_project_detail/(?P<project_id>\d+)/$", get_user_project_detail),
-    url(
-        r"^get_template_schemes/(?P<project_id>\d+)/(?P<template_id>\d+)/$",
-        get_template_schemes,
-    ),
-    url(
-        r"^preview_task_tree/(?P<project_id>\d+)/(?P<template_id>\d+)/$",
-        preview_task_tree,
-    ),
-    url(
-        r"^get_task_node_data/(?P<project_id>\d+)/(?P<task_id>\d+)/$",
-        get_task_node_data,
-    ),
+    url(r"^get_template_schemes/(?P<project_id>\d+)/(?P<template_id>\d+)/$", get_template_schemes,),
+    url(r"^preview_task_tree/(?P<project_id>\d+)/(?P<template_id>\d+)/$", preview_task_tree,),
+    url(r"^get_task_node_data/(?P<project_id>\d+)/(?P<task_id>\d+)/$", get_task_node_data,),
     url(r"^operate_node/(?P<project_id>\d+)/(?P<task_id>\d+)/$", operate_node),
     url(r"^get_tasks_status/(?P<project_id>\d+)/$", get_tasks_status),
     url(r"^get_tasks_manual_intervention_state/(?P<project_id>\d+)/$", get_tasks_manual_intervention_state),
@@ -123,4 +97,5 @@ urlpatterns = [
     url(r"^preview_common_task_tree/(?P<project_id>\d+)/(?P<template_id>\d+)/$", preview_common_task_tree),
     url(r"^register_project/$", register_project),
     url(r"^create_and_start_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_and_start_task),
+    url(r"^get_mini_app_list/(?P<project_id>\d+)/$", get_mini_app_list),
 ]
