94ac5445c451c070696f806833d1be2f14499cf3
v-xugzhou <941071842@qq.com>
2022-06-08 13:12:48 +0800

    bugfix: 模板下拉框支持远程搜索

--- frontend/desktop/src/pages/template/TemplateList/ImportYamlTplDialog.vue
+++ frontend/desktop/src/pages/template/TemplateList/ImportYamlTplDialog.vue
@@ -73,6 +73,7 @@
                                     ext-popover-cls="tpl-popover"
                                     enable-scroll-load
                                     :scroll-loading="{ isLoading: scrollLoading }"
+                                    :remote-method="onTplSearch"
                                     @clear="overriders[row.meta.id] = ''"
                                     @selected="overriders[row.meta.id] = $event"
                                     @scroll-end="onSelectScrollLoad(row)">
@@ -141,6 +142,7 @@
                                         ext-popover-cls="tpl-popover"
                                         enable-scroll-load
                                         :scroll-loading="{ isLoading: scrollLoading }"
+                                        :remote-method="onCommonTplSearch"
                                         @clear="onClearRefer(row)"
                                         @selected="onSelectRefer(row, $event)"
                                         @scroll-end="onSelectScrollLoad(row)">
@@ -207,6 +209,7 @@
 <script>
     import i18n from '@/config/i18n/index.js'
     import { mapActions } from 'vuex'
+    import tools from '@/utils/tools.js'
     import permission from '@/mixins/permission.js'
 
     export default {
@@ -269,6 +272,10 @@
                 return this.subFlowList.slice((this.subFlowPagination.current - 1) * 5, this.subFlowPagination.current * 5)
             }
         },
+        created () {
+            this.onTplSearch = tools.debounce(this.handleTplSearch, 500)
+            this.onCommonTplSearch = tools.debounce(this.handleCommonTplSearch, 500)
+        },
         methods: {
             ...mapActions('templateList', [
                 'loadTemplateList',
@@ -328,14 +335,22 @@
                     })
                 }
             },
-            async getTemplateData (data) {
+            async getTemplateData (data, add) {
                 try {
                     const respData = await this.loadTemplateList(data)
                     if (data.common) {
-                        this.commonTemplateList.push(...respData.results)
+                        if (add) {
+                            this.commonTemplateList.push(...respData.results)
+                        } else {
+                            this.commonTemplateList = respData.results
+                        }
                         this.commonPagination.count = respData.count
                     } else {
-                        this.templateList.push(...respData.results)
+                        if (add) {
+                            this.templateList.push(...respData.results)
+                        } else {
+                            this.templateList = respData.results
+                        }
                         this.pagination.count = respData.count
                     }
                     const totalPage = Math.ceil(respData.count / 15)
@@ -351,25 +366,56 @@
                     this.scrollLoading = false
                 }
             },
+            // 下拉框搜索
+            handleTplSearch (val) {
+                this.pagination.current = 1
+                this.flowName = val
+                const params = this.getQueryData()
+                this.getTemplateData(params)
+            },
+            // 公共模板下拉框搜索
+            handleCommonTplSearch (val) {
+                this.commonPagination.current = 1
+                this.commonFlowName = val
+                const params = this.getQueryData(true)
+                this.getTemplateData(params)
+            },
             // 下拉框滚动加载
             onSelectScrollLoad (row) {
                 if (['useExisting', 'overrider'].includes(row.refer)) {
                     if (this.totalPage !== this.pagination.current) {
                         this.scrollLoading = true
                         this.pagination.current += 1
-                        const offset = (this.pagination.current - 1) * this.pagination.limit
-                        const params = { project__id: this.project_id, offset, limit: 15 }
-                        this.getTemplateData(params)
+                        const params = this.getQueryData()
+                        this.getTemplateData(params, true)
                     }
                 } else if (row.refer === 'useCommonExisting') {
                     if (this.commonTotalPage !== this.commonPagination.current) {
                         this.scrollLoading = true
                         this.commonPagination.current += 1
-                        const offset = (this.commonPagination.current - 1) * this.commonPagination.limit
-                        const params = { common: true, offset, limit: 15 }
-                        this.getTemplateData(params)
+                        const params = this.getQueryData(true)
+                        this.getTemplateData(params, true)
                     }
                 }
+            },
+            getQueryData (common = undefined) {
+                let tplName, offset, projectId
+                if (common) {
+                    tplName = this.commonFlowName
+                    offset = (this.commonPagination.current - 1) * this.commonPagination.limit
+                } else {
+                    tplName = this.flowName
+                    offset = (this.pagination.current - 1) * this.pagination.limit
+                    projectId = this.project_id
+                }
+                const data = {
+                    project__id: projectId || undefined,
+                    common,
+                    offset,
+                    limit: 15,
+                    pipeline_template__name__icontains: tplName || undefined
+                }
+                return data
             },
             handleErrorMsg (data) {
                 if (data && data.error && Object.keys(data.error).length > 0) {
@@ -664,6 +710,9 @@
         }
     }
     .tpl-popover {
+        .bk-option {
+            white-space: nowrap;
+        }
         .bk-spin-title {
             font-size: 12px;
         }
