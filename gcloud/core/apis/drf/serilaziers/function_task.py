66ba79c2463fef7ea6730f4cddd73bfb71c67344
waylon <1158341873@qq.com>
2022-06-17 11:36:26 +0800

    minor: 优化职能化任务显示时间格式

--- gcloud/core/apis/drf/serilaziers/function_task.py
+++ gcloud/core/apis/drf/serilaziers/function_task.py
@@ -19,6 +19,8 @@ from gcloud.core.apis.drf.serilaziers.taskflow import TaskSerializer
 class FunctionTaskSerializer(serializers.ModelSerializer):
     task = TaskSerializer()
     status_name = serializers.CharField(read_only=True)
+    create_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S%z", read_only=True)
+    claim_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S%z", read_only=True)
 
     class Meta:
         model = FunctionTask
