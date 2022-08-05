5942ab7c86f6ed9e9eda1f96be1e6c3eb535ed4a
v-xugzhou <941071842@qq.com>
2022-08-05 17:24:48 +0800

    bugfix: 跨流程变量克隆合并冲突错误修复

--- frontend/desktop/src/pages/template/TemplateEdit/TemplateSetting/TabGlobalVariables/index.vue
+++ frontend/desktop/src/pages/template/TemplateEdit/TemplateSetting/TabGlobalVariables/index.vue
@@ -649,15 +649,7 @@
             },
             // 垮流程克隆变量
             onCloneVarConfirm (constants = []) {
-                const indexList = []
-                const variableKeys = this.variableList.map(item => {
-                    indexList.push(item.index)
-                    return item.key
-                })
-                const maxIndex = Math.max(...indexList)
-                constants.forEach((item, index) => {
-                    item.key = this.setCloneKey(item.key, variableKeys)
-                    item.index = maxIndex + index + 1
+                constants.forEach(item => {
                     this.newCloneKeys.push(item.key)
                     this.addVariable(item)
                 })
