7c3b1efcef98e90ecab721a9f54a69f76b0c9fe5
luofann <fannluowork@gmail.com>
2022-08-26 17:22:34 +0800

    optimization: jsonschema表单样式调整

--- frontend/desktop/src/pages/template/TemplateEdit/NodeConfig/NodeConfig.vue
+++ frontend/desktop/src/pages/template/TemplateEdit/NodeConfig/NodeConfig.vue
@@ -998,10 +998,12 @@
                 this.updateBasicInfo(config)
                 this.inputsParamValue = {}
                 await this.getPluginDetail()
-                this.inputsRenderConfig = this.inputs.reduce((acc, crt) => {
-                    acc[crt.tag_code] = true
-                    return acc
-                }, {})
+                if (Array.isArray(this.inputs)) {
+                    this.inputsRenderConfig = this.inputs.reduce((acc, crt) => {
+                        acc[crt.tag_code] = true
+                        return acc
+                    }, {})
+                }
                 this.$refs.basicInfo && this.$refs.basicInfo.validate() // 清除节点保存报错时的错误信息
             },
             /**
@@ -1022,10 +1024,12 @@
                 await this.clearParamsSourceInfo()
                 this.inputsParamValue = {}
                 await this.getPluginDetail()
-                this.inputsRenderConfig = this.inputs.reduce((acc, crt) => {
-                    acc[crt.tag_code] = true
-                    return acc
-                }, {})
+                if (Array.isArray(this.inputs)) {
+                    this.inputsRenderConfig = this.inputs.reduce((acc, crt) => {
+                        acc[crt.tag_code] = true
+                        return acc
+                    }, {})
+                }
             },
             /**
              * 子流程切换
