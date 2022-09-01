da5cfb552e6fdfd8876ae7a6fe8f9571aeec2cff
waylon <1158341873@qq.com>
2022-09-01 20:17:08 +0800

    minor: 修改获取通知人业务查询报错提示

--- gcloud/utils/cmdb.py
+++ gcloud/utils/cmdb.py
@@ -174,7 +174,7 @@ def get_notify_receivers(client, biz_cc_id, supplier_account, receiver_group, mo
         logger.error(handle_api_error("CMDB", "cc.search_business", kwargs, cc_result))
         result = {
             "result": False,
-            "message": _("从 CMDB 查询到业务不唯一，业务ID:{}, 返回数量: {}".format(biz_cc_id, biz_count)),
+            "message": _("从 CMDB 查询业务异常，可能为如下原因：无CMDB业务访问权限/业务ID不存在/业务ID重复"),
             "data": None,
         }
         return result
