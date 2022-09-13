6a43b96ca96f9a4673cc570142b767b1ffab7b80
v_xugzhou <941071842@qq.com>
2022-09-13 18:11:26 +0800

    minor: 职能化描述文案修改

--- frontend/desktop/src/components/layout/Navigation.vue
+++ frontend/desktop/src/components/layout/Navigation.vue
@@ -122,7 +122,7 @@
                 if (this.currentNav === 'appMakerList') {
                     return this.$t('将流程快速⽣成⼀个蓝鲸SaaS 应⽤，可在蓝鲸应⽤市场进⾏搜索并添加到蓝鲸桌⾯。这种⽆需开发、快速⽣成的SaaS 应⽤称为 “轻应⽤”。')
                 } else if (this.currentNav === 'functionHome') {
-                    return this.$t('将流程的编排和执行进行角色分离，高阶人员负责编排流程，而日常执行这类简单任务交付给初级人员使用。这种交付模式称为“职能化”。')
+                    return this.$t('将流程的编排和执行进行角色分离，高阶人员负责编排流程，而日常执行这类简单任务交付给初阶人员，这种交付模式称为“职能化”。')
                 }
                 return ''
             }
--- frontend/desktop/src/config/i18n/en.js
+++ frontend/desktop/src/config/i18n/en.js
@@ -1479,7 +1479,7 @@ const en = {
     '。': '.',
     '条件': 'Condition',
     '将流程快速⽣成⼀个蓝鲸SaaS 应⽤，可在蓝鲸应⽤市场进⾏搜索并添加到蓝鲸桌⾯。这种⽆需开发、快速⽣成的SaaS 应⽤称为 “轻应⽤”。': 'Quickly generate a BlueKing SaaS application from the process, which can be searched in the BlueKing application market and added to the BlueKing desktop. This kind of SaaS application that does not require development and is quickly generated is called "light application".',
-    '将流程的编排和执行进行角色分离，高阶人员负责编排流程，而日常执行这类简单任务交付给初级人员使用。这种交付模式称为“职能化”。': 'The roles of process choreography and execution are separated, and senior personnel are responsible for choreographing the process, while simple tasks such as daily execution are delivered to junior personnel. This delivery model is called "functionalization".',
+    '将流程的编排和执行进行角色分离，高阶人员负责编排流程，而日常执行这类简单任务交付给初阶人员，这种交付模式称为“职能化”。': 'The roles of process choreography and execution are separated, and senior personnel are responsible for choreographing the process, while simple tasks such as daily execution are delivered to junior personnel. This delivery model is called "functionalization".',
     '未找到可用的插件或插件版本': 'No available plugin or plugin version found',
     '重选插件': 'Reselect plugin',
     '复⽤参数值并使⽤流程最新数据重新执⾏': 'Reuse parameter values and re-execute using the latest data of the process',
