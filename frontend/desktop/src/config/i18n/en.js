69a88097ff510605c9e6a9253295901e5beb477d
v_xugzhou <941071842@qq.com>
2022-09-14 16:20:18 +0800

    optimization: 添加刷新子流程任务状态提示

--- frontend/desktop/src/config/i18n/en.js
+++ frontend/desktop/src/config/i18n/en.js
@@ -1468,7 +1468,9 @@ const en = {
     '输入参数可在填参页面自定义传值。非输入参数填参页面不显示，使用变量默认值': 'Input parameters can be custom passed values on the parameter filling page. The non-input parameter filling page is not displayed, use the default value of the variable',
     '是否为输入参数': 'Is it an input parameter',
     '当满足触发条件时，输入参数在填参页面将不显示。可实现不同条件下配置不同的输入参数': 'When the trigger conditions are met, the input parameters will not be displayed on the parameter filling page. Different input parameters can be configured under different conditions',
-    '输入参数条件隐藏': 'Input parameter condition hidden'
+    '输入参数条件隐藏': 'Input parameter condition hidden',
+    '存在子流程节点执行失败，可从节点执行记录去往子任务处理，并及时': 'There is a subprocess node execution failure, you can go from the node execution record to the sub-task processing, and timely',
+    '刷新任务状态': 'Refresh task status'
 }
 
 export default en
