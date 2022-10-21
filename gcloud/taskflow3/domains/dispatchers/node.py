47e112491842f2c32edf3b8601e0dece18dec25d
hanshuaikang <1758504262@qq.com>
2022-10-21 19:57:33 +0800

    bugfix: 输出变量展示重命名之后的变量

--- gcloud/taskflow3/domains/dispatchers/node.py
+++ gcloud/taskflow3/domains/dispatchers/node.py
@@ -339,6 +339,29 @@ class NodeCommandDispatcher(EngineCommandDispatcher):
                 )
                 return False, "_format_outputs fail", []
 
+        # 尝试搜索并替换变量重命名的值
+        constants = pipeline_instance.execution_data.get("constants", {})
+
+        node_id_constants_map = {}
+
+        for key, value in constants.items():
+            if not value.get("source_type") == "component_outputs":
+                continue
+
+            # 搜索这些新变量的来源
+            source_info = value.get("source_info", {})
+            # 查看来源中是否有自己
+            if self.node_id in source_info.keys():
+                if len(source_info[self.node_id]) > 0:
+                    # key = ${key}, key[2:-1] = key
+                    node_id_constants_map[source_info[self.node_id][0]] = key[2:-1]
+
+        for item in outputs_table:
+            key = item.get("key")
+            if key in node_id_constants_map:
+                # 替换key值
+                item["key"] = node_id_constants_map[key]
+
         return True, "", outputs_table
 
     def get_node_data_v1(
