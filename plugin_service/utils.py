6c7712015bf02b0783c8ac5d89de417ded9aa533
waylon <1158341873@qq.com>
2022-11-07 19:48:52 +0800

    feature: plugin_service 支持插件分类功能

--- plugin_service/utils.py
+++ plugin_service/utils.py
@@ -10,19 +10,10 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-
-from django.conf.urls import url
-
-from . import api
+from plugin_service import env
 
 
-urlpatterns = [
-    url(r"^list/$", api.get_plugin_list),
-    url(r"^detail_list/$", api.get_plugin_detail_list),
-    url(r"^tags/$", api.get_plugin_tags),
-    url(r"^meta/$", api.get_meta),
-    url(r"^detail/$", api.get_plugin_detail),
-    url(r"^logs/$", api.get_logs),
-    url(r"^app_detail/$", api.get_plugin_app_detail),
-    url(r"^data_api/(?P<plugin_code>.+?)/(?P<data_api_path>.+)$", api.get_plugin_api_data),
-]
+def handle_plain_message(message):
+    if env.PAASV3_APIGW_API_TOKEN:
+        message = message.replace(env.PAASV3_APIGW_API_TOKEN, "******")
+    return message.replace(env.PLUGIN_SERVICE_APIGW_APP_SECRET, "******")
