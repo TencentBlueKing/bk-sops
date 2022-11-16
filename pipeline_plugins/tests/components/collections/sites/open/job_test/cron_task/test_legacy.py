4d69be211ba624727fd9dce896fbb04e1583968d
关健强 <905685941@qq.com>
2022-11-16 12:37:11 +0800

    bugfix: 后台报错文案优化处理-测试用例处理

--- pipeline_plugins/tests/components/collections/sites/open/job_test/cron_task/test_legacy.py
+++ pipeline_plugins/tests/components/collections/sites/open/job_test/cron_task/test_legacy.py
@@ -151,10 +151,10 @@ UPDATE_CRON_STATUS_FAIL_CASE = ComponentTestCase(
         success=False,
         outputs={
             "cron_id": 1,
+            "status": "暂停",
             "ex_data": "定时任务启动失败: [作业平台]定时任务启动发生异常：调用作业平台(JOB)接口jobv3.update_cron_status返回失败, "
             "params={'bk_scope_type':'biz','bk_scope_id':'1','bk_biz_id':1,'status':1,'id':1}, "
             "error=update_cron_status fail | execute",
-            "status": "暂停",
         },
     ),
     schedule_assertion=None,
