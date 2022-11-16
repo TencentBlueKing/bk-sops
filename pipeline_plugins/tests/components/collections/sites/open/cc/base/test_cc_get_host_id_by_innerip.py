60e913123b3171142da54bcb8e75a1f78c9cbec6
关健强 <905685941@qq.com>
2022-11-16 12:54:30 +0800

    bugfix: 后台报错文案优化处理-测试用例处理

--- pipeline_plugins/tests/components/collections/sites/open/cc/base/test_cc_get_host_id_by_innerip.py
+++ pipeline_plugins/tests/components/collections/sites/open/cc/base/test_cc_get_host_id_by_innerip.py
@@ -36,7 +36,9 @@ class CCGetHostIdByInnerIpTestCase(TestCase):
             self.executor, self.bk_biz_id, self.supplier_account, ["bk_host_id", "bk_host_innerip"], self.ip_list
         )
         self.assertFalse(data["result"])
-        self.assertEqual(data["message"], "IP [1.1.1.1, 2.2.2.2] 在本业务下重复: 请检查配置, 修复后重新执行 | cc_get_host_id_by_innerip")
+        self.assertEqual(
+            data["message"], "IP ['1.1.1.1', '2.2.2.2', '3.3.3.3'] 在本业务下重复: 请检查配置, 修复后重新执行 | cc_get_host_id_by_innerip"
+        )
 
     def test__return_host_list_gt_ip_list(self):
         mock_cmdb = MagicMock()
@@ -56,7 +58,9 @@ class CCGetHostIdByInnerIpTestCase(TestCase):
             self.executor, self.bk_biz_id, self.supplier_account, ["bk_host_id", "bk_host_innerip"], self.ip_list
         )
         self.assertFalse(data["result"])
-        self.assertEqual(data["message"], "IP [1.1.1.1, 2.2.2.2] 在本业务下重复: 请检查配置, 修复后重新执行 | cc_get_host_id_by_innerip")
+        self.assertEqual(
+            data["message"], "IP ['1.1.1.1', '2.2.2.2', '3.3.3.3'] 在本业务下重复: 请检查配置, 修复后重新执行 | cc_get_host_id_by_innerip"
+        )
 
     def test__return_host_list_lt_ip_list(self):
         mock_cmdb = MagicMock()
