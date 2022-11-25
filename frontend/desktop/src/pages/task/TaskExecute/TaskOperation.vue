adf9157b6096068bc25e201abfe26c67e7354015
yiwenZhou <67539158+ywywZhou@users.noreply.github.com>
2022-11-25 09:43:39 +0800

    Hawk开发功能自测问题修复 (#6338)
    * optimization: 完善任务节点没有node_tempalye_id字段的交互
    
    * bugfix: 后台管理的计划任务，查看执行历史后返回导航显示错误修复

--- frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
+++ frontend/desktop/src/pages/task/TaskExecute/TaskOperation.vue
@@ -1431,6 +1431,8 @@
                             curTime: this.formatDuring(this.instanceStatus.elapsed_time),
                             count: execution_time.length
                         }
+                    } else {
+                        this.$refs.templateCanvas.closeNodeExecRecord()
                     }
                 } catch (error) {
                     console.warn(error)
