201a8d68703028e14904c0d141694790674566f3
v_xugzhou <941071842@qq.com>
2023-01-03 10:09:41 +0800

    optimization: 报错信息复制内容保持一致

--- frontend/desktop/src/utils/errorNotify.js
+++ frontend/desktop/src/utils/errorNotify.js
@@ -48,7 +48,7 @@ export default class ErrorNotify {
                         style: { display: 'none', position: 'relative', margin: '10px 10px 0 auto' },
                         on: {
                             click: () => {
-                                this.handleCopy(vueInstance, traceId ? `trace_id：${traceId}\n${msg}` : msg)
+                                this.handleCopy(vueInstance, errorInfo)
                             }
                         }
                     }, [i18n.t('复制')])
@@ -149,13 +149,25 @@ export default class ErrorNotify {
         const errorMsg = instance.$el.textContent
         bus.$emit('onCloseErrorNotify', errorMsg)
     }
-    handleCopy (vueInstance, msg) {
+    handleCopy (vueInstance, errorInfo) {
+        const { msg, traceId, errorSource } = errorInfo
+        let copyMsg = ''
+        if (msg) {
+            copyMsg = this.setNotifyTitleAndContent(msg, true, errorSource) + '\n'
+            copyMsg = copyMsg + this.setNotifyTitleAndContent(msg, false, errorSource, 0) + '\n'
+        }
+        if (traceId) {
+            copyMsg = copyMsg + `trace_id：${traceId}` + '\n'
+        }
+        if (msg && errorSource === 'result') {
+            copyMsg = copyMsg + 'error_function: ' + this.setNotifyTitleAndContent(msg, false, errorSource, 1) || '--'
+        }
         const textarea = document.createElement('textarea')
         document.body.appendChild(textarea)
         textarea.style.position = 'fixed'
         textarea.style.clip = 'rect(0 0 0 0)'
         textarea.style.top = '10px'
-        textarea.value = msg
+        textarea.value = copyMsg
         textarea.select()
         document.execCommand('copy', true)
         document.body.removeChild(textarea)
