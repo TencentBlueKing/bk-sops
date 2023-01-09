b6909897d07b522d83be100b4e3fa14d6a2937f4
waylon <1158341873@qq.com>
2023-01-09 11:38:20 +0800

    optimization: 执行作业插件v1.2参数加载不添加双引号

--- pipeline_plugins/components/static/components/atoms/job/execute_task/v1_2.js
+++ pipeline_plugins/components/static/components/atoms/job/execute_task/v1_2.js
@@ -203,12 +203,6 @@
                                                 }
                                                 return item;
                                             })
-                                            // 清除首尾的双引号"，然后在首尾各加上一个双引号"
-                                            for (var i = new_global_var.length - 1; i >= 0; i--) {
-                                                if (new_global_var[i] != null && new_global_var[i].value != null) {
-                                                    new_global_var[i].value = '\"' + new_global_var[i].value.toString().replace(/^(\s|")+|(\s|")+$/g, '') + '\"';
-                                                }
-                                            }
                                             $this._set_value(new_global_var);
                                         }
                                         $this.set_loading(false);
@@ -246,11 +240,6 @@
                                 if (resp.result === false) {
                                     show_msg(resp.message, 'error');
                                 } else {
-                                    for (var i = resp.data.global_var.length - 1; i >= 0; i--) {
-                                        if (resp.data.global_var[i] !== null && resp.data.global_var[i].value != null) {
-                                            resp.data.global_var[i].value = '\"' + resp.data.global_var[i].value.toString().replace(/^(\s|")+|(\s|")+$/g, '') + '\"';
-                                        }
-                                    }
                                     $this._set_value(resp.data.global_var);
                                 }
 
