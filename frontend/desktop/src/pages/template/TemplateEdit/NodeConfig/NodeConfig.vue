3b52ab34d87f72c2d654d065eb9c5516175865af
v_xugzhou <941071842@qq.com>
2023-09-20 16:34:08 +0800

    optimization: 变量取消勾选后是否自动删除变量只根据变量来源数判断

--- frontend/desktop/src/pages/template/TemplateEdit/NodeConfig/NodeConfig.vue
+++ frontend/desktop/src/pages/template/TemplateEdit/NodeConfig/NodeConfig.vue
@@ -1293,12 +1293,7 @@
                     const { activities, conditions, constants } = this.variableCited[key]
                     const citedNum = activities.length + conditions.length + constants.length
                     if (citedNum <= 1) {
-                        // 切换插件/切换版本/更新子流程时直接删除引用量为1变量
-                        if (this.isUpdateConstants) {
-                            this.deleteUnhookingVar()
-                        } else {
-                            this.isCancelGloVarDialogShow = true
-                        }
+                        this.deleteUnhookingVar()
                     } else {
                         // 当变量来源为0时，自动删除变量
                         if (sourceInfo[id].length <= 1) {
