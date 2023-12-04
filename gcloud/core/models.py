d98b10da53869df8c2e5594ae96dfe0ed7bf0d0c
waylon <1158341873@qq.com>
2023-12-04 19:54:05 +0800

    feature: 支持项目下插件自由选择业务配置项

--- gcloud/core/models.py
+++ gcloud/core/models.py
@@ -424,6 +424,7 @@ class ProjectConfig(models.Model):
     executor_proxy = models.CharField(_("任务执行人代理"), max_length=255, default="", blank=True)
     executor_proxy_exempts = models.TextField(_("不使用执行人代理的用户列表"), default="", blank=True)
     max_periodic_task_num = models.IntegerField(_("项目下最大周期任务数"), default=-1, blank=True)
+    allow_plugin_cross_biz = models.BooleanField("支持模板插件中选择业务", default=False)
 
     objects = ProjectConfigManager()
 
