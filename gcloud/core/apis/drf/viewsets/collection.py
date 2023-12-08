5f5e0fcf2ad21aedf186ef12bd6b7aceca8f07ed
waylon <1158341873@qq.com>
2023-12-08 19:55:09 +0800

    fix: 我的收藏页面支持展示收藏的项目

--- gcloud/core/apis/drf/viewsets/collection.py
+++ gcloud/core/apis/drf/viewsets/collection.py
@@ -11,17 +11,20 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 
-from rest_framework import permissions, status, mixins
-from rest_framework.response import Response
-from rest_framework.exceptions import ErrorDetail
+import logging
 
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import mixins, permissions, status
+from rest_framework.exceptions import ErrorDetail
+from rest_framework.response import Response
+
+from gcloud import err_code
 from gcloud.contrib.collection.models import Collection
 from gcloud.core.apis.drf.serilaziers.collection import CollectionSerializer
 from gcloud.core.apis.drf.viewsets import GcloudReadOnlyViewSet
-from gcloud import err_code
-from gcloud.iam_auth import IAMMeta, utils as iam_auth_utils
-from django.utils.translation import ugettext_lazy as _
-import logging
+from gcloud.iam_auth import IAMMeta
+from gcloud.iam_auth import utils as iam_auth_utils
+from gcloud.iam_auth.utils import get_user_projects
 
 logger = logging.getLogger("root")
 
@@ -69,6 +72,7 @@ class CollectionViewSet(GcloudReadOnlyViewSet, mixins.CreateModelMixin, mixins.D
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def injection_auth_actions(self, request, serializer_data, queryset_data):
+        # 计算项目之下其他资源的权限
         resource_id_list_map = {r_type: [] for r_type in self.append_resource_actions}
 
         resource_allowed_actions_map = {}
@@ -83,7 +87,14 @@ class CollectionViewSet(GcloudReadOnlyViewSet, mixins.CreateModelMixin, mixins.D
                 iam_auth_utils, "get_{}_allowed_actions_for_user".format(r_type)
             )(request.user.username, self.append_resource_actions[r_type], id_list)
 
+        # 计算有查看权限的项目资源
+        user_projects = set(get_user_projects(request.user.username).values_list("id", flat=True))
+
         for item in serializer_data:
+            if item["category"] == "project":
+                item["auth_actions"] = [IAMMeta.PROJECT_VIEW_ACTION] if item["instance_id"] in user_projects else []
+                continue
+
             if item["category"] not in resource_allowed_actions_map:
                 item["auth_actions"] = []
                 continue
