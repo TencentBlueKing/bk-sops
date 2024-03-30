e75b54f01d532eb243387251054d5ff88dc14a4f
liujun <liujun@canway.net>
2024-03-30 11:57:28 +0800

    feat: 节点管理插件支持安装通道 #7397

--- pipeline_plugins/components/query/sites/open/nodeman.py
+++ pipeline_plugins/components/query/sites/open/nodeman.py
@@ -13,9 +13,9 @@ specific language governing permissions and limitations under the License.
 import logging
 import os
 
+from django.conf.urls import url
 from django.http import JsonResponse
 from django.utils.translation import ugettext_lazy as _
-from django.conf.urls import url
 
 from api.collections.nodeman import BKNodeManClient
 from gcloud.iam_auth.utils import check_and_raise_raw_auth_fail_exception
@@ -112,10 +112,35 @@ def nodeman_is_support_tjj(request):
     return JsonResponse({"result": True, "message": "success", "code": 0, "data": BKAPP_NODEMAN_SUPPORT_TJJ})
 
 
+def nodeman_get_install_channel(request, cloud_id: int):
+    client = BKNodeManClient(username=request.user.username)
+    install_channel_result = client.install_channel()
+
+    if not install_channel_result["result"]:
+        message = handle_api_error(_("节点管理(NODEMAN)"), "nodeman.install_channel", {}, install_channel_result)
+        logger.error(message)
+        check_and_raise_raw_auth_fail_exception(install_channel_result, message)
+        return JsonResponse(
+            {
+                "result": install_channel_result["result"],
+                "code": install_channel_result.get("code", "-1"),
+                "message": message,
+            }
+        )
+
+    data = install_channel_result["data"]
+    result = [
+        {"text": channel["name"], "value": channel["id"]} for channel in data if channel["bk_cloud_id"] == int(cloud_id)
+    ]
+    install_channel_result["data"] = result
+    return JsonResponse(install_channel_result)
+
+
 nodeman_urlpatterns = [
     url(r"^nodeman_get_cloud_area/$", nodeman_get_cloud_area),
     url(r"^nodeman_get_ap_list/$", nodeman_get_ap_list),
     url(r"^nodeman_is_support_tjj/$", nodeman_is_support_tjj),
     url(r"^nodeman_get_plugin_list/(?P<category>\w+)/$", nodeman_get_plugin_list),
     url(r"^nodeman_get_plugin_version/(?P<plugin>\w+)/(?P<os_type>\w+)/$", nodeman_get_plugin_version),
+    url(r"^nodeman_get_install_channel/(?P<cloud_id>\w+)/$", nodeman_get_install_channel),
 ]
