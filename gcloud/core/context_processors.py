2dd0220c8b7f8a6e164d165ce1f1ba3d6dd84893
waylon <1158341873@qq.com>
2024-06-19 17:56:30 +0800

    fix: 修改上下文登录链接 #ignore

--- gcloud/core/context_processors.py
+++ gcloud/core/context_processors.py
@@ -78,7 +78,7 @@ def mysetting(request):
         "BK_USER_MANAGE_HOST": settings.BK_USER_MANAGE_HOST,
         "BK_PAAS_ESB_HOST": settings.BK_PAAS_ESB_API_HOST,
         "APP_PATH": request.get_full_path(),  # 当前页面，主要为了login_required做跳转用
-        "LOGIN_URL": settings.LOGIN_URL,  # 登录链接
+        "LOGIN_URL": settings.BK_LOGIN_URL,  # 登录链接
         "RUN_MODE": settings.RUN_MODE,  # 运行模式
         "APP_CODE": settings.APP_CODE,  # 在蓝鲸系统中注册的  "应用编码"
         "APP_NAME": settings.APP_NAME,  # 应用名称
