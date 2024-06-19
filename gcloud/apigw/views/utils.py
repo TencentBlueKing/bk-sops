0e6beefd8ceea290d6ad6aee047efab63aa82872
liujun <liujun@canway.net>
2024-06-19 16:01:36 +0800

    perf: 任务列表响应速度优化 #7489

--- gcloud/apigw/views/utils.py
+++ gcloud/apigw/views/utils.py
@@ -180,7 +180,7 @@ def paginate_list_data(request, queryset):
         limit = int(request.GET.get("limit", 100))
         # limit 最大数量为200
         limit = 200 if limit > 200 else limit
-        if request.GET.get("no_count"):
+        if request.GET.get("no_pagination"):
             count = -1
         else:
             count = queryset.count()
