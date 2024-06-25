00f9099eb883504c9a307bf0fca4bd48736eadfc
lTimej <65690352+lTimej@users.noreply.github.com>
2024-06-25 09:51:31 +0800

    perf: 任务列表响应速度优化 #7489 (#7498)
    * perf: 任务列表响应速度优化 #7489
    
    * perf: 任务列表响应速度优化 #7489
    
    * perf: 任务列表响应速度优化 #7489
    
    * perf: 任务列表响应速度优化 #7489
    
    * perf: 任务列表响应速度优化 #7489
    
    * perf: 任务列表响应速度优化 #7489

--- gcloud/apigw/urls.py
+++ gcloud/apigw/urls.py
@@ -13,31 +13,35 @@ specific language governing permissions and limitations under the License.
 
 from django.conf.urls import url
 
-from gcloud.apigw.views.create_clocked_task import create_clocked_task
-from gcloud.apigw.views.get_mini_app_list import get_mini_app_list
-from gcloud.apigw.views.register_project import register_project
-from gcloud.apigw.views.get_plugin_detail import get_plugin_detail
-from gcloud.apigw.views.get_functionalization_task_list import get_functionalization_task_list
 from gcloud.apigw.views.claim_functionalization_task import claim_functionalization_task
+from gcloud.apigw.views.create_and_start_task import create_and_start_task
+from gcloud.apigw.views.create_clocked_task import create_clocked_task
 from gcloud.apigw.views.create_periodic_task import create_periodic_task
 from gcloud.apigw.views.create_task import create_task
 from gcloud.apigw.views.fast_create_task import fast_create_task
 from gcloud.apigw.views.get_common_template_info import get_common_template_info
 from gcloud.apigw.views.get_common_template_list import get_common_template_list
+from gcloud.apigw.views.get_functionalization_task_list import get_functionalization_task_list
+from gcloud.apigw.views.get_mini_app_list import get_mini_app_list
 from gcloud.apigw.views.get_periodic_task_info import get_periodic_task_info
 from gcloud.apigw.views.get_periodic_task_list import get_periodic_task_list
+from gcloud.apigw.views.get_plugin_detail import get_plugin_detail
 from gcloud.apigw.views.get_plugin_list import get_plugin_list
-from gcloud.apigw.views.get_task_list import get_task_list
+from gcloud.apigw.views.get_task_count import get_task_count
 from gcloud.apigw.views.get_task_detail import get_task_detail
+from gcloud.apigw.views.get_task_list import get_task_list
 from gcloud.apigw.views.get_task_node_data import get_task_node_data
 from gcloud.apigw.views.get_task_node_detail import get_task_node_detail
 from gcloud.apigw.views.get_task_status import get_task_status
+from gcloud.apigw.views.get_tasks_manual_intervention_state import get_tasks_manual_intervention_state
+from gcloud.apigw.views.get_tasks_status import get_tasks_status
 from gcloud.apigw.views.get_template_info import get_template_info
 from gcloud.apigw.views.get_template_list import get_template_list
 from gcloud.apigw.views.get_template_schemes import get_template_schemes
 from gcloud.apigw.views.get_user_project_detail import get_user_project_detail
 from gcloud.apigw.views.get_user_project_list import get_user_project_list
 from gcloud.apigw.views.import_common_template import import_common_template
+from gcloud.apigw.views.import_project_template import import_project_template
 from gcloud.apigw.views.modify_constants_for_periodic_task import modify_constants_for_periodic_task
 from gcloud.apigw.views.modify_constants_for_task import modify_constants_for_task
 from gcloud.apigw.views.modify_cron_for_periodic_task import modify_cron_for_periodic_task
@@ -48,17 +52,17 @@ from gcloud.apigw.views.plugin_proxy import dispatch_plugin_query
 from gcloud.apigw.views.preview_common_task_tree import preview_common_task_tree
 from gcloud.apigw.views.preview_task_tree import preview_task_tree
 from gcloud.apigw.views.query_task_count import query_task_count
+from gcloud.apigw.views.register_project import register_project
 from gcloud.apigw.views.set_periodic_task_enabled import set_periodic_task_enabled
 from gcloud.apigw.views.start_task import start_task
-from gcloud.apigw.views.get_tasks_status import get_tasks_status
-from gcloud.apigw.views.import_project_template import import_project_template
-from gcloud.apigw.views.get_tasks_manual_intervention_state import get_tasks_manual_intervention_state
-from gcloud.apigw.views.create_and_start_task import create_and_start_task
 
 urlpatterns = [
     url(r"^dispatch_plugin_query/$", dispatch_plugin_query),
     url(r"^get_template_list/(?P<project_id>\d+)/$", get_template_list),
-    url(r"^get_template_info/(?P<template_id>\d+)/(?P<project_id>\d+)/$", get_template_info,),
+    url(
+        r"^get_template_info/(?P<template_id>\d+)/(?P<project_id>\d+)/$",
+        get_template_info,
+    ),
     url(r"^get_common_template_list/$", get_common_template_list),
     url(r"^get_common_template_info/(?P<template_id>\d+)/$", get_common_template_info),
     url(r"^create_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_task),
@@ -68,10 +72,22 @@ urlpatterns = [
     url(r"^get_task_status/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_task_status),
     url(r"^query_task_count/(?P<project_id>\d+)/$", query_task_count),
     url(r"^get_periodic_task_list/(?P<project_id>\d+)/$", get_periodic_task_list),
-    url(r"^get_periodic_task_info/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_periodic_task_info,),
-    url(r"^create_periodic_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_periodic_task,),
-    url(r"^set_periodic_task_enabled/(?P<task_id>\d+)/(?P<project_id>\d+)/$", set_periodic_task_enabled,),
-    url(r"^modify_cron_for_periodic_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$", modify_cron_for_periodic_task,),
+    url(
+        r"^get_periodic_task_info/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
+        get_periodic_task_info,
+    ),
+    url(
+        r"^create_periodic_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$",
+        create_periodic_task,
+    ),
+    url(
+        r"^set_periodic_task_enabled/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
+        set_periodic_task_enabled,
+    ),
+    url(
+        r"^modify_cron_for_periodic_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
+        modify_cron_for_periodic_task,
+    ),
     url(r"^modify_constants_for_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$", modify_constants_for_task),
     url(
         r"^modify_constants_for_periodic_task/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
@@ -79,16 +95,28 @@ urlpatterns = [
     ),
     url(r"^get_task_list/(?P<project_id>\d+)/$", get_task_list),
     url(r"^get_task_detail/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_task_detail),
-    url(r"^get_task_node_detail/(?P<task_id>\d+)/(?P<project_id>\d+)/$", get_task_node_detail,),
+    url(
+        r"^get_task_node_detail/(?P<task_id>\d+)/(?P<project_id>\d+)/$",
+        get_task_node_detail,
+    ),
     url(r"^node_callback/(?P<task_id>\d+)/(?P<project_id>\d+)/$", node_callback),
     url(r"^import_common_template/$", import_common_template),
     url(r"^get_plugin_list/(?P<project_id>\d+)/$", get_plugin_list),
     url(r"^get_plugin_detail/(?P<project_id>\d+)/$", get_plugin_detail),
     url(r"^get_user_project_list/$", get_user_project_list),
     url(r"^get_user_project_detail/(?P<project_id>\d+)/$", get_user_project_detail),
-    url(r"^get_template_schemes/(?P<project_id>\d+)/(?P<template_id>\d+)/$", get_template_schemes,),
-    url(r"^preview_task_tree/(?P<project_id>\d+)/(?P<template_id>\d+)/$", preview_task_tree,),
-    url(r"^get_task_node_data/(?P<project_id>\d+)/(?P<task_id>\d+)/$", get_task_node_data,),
+    url(
+        r"^get_template_schemes/(?P<project_id>\d+)/(?P<template_id>\d+)/$",
+        get_template_schemes,
+    ),
+    url(
+        r"^preview_task_tree/(?P<project_id>\d+)/(?P<template_id>\d+)/$",
+        preview_task_tree,
+    ),
+    url(
+        r"^get_task_node_data/(?P<project_id>\d+)/(?P<task_id>\d+)/$",
+        get_task_node_data,
+    ),
     url(r"^operate_node/(?P<project_id>\d+)/(?P<task_id>\d+)/$", operate_node),
     url(r"^get_tasks_status/(?P<project_id>\d+)/$", get_tasks_status),
     url(r"^get_tasks_manual_intervention_state/(?P<project_id>\d+)/$", get_tasks_manual_intervention_state),
@@ -100,4 +128,5 @@ urlpatterns = [
     url(r"^create_and_start_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_and_start_task),
     url(r"^create_clocked_task/(?P<template_id>\d+)/(?P<project_id>\d+)/$", create_clocked_task),
     url(r"^get_mini_app_list/(?P<project_id>\d+)/$", get_mini_app_list),
+    url(r"^get_task_count/(?P<project_id>\d+)/$", get_task_count),
 ]
