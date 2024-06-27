f80fbe614cfe9d218b52f64a66ce6eb2abef135d
v_xugzhou <941071842@qq.com>
2024-06-27 17:20:05 +0800

    fix: 插件开发页面打不开问题修复 --bug=126434553

--- frontend/desktop/src/utils/serializeObj.js
+++ frontend/desktop/src/utils/serializeObj.js
@@ -9,7 +9,6 @@
 * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 * specific language governing permissions and limitations under the License.
 */
-import serialize from 'serialize-javascript'
 
 const DEFAULT_OPTIONS = {
     space: 4
@@ -17,5 +16,5 @@ const DEFAULT_OPTIONS = {
 
 export default function serializeObj (obj, options = {}) {
     options = Object.assign({}, DEFAULT_OPTIONS, options)
-    return serialize(obj, options)
+    return JSON.stringify(obj, null, options.space)
 }
