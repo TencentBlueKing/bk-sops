d00ed2b3e070dbdd5124a7eb594eaf9a4ea6db6b
huangpixu-canway <67646759+huangpixu@users.noreply.github.com>
2025-02-08 16:04:01 +0800

    修改版本号和pipeline的web模块celery启动命令 (#7692)
    * fix: 修改runtime文件python版本 #7625
    
    * fix: 修改版本号和pipeline的web模块celery启动命令 #7625

--- config/default.py
+++ config/default.py
@@ -216,7 +216,7 @@ LOGGING = get_logging_config_dict(locals())
 # mako模板中：<script src="/a.js?v=${ STATIC_VERSION }"></script>
 # 如果静态资源修改了以后，上线前改这个版本号即可
 
-STATIC_VERSION = "3.33.8"
+STATIC_VERSION = "3.34.1-rc.1"
 DEPLOY_DATETIME = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
@@ -688,6 +688,7 @@ def monitor_report_config():
 
         from bk_monitor_report import MonitorReporter  # noqa
         from bk_monitor_report.contrib.celery import MonitorReportStep  # noqa
+
         from blueapps.core.celery import celery_app  # noqa
 
         reporter = MonitorReporter(
