85913ef53112412a295bd8c7b38c0ee6dc3e3c72
arthurhuang <arthurhuang@canway.net>
2025-03-28 14:48:27 +0800

    fix: gunicorn启动添加--keep-alive参数 #7634

--- bin/start_web.sh
+++ bin/start_web.sh
@@ -1,2 +1,2 @@
 python manage.py collectstatic --noinput
-gunicorn wsgi -w $GUNICORN_WORKER_NUM -b [::]:${PORT:-5000} --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s "%(r)s" %(s)s %(D)s %(b)s "%(f)s" "%(a)s"' --max-requests=500 -k gthread --threads $GUNICORN_THREAD_NUM
+gunicorn wsgi -w $GUNICORN_WORKER_NUM -b [::]:${PORT:-5000} --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s "%(r)s" %(s)s %(D)s %(b)s "%(f)s" "%(a)s"' --max-requests=500 -k gthread --threads $GUNICORN_THREAD_NUM --keep-alive ${GUNICORN_KEEP_ALIVE:-0}
