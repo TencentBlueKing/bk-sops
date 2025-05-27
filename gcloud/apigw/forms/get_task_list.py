e1c5d7b72bce405e3729beb744af23754aa0ac31
yz <yz93407141@sina.com>
2025-05-27 19:13:21 +0800

    fix: 显式转换template_ids为list类型 --story=124236491 # Reviewed, transaction id: 44112

--- gcloud/apigw/forms/get_task_list.py
+++ gcloud/apigw/forms/get_task_list.py
@@ -24,3 +24,9 @@ class GetTaskListForm(forms.Form):
     without_count = forms.BooleanField(required=False)
     template_ids = forms.CharField(required=False)
     is_child_taskflow = forms.BooleanField(required=False)
+
+    def clean_template_ids(self):
+        template_ids = self.cleaned_data["template_ids"]
+        if not template_ids:
+            return []
+        return [id.strip() for id in template_ids.split(",") if id.strip()]
