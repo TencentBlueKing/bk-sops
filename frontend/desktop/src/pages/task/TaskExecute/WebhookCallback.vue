fb067421d6da606f47d40dce07cb814424f2bd64
Mianhuatang8 <2542880657@qq.com>
2025-09-15 15:02:46 +0800

    fix: 无数据情况下展示内容修改 --story=127437759

--- frontend/desktop/src/pages/task/TaskExecute/WebhookCallback.vue
+++ frontend/desktop/src/pages/task/TaskExecute/WebhookCallback.vue
@@ -3,17 +3,40 @@
         <bk-table
             ext-cls="wehhook-callback-table"
             :data="webhookHistory">
-            <bk-table-column width="200" show-overflow-tooltip :label="$t('回调时间')" prop="created_at"></bk-table-column>
-            <bk-table-column width="160" show-overflow-tooltip :label="$t('回调事件类型')" prop="event_code"></bk-table-column>
-            <bk-table-column width="160" show-overflow-tooltip :label="$t('回调结果')" prop="is_success">
+            <bk-table-column width="200" show-overflow-tooltip :label="$t('回调时间')" prop="created_at">
                 <template slot-scope="props">
-                    <bk-icon :type="statusInfo[props.row.is_success].icon"
-                        :class="`${props.row.is_success}-icon`" />
-                    <span class="task-status-text">{{$t(statusInfo[props.row.is_success].text)}}</span>
+                    <span v-if="props.row.created_at">{{ props.row.created_at }}</span>
+                    <span v-else>--</span>
                 </template>
             </bk-table-column>
-            <bk-table-column width="160" show-overflow-tooltip :label="$t('请求状态码')" prop="status_code"></bk-table-column>
-            <bk-table-column show-overflow-tooltip :label="$t('请求返回message')" prop="response"></bk-table-column>
+            <bk-table-column width="160" show-overflow-tooltip :label="$t('回调事件类型')" prop="event_code">
+                <template slot-scope="props">
+                    <span v-if="props.row.event_code">{{ props.row.event_code }}</span>
+                    <span v-else>--</span>
+                </template>
+            </bk-table-column>
+            <bk-table-column width="160" show-overflow-tooltip :label="$t('回调结果')" prop="is_success">
+                <template slot-scope="props">
+                    <span v-if="props.row.is_success !== undefined">
+                        <bk-icon :type="statusInfo[props.row.is_success].icon"
+                            :class="`${props.row.is_success}-icon`" />
+                        <span class="task-status-text">{{$t(statusInfo[props.row.is_success].text)}}</span>
+                    </span>
+                    <span v-else>--</span>
+                </template>
+            </bk-table-column>
+            <bk-table-column width="160" show-overflow-tooltip :label="$t('请求状态码')" prop="status_code">
+                <template slot-scope="props">
+                    <span v-if="props.row.status_code">{{ props.row.status_code }}</span>
+                    <span v-else>--</span>
+                </template>
+            </bk-table-column>
+            <bk-table-column show-overflow-tooltip :label="$t('请求返回message')" prop="response">
+                <template slot-scope="props">
+                    <span v-if="props.row.response">{{ props.row.response }}</span>
+                    <span v-else>--</span>
+                </template>
+            </bk-table-column>
             <div class="empty-data" slot="empty">
                 <NoData></NoData>
             </div>
