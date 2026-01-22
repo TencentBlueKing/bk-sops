48b6c60e1b54d08697be4a2e8a07b33fbb899d6a
dengyh <dengyh071@gmail.com>
2026-01-22 20:02:41 +0800

    refactor: 处理从master合入的代码, 使其符合python3.11的规范 --story=130255505

--- pipeline_plugins/components/collections/sites/open/job/execute_task/v2_0.py
+++ pipeline_plugins/components/collections/sites/open/job/execute_task/v2_0.py
@@ -13,7 +13,7 @@ specific language governing permissions and limitations under the License.
 
 from functools import partial
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from pipeline.component_framework.component import Component
 from pipeline.core.flow.io import StringItemSchema
 
--- pipeline_plugins/components/collections/sites/open/monitor/alarm_shield/v2_0.py
+++ pipeline_plugins/components/collections/sites/open/monitor/alarm_shield/v2_0.py
@@ -11,13 +11,13 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 from django.utils import translation
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from pipeline.component_framework.component import Component
 from pipeline.core.flow.io import StringItemSchema
-from pipeline_plugins.base.utils.inject import supplier_account_for_business
-from api.collections.monitor import BKMonitorClient
 
+from api.collections.monitor import BKMonitorClient
 from gcloud.conf import settings
+from pipeline_plugins.base.utils.inject import supplier_account_for_business
 from pipeline_plugins.components.collections.sites.open.monitor.alarm_shield.base import MonitorAlarmShieldServiceBase
 from pipeline_plugins.components.utils.sites.open.choose_time_tools import choose_time
 
