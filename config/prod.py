2b448d3aea6ad28cec0e65a4b44c12f1dadd3031
guohelu <19503896967@163.com>
2026-02-27 14:37:52 +0800

    feat: 修改数据库host定义 --story=130307462 # Reviewed, transaction id: 74440

--- config/prod.py
+++ config/prod.py
@@ -10,6 +10,8 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+import sys
+
 from config import RUN_VER
 
 if RUN_VER == "open":
@@ -31,3 +33,34 @@ CSRF_COOKIE_NAME = APP_CODE + "_csrftoken"
 default.logging_addition_settings(LOGGING, environment="prod")
 
 BK_APIGW_STAGE_NAME = "prod"
+
+
+def get_default_database_config_dict(settings_module):
+    if os.getenv("GCS_MYSQL_NAME") and os.getenv("MYSQL_NAME"):
+        db_prefix = os.getenv("DB_PREFIX")
+        if not db_prefix:
+            sys.stderr.write(
+                "Without DB_PREFIX environment variable while 'GCS_MYSQL_NAME' and 'MYSQL_NAME' are configured."
+            )
+            return {}
+    elif os.getenv("GCS_MYSQL_NAME"):
+        db_prefix = "GCS_MYSQL"
+    elif os.getenv("MYSQL_NAME"):
+        db_prefix = "MYSQL"
+    else:
+        if not settings_module.get("IS_LOCAL", False):
+            # 对应非GCS_MYSQL或MYSQL开头的情况，需开发者自行配置
+            sys.stderr.write("DB_PREFIX config is not 'GCS_MYSQL' or 'MYSQL_NAME'\n")
+        return {}
+    return {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ["%s_NAME" % db_prefix],
+        "USER": os.environ["%s_USER" % db_prefix],
+        "PASSWORD": os.environ["%s_PASSWORD" % db_prefix],
+        "HOST": os.environ["SOPS_HOST"] or os.environ["%s_HOST" % db_prefix],
+        "PORT": os.environ["%s_PORT" % db_prefix],
+        "OPTIONS": {"isolation_level": "repeatable read"},
+    }
+
+
+DATABASES = {"default": get_default_database_config_dict(locals())}
