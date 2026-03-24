531589a2d19c1d4ae5b73e95388ebf4fa8763ead
Mianhuatang8 <2542880657@qq.com>
2026-03-24 15:30:05 +0800

    fix: 轻应用创建任务人名称展示不对 --bug=155923954  --story=132934185 # Reviewed, transaction id: 76608

--- frontend/desktop/src/components/common/Individualization/UserDisplayName.vue
+++ frontend/desktop/src/components/common/Individualization/UserDisplayName.vue
@@ -2,7 +2,7 @@
     <div class="user-display-name-container">
         <span v-if="!name">{{ '--' }}</span>
         <bk-user-display-name v-else-if="isMultiTenantMode" :user-id="name" />
-        <span v-else>{{ name }}</span>
+        <span v-else>{{ displayName }}</span>
     </div>
 </template>
 <script>
@@ -14,10 +14,50 @@
                 default: ''
             }
         },
+        data () {
+            return {
+                displayName: this.name
+            }
+        },
         computed: {
             ...mapState({
                 'isMultiTenantMode': state => state.isMultiTenantMode
             })
+        },
+        watch: {
+            name: {
+                immediate: true,
+                handler (val) {
+                    if (val && !this.isMultiTenantMode) {
+                        this.displayName = val
+                        this.fetchUserDisplayInfo()
+                    }
+                }
+            }
+        },
+        methods: {
+            async fetchUserDisplayInfo () {
+                if (!this.name || this.isMultiTenantMode) {
+                    return
+                }
+                try {
+                    const resp = await fetch(`${window.BK_USER_WEB_APIGW_URL}/api/v3/open-web/tenant/users/${this.name}/display_info/`, {
+                        headers: {
+                            'x-bk-tenant-id': window.TENANT_ID
+                        },
+                        credentials: 'include'
+                    })
+                    const data = await resp.json()
+                    if (data.data && data.data.display_name) {
+                        this.displayName = data.data.display_name
+                    } else {
+                        this.displayName = this.name
+                    }
+                } catch (error) {
+                    console.error(error)
+                    this.displayName = this.name
+                }
+            }
         }
     }
 </script>
