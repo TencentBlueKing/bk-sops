94cadd85404aef2d7a32c48f09366712f13de316
guohelu <19503896967@163.com>
2026-05-18 17:00:10 +0800

    fix: 修改众测测试问题 --story=132553972

--- pipeline_plugins/components/collections/controller.py
+++ pipeline_plugins/components/collections/controller.py
@@ -16,8 +16,9 @@ import logging
 import os
 import re
 
+import pytz
 from django.conf import settings
-from django.utils import timezone, translation
+from django.utils import translation
 from django.utils.translation import gettext_lazy as _
 from pipeline.component_framework.component import Component
 from pipeline.core.flow.activity import StaticIntervalGenerator
@@ -112,7 +113,7 @@ class SleepTimerService(BasePluginService):
         # 项目时区获取
         project = Project.objects.get(id=parent_data.inputs.project_id)
 
-        project_tz = timezone.pytz.timezone(project.time_zone)
+        project_tz = pytz.timezone(project.time_zone)
         data.outputs.business_tz = project_tz
 
         now = datetime.datetime.now(tz=project_tz)
