3b498ce9fdc40870ba06cc31d9fa6d7b7d5e4753
v_lpggliu <flowerslanguage@126.com>
2026-06-01 17:29:56 +0800

    feat: 标准运维所有插件支持全局密码变量 --story=1010131351134372119

--- config/urls_custom.py
+++ config/urls_custom.py
@@ -57,5 +57,4 @@ if settings.ENVIRONMENT != "production" or settings.ENABLE_SWAGGER_UI:
         url(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
         url(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
         url(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-        
     ]
