a0e078afa26d7eba2f4631e26a5113f7969a4d25
Mianhuatang8 <2542880657@qq.com>
2026-08-25 16:38:48 +0800

    fix: iframe嵌套问题修复 --story=137395500 # Reviewed, transaction id: 85010

--- frontend/desktop/src/store/index.js
+++ frontend/desktop/src/store/index.js
@@ -44,7 +44,7 @@ const store = new Vuex.Store({
         },
         hasAdminPerm: null, // 是否有管理员查看权限
         hasStatisticsPerm: null, // 是否有运营数据查看权限
-        hideHeader: window.HIDE_HEADER === 1,
+        hideHeader: window.HIDE_HEADER === 1 || (window.self !== window.top && window.location.pathname.startsWith(`${window.SITE_URL}sub/`)),
         site_url: window.SITE_URL,
         app_id: window.APP_ID, // 轻应用 id
         view_mode: window.VIEW_MODE,
