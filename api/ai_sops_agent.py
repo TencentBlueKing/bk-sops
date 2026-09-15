0f4e378be5ef76ca8fc0f4840849e94d0fe95b12
ghluguo <19503896967@163.com>
2026-09-15 15:26:11 +0800

    fix: 修改智能体环境配置 --story=137888690

--- api/ai_sops_agent.py
+++ api/ai_sops_agent.py
@@ -67,7 +67,7 @@ class BKSopsAgentClient:
     def __init__(self, agent_host, request_type=AgentRequestType.PLUGIN, username=""):
         self.app_code = settings.APP_CODE
         self.app_secret = settings.SECRET_KEY
-        self.agent_host = agent_host or env.BK_SOPS_AGENT_HOST
+        self.agent_host = agent_host or env.AI_SOPS_AGENT_URL
         self.request_path = PATH_CONFIG_MAP.get(request_type.value)
         self.request_data = AgentRequestData(request_type)
 
@@ -82,7 +82,7 @@ class BKSopsAgentClient:
 
     def _make_request(self, method, params=None, data=None, timeout=None):
         try:
-            url = f"{self.agent_host}/prod/{self.request_path}/"
+            url = f"{self.agent_host}/{self.request_path}/"
             response = requests.request(
                 method=method,
                 url=url,
